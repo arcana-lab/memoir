52ddf3dc089ec5bc0618a243c9a19aaed88e1a1e
Tommy McMichen <tommymcmichen@gmail.com>
2022-03-29 22:38:12 -0500

    Fixed function signatures in accessor

--- runtime/include/accessor.h
+++ runtime/include/accessor.h
@@ -42,74 +42,53 @@ __attribute__((noinline)) Field *getUnionMember(
  * Field accesses
  */
 // Unsigned integer access
-__attribute__((noinline)) void writeUInt64(
-    IntegerField *field,
-    uint64_t value);
-__attribute__((noinline)) void writeUInt32(
-    IntegerField *field,
-    uint32_t value);
-__attribute__((noinline)) void writeUInt16(
-    IntegerField *field,
-    uint16_t value);
-__attribute__((noinline)) void writeUInt8(
-    IntegerField *field,
-    uint8_t value);
+__attribute__((noinline)) void writeUInt64(Field *field,
+                                           uint64_t value);
+__attribute__((noinline)) void writeUInt32(Field *field,
+                                           uint32_t value);
+__attribute__((noinline)) void writeUInt16(Field *field,
+                                           uint16_t value);
+__attribute__((noinline)) void writeUInt8(Field *field,
+                                          uint8_t value);
 
 // Signed integer access
-__attribute__((noinline)) void writeInt64(
-    IntegerField *field,
-    int64_t value);
-__attribute__((noinline)) void writeInt32(
-    IntegerField *field,
-    int32_t value);
-__attribute__((noinline)) void writeInt16(
-    IntegerField *field,
-    int16_t value);
-__attribute__((noinline)) void writeInt8(
-    IntegerField *field,
-    int8_t value);
+__attribute__((noinline)) void writeInt64(Field *field,
+                                          int64_t value);
+__attribute__((noinline)) void writeInt32(Field *field,
+                                          int32_t value);
+__attribute__((noinline)) void writeInt16(Field *field,
+                                          int16_t value);
+__attribute__((noinline)) void writeInt8(Field *field,
+                                         int8_t value);
 
 // Floating point access
-__attribute__((noinline)) void writeFloat(FloatField *field,
+__attribute__((noinline)) void writeFloat(Field *field,
                                           float value);
-__attribute__((noinline)) void writeDouble(
-    DoubleField *field,
-    double value);
+__attribute__((noinline)) void writeDouble(Field *field,
+                                           double value);
 
 // Pointer access
-__attribute__((noinline)) void writeObject(
-    ObjectField *field,
-    Object *object);
+__attribute__((noinline)) void writeObject(Field *field,
+                                           Object *object);
 
 // Unsigned integer access
-__attribute__((noinline)) uint64_t readUInt64(
-    IntegerField *field);
-__attribute__((noinline)) uint32_t readUInt32(
-    IntegerField *field);
-__attribute__((noinline)) uint16_t readUInt16(
-    IntegerField *field);
-__attribute__((noinline)) uint8_t readUInt8(
-    IntegerField *field);
+__attribute__((noinline)) uint64_t readUInt64(Field *field);
+__attribute__((noinline)) uint32_t readUInt32(Field *field);
+__attribute__((noinline)) uint16_t readUInt16(Field *field);
+__attribute__((noinline)) uint8_t readUInt8(Field *field);
 
 // Signed integer access
-__attribute__((noinline)) int64_t readInt64(
-    IntegerField *field);
-__attribute__((noinline)) int32_t readInt32(
-    IntegerField *field);
-__attribute__((noinline)) int16_t readInt16(
-    IntegerField *field);
-__attribute__((noinline)) int8_t readInt8(
-    IntegerField *field);
+__attribute__((noinline)) int64_t readInt64(Field *field);
+__attribute__((noinline)) int32_t readInt32(Field *field);
+__attribute__((noinline)) int16_t readInt16(Field *field);
+__attribute__((noinline)) int8_t readInt8(Field *field);
 
 // Floating point access
-__attribute__((noinline)) float readFloat(
-    FloatField *field);
-__attribute__((noinline)) double readDouble(
-    DoubleField *field);
+__attribute__((noinline)) float readFloat(Field *field);
+__attribute__((noinline)) double readDouble(Field *field);
 
 // Pointer access
-__attribute__((noinline)) Object *readObject(
-    ObjectField *field);
+__attribute__((noinline)) Object *readObject(Field *field);
 
 #ifdef __cplusplus
 } // extern "C"
