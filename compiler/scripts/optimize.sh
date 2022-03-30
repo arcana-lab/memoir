942afc80af593b4ce7e902832045634d4934fd5b
Tommy McMichen <tommymcmichen@gmail.com>
2022-03-30 17:44:50 -0500

    Added optimizing compiler pipeline

---
+++ compiler/scripts/optimize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+GIT_ROOT=`git rev-parse --show-toplevel` ;
+COMPILER_DIR=${GIT_ROOT}/compiler ;
+
+IR_FILE="$1" ;
+
+noelle-norm ${IR_FILE} -o ${IR_FILE};
+
+echo "Lower Objects (I: ${IR_FILE}, O: ${IR_FILE})" ;
+IR_FILE_LOWERED=all_in_one_lowered.bc ;
+noelle-load -load ${COMPILER_DIR}/passes/build/lib/ObjectLowering.so -ObjectLowering ${IR_FILE} -o ${IR_FILE} ;
+cp ${IR_FILE} ${IR_FILE_LOWERED} ;
