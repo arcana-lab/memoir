261274b26c917db040b31b87dee7c6a79a0e21de
Tommy McMichen <tommymcmichen@gmail.com>
2022-03-30 17:44:20 -0500

    Updated unit test to use c++

--- tests/unit/test_0/main.cpp
+++ tests/unit/test_0/main.cpp
@@ -1,9 +1,17 @@
+#include <iostream>
+
 #include "object_ir.h"
+
+using namespace objectir;
 
 int main() {
   Type *objTy = getObjectType(3, getUInt64Type(), getUInt64Type(), getUInt64Type());
 
+  std::cerr << objTy->toString() << "\n";
+    
   Object *myObj = buildObject(objTy);
+
+  std::cerr << myObj->toString() << "\n";
 
   Field *field1 = getObjectField(myObj, 0);
   Field *field2 = getObjectField(myObj, 1);
@@ -17,7 +25,19 @@ int main() {
   uint64_t read2 = readUInt64(field2);
   uint64_t read3 = readUInt64(field3);
 
+  std::cerr << "1: " << read1 << "\n";
+  std::cerr << "2: " << read2 << "\n";
+  std::cerr << "3: " << read3 << "\n\n";
+
   writeUInt64(field1, read1 + read2);
   writeUInt64(field2, read2 + read3);
   writeUInt64(field3, read3 + read1);
+  
+  read1 = readUInt64(field1);
+  read2 = readUInt64(field2);
+  read3 = readUInt64(field3);
+
+  std::cerr << "1: " << read1 << "\n";
+  std::cerr << "2: " << read2 << "\n";
+  std::cerr << "3: " << read3 << "\n\n";
 }
