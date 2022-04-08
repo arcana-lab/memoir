9ddad3cfcae41755b648c82ce14c79076194c9ad
tommymcm <johnmcmichen2025@u.northwestern.edu>
2022-04-08 13:21:52 -0500

    Updated noelle build pipeline

--- compiler/scripts/buildNOELLE.sh
+++ compiler/scripts/buildNOELLE.sh
@@ -4,11 +4,11 @@ if test $# -lt 1 ; then
   echo "USAGE: `basename $0` DESTINATION" ;
   exit 1;
 fi
-if test -e $1 ; then
-  exit 1;
+
+if ! test -d $1 ; then
+  git clone git@github.com:scampanoni/noelle.git $1 ;
 fi
 
-git clone git@github.com:scampanoni/noelle.git $1 ;
-cd $1 ; 
+cd $1 ;
 
 make ;
