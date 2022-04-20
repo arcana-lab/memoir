bb41806f68bf5713501ea16ce69a15a33008f440
Peter Zhong <peterzhong2023@u.northwestern.edu>
2022-04-19 20:04:29 -0500

    add more boilerplate

--- compiler/passes/object_lowering/src/ObjectLowering.cpp
+++ compiler/passes/object_lowering/src/ObjectLowering.cpp
@@ -27,7 +27,7 @@ void ObjectLowering::analyze() {
         callee->getName().str();
         errs() << I << "\n\n";
 
-        if (!callee) {
+        if (callee == nullptr) {
           // This is an indirect call, ignore for now
           continue;
         }
@@ -58,11 +58,82 @@ ObjectWrapper *ObjectLowering::parseObjectWrapperInstruction(CallInst *i) {
 }
 
 object_lowering::Type *ObjectLowering::parseType(Instruction *ins) {
-    // dispatch based on the types
+    // dispatch on the dynamic type of ins
+    if(auto callins = dyn_cast<CallInst>(ins))
+    {
+        return parseTypeCallInst(callins);
+    }
+    else if (auto storeIns = dyn_cast<StoreInst>(ins))
+    {
+        return parseTypeStoreInst(storeIns);
+    }
+    else if(auto loadIns = dyn_cast<LoadInst>(ins))
+    {
+        return parseTypeLoadInst(loadIns);
+    }
+    else if (auto allocaIns = dyn_cast<AllocaInst>(ins))
+    {
+        return parseTypeAllocaInst(allocaIns);
+    }
+    // we can't handle this so we just like low key give up
+    errs() << "Unrecognized Instruction" << *ins <<"\n";
+    assert(false);
     return nullptr;
 }
 
 object_lowering::Type *ObjectLowering::parseTypeCallInst(CallInst *ins) {
+    // check to see what sort of call instruction this is dispatch on the name of the function
+    auto callee = ins->getCalledFunction();
+    if (!callee) {
+        errs() << "Unrecognized indirect call" << *ins << "\n";
+        assert(false);
+    }
+    auto n = callee->getName().str();
+    if (!isObjectIRCall(n)) {
+        errs() << "Unrecognized function call " << *ins << "\n";
+        assert(false);
+    }
+    switch (FunctionNamesToObjectIR[n])
+    {
+        case OBJECT_TYPE:
+            break;
+        case ARRAY_TYPE:
+            break;
+        case UNION_TYPE:
+            break;
+        case INTEGER_TYPE:
+            break;
+        case UINT64_TYPE:
+            break;
+        case UINT32_TYPE:
+            break;
+        case UINT16_TYPE:
+            break;
+        case UINT8_TYPE:
+            break;
+        case INT64_TYPE:
+            break;
+        case INT32_TYPE:
+            break;
+        case INT16_TYPE:
+            break;
+        case INT8_TYPE:
+            break;
+        case FLOAT_TYPE:
+            break;
+        case DOUBLE_TYPE:
+            break;
+        case BUILD_OBJECT:
+            break;
+        case BUILD_ARRAY:
+            break;
+        case BUILD_UNION:
+            break;
+        default:
+            errs() <<"the switch should cover everything this is wrong\n";
+            assert(false);
+            break;
+    }
     return nullptr;
 }
 
