241d7c006d53a7a51e4cccc324787f293c994828
Yian Su <yiansu2018@u.northwestern.edu>
2022-05-02 10:50:45 -0500

    Add new unit test_abstraction

---
+++ tests/unit/test_abstraction/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "object_ir.h"
+
+using namespace objectir;
+
+Type *type = getObjectType(2, getUInt64Type(), getUInt64Type());
+
+Type *type2 = getObjectType(3, getUInt64Type(), getPointerType(type), getPointerType(type));
+
+int main () {
+  Object *object = buildObject(type);
+  Field *field1 = getObjectField(object, 0);
+  Field *field2 = getObjectField(object, 1);
+  writeUInt64(field1, rand());
+  writeUInt64(field2, rand());
+
+  std::cout << readUInt64(field1) + readUInt64(field2) << std::endl;
+
+  deleteObject(object);
+  return 0;
+}
