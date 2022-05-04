f8f5ae418ab3fce890d98a564128bc418f280f99
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-05-04 10:38:02 -0500

    Compiler: added source for noelle so that all scripts work regardless of your environment

--- compiler/scripts/optimize.sh
+++ compiler/scripts/optimize.sh
@@ -3,11 +3,18 @@
 GIT_ROOT=`git rev-parse --show-toplevel` ;
 COMPILER_DIR=${GIT_ROOT}/compiler ;
 
+source ${COMPILER_DIR}/noelle/enable ;
+
 IR_FILE="$1" ;
 
-noelle-norm ${IR_FILE} -o ${IR_FILE};
+OUT_DIR=$(dirname $(realpath ${IR_FILE}));
 
-echo "Lower Objects (I: ${IR_FILE}, O: ${IR_FILE})" ;
-IR_FILE_LOWERED=all_in_one_lowered.bc ;
+IR_FILE_NORM=${OUT_DIR}/all_in_one_norm.bc ;
+echo "Normalize Bitcode (I: ${IR_FILE}, O: ${IR_FILE_NORM})" ;
+noelle-norm ${IR_FILE} -o ${IR_FILE};
+cp ${IR_FILE} ${IR_FILE_NORM} ;
+
+IR_FILE_LOWERED=${OUT_DIR}/all_in_one_lowered.bc ;
+echo "Lower Objects (I: ${IR_FILE}, O: ${IR_FILE_LOWERED})" ;
 noelle-load -load ${COMPILER_DIR}/passes/build/lib/ObjectLowering.so -ObjectLowering ${IR_FILE} -o ${IR_FILE} ;
 cp ${IR_FILE} ${IR_FILE_LOWERED} ;
