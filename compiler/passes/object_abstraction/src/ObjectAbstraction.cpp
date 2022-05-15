525b726a83b720b12b395b8f4d584c80d86c4ea6
Yian Su <yiansu2018@u.northwestern.edu>
2022-05-15 08:45:49 -0500

    Implemented ObjectAbstraction pass

--- compiler/passes/object_abstraction/src/ObjectAbstraction.cpp
+++ compiler/passes/object_abstraction/src/ObjectAbstraction.cpp
@@ -6,18 +6,79 @@ TypeNode::TypeNode(CallInst *callInst)
   : definition(callInst) {
   this->numFields = dyn_cast<ConstantInt>(callInst->getArgOperand(0))->getSExtValue();
   for (int i = 1; i < 1 + this->numFields; i++) {
-    this->fields.push_back(static_cast<CallInst *>(callInst->getArgOperand(i)));
+    this->fieldTypes.push_back(dyn_cast<CallInst>(callInst->getArgOperand(i)));
   }
 
   return;
 }
 
-void TypeNode::printTypeInfo() {
-  errs() << "Definition: " << *this->definition << "\n";
-  errs() << "  Num of Fields: " << this->numFields << "\n";
+void TypeNode::printTypeInfo(string prefix="") {
+  errs() << prefix << "Definition: " << *this->definition << "\n";
+  errs() << prefix << "  Num of Fields: " << this->numFields << "\n";
   for (int i = 0; i < this->numFields; i++) {
-    errs() << "  Field " << i << ": " << *this->fields[i] << "\n";
+    errs() << prefix << "    Field " << i << ": " << *this->fieldTypes[i] << "\n";
   }
+
+  return;
+}
+
+FieldNode::FieldNode(ObjectNode *objectBelongsTo, CallInst *fieldType)
+  : objectBelongsTo(objectBelongsTo),
+    fieldType(fieldType) {
+  this->fieldReads = std::unordered_set<CallInst *>();
+  this->fieldWrites = std::unordered_set<CallInst *>();
+  
+  return;
+}
+
+void FieldNode::printFieldInfo(string prefix="") {
+  errs() << prefix << "Field type: " << *this->fieldType << "\n";
+  errs() << prefix << "  Field reads: " << "\n";
+  for (auto fieldRead : this->fieldReads) {
+    errs() << prefix << "  " << *fieldRead << "\n";
+  }
+  errs() << prefix << "  Field writes: " << "\n";
+  for (auto fieldWrite : this->fieldWrites) {
+    errs() << prefix << "  " << *fieldWrite << "\n";
+  }
+
+  return;
+}
+
+ObjectNode::ObjectNode(CallInst *callInst, TypeNode *typeNode)
+  : allocation(callInst),
+    manifest(typeNode) {
+  this->fields = std::vector<FieldNode *>();
+  this->fieldAccessorMap = std::unordered_map<CallInst *, FieldNode *>();
+  for (int i = 0; i < typeNode->getNumFields(); i++) {
+    FieldNode *fieldNode = new FieldNode(this, typeNode->getFieldType(i));
+    this->fields.push_back(fieldNode);
+  }
+
+  return;
+}
+
+void ObjectNode::addFieldAccessor(CallInst *fieldAccessor) {
+  if (auto fieldIndexConstant = dyn_cast<ConstantInt>(fieldAccessor->getArgOperand(1))) {
+    this->fieldAccessorMap[fieldAccessor] = this->fields[fieldIndexConstant->getSExtValue()];
+  }
+
+  return;
+}
+
+void ObjectNode::printObjectInfo(string prefix="") {
+  errs() << prefix << "Allocation: " << *this->allocation << "\n";
+  errs() << prefix << "Manifest: " << "\n";
+  this->manifest->printTypeInfo("  ");
+  errs() << prefix << "Field accesses: " << "\n";
+  for (auto &pair : this->fieldAccessorMap) {
+    errs() << prefix << "  " << *pair.first << "\n";
+  }
+  errs() << prefix << "Fields: " << "\n";
+  for (auto fieldNode : this->fields) {
+    fieldNode->printFieldInfo("  ");
+  }
+  errs() << prefix << "Deallocation: " << *this->deletion << "\n";
 
   return;
 }
@@ -33,15 +94,12 @@ void ObjectAbstraction::analyze() {
   // Analyze the program
 
   errs() << "Gathering objectIR calls\n";
-
   for (auto &F : this->M) {
     // Ignore function declaration
     if (F.empty()) {
       continue;
     }
-
     errs() << "  Inside function: " << F.getName() << "\n";
-
     for (auto &BB : F) {
       for (auto &I : BB) {
         if (auto callInst = dyn_cast<CallInst>(&I)) {
@@ -67,7 +125,6 @@ void ObjectAbstraction::analyze() {
     if (F.empty()) {
       continue;
     }
-
     for (auto &BB : F) {
       for (auto &I : BB) {
         if (auto callInst = dyn_cast<CallInst>(&I)) {
@@ -79,6 +136,126 @@ void ObjectAbstraction::analyze() {
           if (callee->getName() == ObjectIRToFunctionNames[OBJECT_TYPE]) {
             TypeNode *typeNode = new TypeNode(callInst);
             this->types.insert(typeNode);
+            this->callToTypeNodeMap[callInst] = typeNode;
+          }
+        }
+      }
+    }
+  }
+
+  errs() << "Gathering all object allocations\n";
+  for (auto &F : M) {
+    if (F.empty()) {
+      continue;
+    }
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto callInst = dyn_cast<CallInst>(&I)) {
+          auto callee = callInst->getCalledFunction();
+          if (!callee) {
+            continue;
+          }
+
+          // assumption at the moment
+          // the callInst is an object-ir call to buildObject
+          if (callee->getName() == ObjectIRToFunctionNames[BUILD_OBJECT]) {
+            if (auto *loadTypeInst = dyn_cast<LoadInst>(callInst->getArgOperand(0))) {
+              Value *globalTypePointer = loadTypeInst->getPointerOperand();
+              for (auto user : globalTypePointer->users()) {
+                if (auto storeTypeInst = dyn_cast<StoreInst>(user)) {
+                  if (auto callTypeInst = dyn_cast<CallInst>(storeTypeInst->getValueOperand())) {
+                    ObjectNode *objectNode = new ObjectNode(callInst, callToTypeNodeMap[callTypeInst]);
+                    this->objects.insert(objectNode);
+                    this->callToObjectNodeMap[callInst] = objectNode;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  errs() << "Gathering all field accessors of all objects\n";
+  for (auto &F : M) {
+    if (F.empty()) {
+      continue;
+    }
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto callInst = dyn_cast<CallInst>(&I)) {
+          auto callee = callInst->getCalledFunction();
+          if (!callee) {
+            continue;
+          }
+
+          // assumption at the moment
+          // the callInst is an object-ir call to getObjectField
+          if (callee->getName() == "getObjectField") {
+            if (auto *callObjectInst = dyn_cast<CallInst>(callInst->getArgOperand(0))) {
+              this->callToObjectNodeMap[callObjectInst]->addFieldAccessor(callInst);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  errs() << "Gathering all object deallocations\n";
+  for (auto &F : M) {
+    if (F.empty()) {
+      continue;
+    }
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto callInst = dyn_cast<CallInst>(&I)) {
+          auto callee = callInst->getCalledFunction();
+          if (!callee) {
+            continue;
+          }
+
+          // assumption at the moment
+          // the callInst is an object-ir call to getObjectField
+          if (callee->getName() == "deleteObject") {
+            if (auto *callObjectInst = dyn_cast<CallInst>(callInst->getArgOperand(0))) {
+              this->callToObjectNodeMap[callObjectInst]->setObjectDeletion(callInst);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  errs() << "Gathering all reads and writes of all objects fields\n";
+  for (auto &F : M) {
+    if (F.empty()) {
+      continue;
+    }
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto callInst = dyn_cast<CallInst>(&I)) {
+          auto callee = callInst->getCalledFunction();
+          if (!callee) {
+            continue;
+          }
+
+          // assumption at the moment
+          // the field accessors is only for UInt64
+          if (callee->getName() == "writeUInt64") {
+            if (auto *callFieldInst = dyn_cast<CallInst>(callInst->getArgOperand(0))) {
+              if (auto *callObjectInst = dyn_cast<CallInst>(callFieldInst->getArgOperand(0))) {
+                this->callToObjectNodeMap[callObjectInst]->addFieldWrite(callFieldInst, callInst);
+              }
+            }
+          }
+          if (callee->getName() == "readUInt64") {
+            if (auto *callFieldInst = dyn_cast<CallInst>(callInst->getArgOperand(0))) {
+              if (auto *callObjectInst = dyn_cast<CallInst>(callFieldInst->getArgOperand(0))) {
+                this->callToObjectNodeMap[callObjectInst]->addFieldRead(callFieldInst, callInst);
+              }
+            }
           }
         }
       }
@@ -86,7 +263,11 @@ void ObjectAbstraction::analyze() {
   }
 
   for (auto *typeNode : this->types) {
-      typeNode->printTypeInfo();
+    typeNode->printTypeInfo();
+  }
+
+  for (auto *objectNode : this->objects) {
+    objectNode->printObjectInfo();
   }
 }
 
--- compiler/passes/object_abstraction/src/ObjectAbstraction.hpp
+++ compiler/passes/object_abstraction/src/ObjectAbstraction.hpp
@@ -13,23 +13,55 @@
 
 namespace object_abstraction {
 
+class TypeNode;
+class FieldNode;
+class ObjectNode;
+
 class TypeNode {
 private:
   CallInst *definition;
   int numFields;
-  std::vector<CallInst *> fields;
+  std::vector<CallInst *> fieldTypes;
 
 public:
+  ~TypeNode();
   TypeNode(CallInst *inst);
-  void printTypeInfo();
-};
-
-class ObjectNode {
-  // TODO
+  inline int getNumFields() { return this->numFields; };
+  inline CallInst * getFieldType(int fieldNum) { return this->fieldTypes[fieldNum]; };
+  void printTypeInfo(string prefix);
 };
 
 class FieldNode {
-  // TODO
+private:
+  ObjectNode *objectBelongsTo;
+  CallInst *fieldType;
+  std::unordered_set<CallInst *> fieldReads;
+  std::unordered_set<CallInst *> fieldWrites;
+
+public:
+  ~FieldNode();
+  FieldNode(ObjectNode *objectBelongsTo, CallInst *fieldType);
+  inline void addFieldRead(CallInst *fieldRead) { this->fieldReads.insert(fieldRead); };
+  inline void addFieldWrite(CallInst *fieldWrite) { this->fieldWrites.insert(fieldWrite); };
+  void printFieldInfo(string prefix);
+};
+
+class ObjectNode {
+private:
+  CallInst *allocation;
+  TypeNode *manifest;
+  std::unordered_map<CallInst *, FieldNode *> fieldAccessorMap;
+  std::vector<FieldNode *> fields;
+  CallInst *deletion;
+
+public:
+  ~ObjectNode();
+  ObjectNode(CallInst *inst, TypeNode *typeNode);
+  void addFieldAccessor(CallInst *fieldAccessor);
+  inline void addFieldRead(CallInst *fieldAccessor, CallInst *fieldRead) { this->fieldAccessorMap[fieldAccessor]->addFieldRead(fieldRead); };
+  inline void addFieldWrite(CallInst *fieldAccessor, CallInst *fieldWrite) { this->fieldAccessorMap[fieldAccessor]->addFieldWrite(fieldWrite); };
+  inline void setObjectDeletion(CallInst *deletion) { this->deletion = deletion; };
+  void printObjectInfo(string prefix);
 };
 
 class ObjectAbstraction {
@@ -41,8 +73,10 @@ private:
   PDG *pdg;
 
   std::unordered_set<CallInst *> callsToObjectIR;
-
-  std::unordered_set<TypeNode *> types; // All definition of object types
+  std::unordered_set<TypeNode *> types; // All definition of object 
+  std::unordered_map<CallInst *, TypeNode *> callToTypeNodeMap; // Type definition to created TypeNode
+  std::unordered_set<ObjectNode *> objects; // All allocation of objects
+  std::unordered_map<CallInst *, ObjectNode *> callToObjectNodeMap; // Object allocation to created ObjectNode
 
 public:
   ObjectAbstraction(Module &M, Noelle *noelle, PDG *pdg);
