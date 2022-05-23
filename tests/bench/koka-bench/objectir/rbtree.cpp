46d636d022eb5084915c4b914ba1df005cb37f81
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-05-23 18:34:36 -0500

    Started porting rbtree to objectir

--- tests/bench/koka-bench/objectir/rbtree.cpp
+++ tests/bench/koka-bench/objectir/rbtree.cpp
@@ -1,43 +1,111 @@
 // Using standard STL to test the red-black tree in C++
-// In glibc++ this uses <https://github.com/gcc-mirror/gcc/tree/master/libstdc++-v3/src/c++98/tree.cc>
-// With the LLVM libc++ this uses <https://github.com/llvm/llvm-project/blob/main/libcxx/include/__tree>
-// In glibc this uses eventually: <https://sourceware.org/git/?p=glibc.git;a=blob;f=misc/tsearch.c>
-// (Highly optimized in-place red-black tree using the low pointer bit to encode color information.)
+// In glibc++ this uses
+// <https://github.com/gcc-mirror/gcc/tree/master/libstdc++-v3/src/c++98/tree.cc>
+// With the LLVM libc++ this uses
+// <https://github.com/llvm/llvm-project/blob/main/libcxx/include/__tree>
+// In glibc this uses eventually:
+// <https://sourceware.org/git/?p=glibc.git;a=blob;f=misc/tsearch.c>
+// (Highly optimized in-place red-black tree using the low
+// pointer bit to encode color information.)
 
 #include <iostream>
-#include <map>
-#include <algorithm>
-using std::for_each;
 
-typedef int nat;
+/*
+ * This implementation is based on the Java implementation
+ * since the C++ version uses strd::map's internal rb-tree
+ * which is too hacky and we want a more grounded
+ * implementation.
+ */
 
-struct nat_lt_fn {
-    bool operator()(nat const & n1, nat const & n2) const { return n1 < n2; }
-};
-
-typedef std::map<nat, bool, nat_lt_fn> map;
-
-map mk_map(unsigned n) {
-    map m;
-    while (n > 0) {
-        --n;
-        m.insert(std::make_pair(nat(n), n%10 == 0));
-    }
-    return m;
+enum Color {
+  Red,  // 0
+  Black // 1
 }
 
-nat fold(map const & m) {
-    nat r(0);
-    for_each(m.begin(), m.end(), [&](std::pair<nat, bool> const & p) { if (p.second) r = r + nat(1); });
-    return r;
+Type *Tree = nameObjectType(
+    "Tree",
+    5,
+    getBooleanType(),                     // Color
+    getPointerType(getNamedType("Tree")), // left
+    getInt32Type(),                       // key
+    getBooleanType(1, false),             // val
+    getPointerType(getNamedType("Tree"))  // right
+);
+
+Object *new_Tree(Color c,
+                 Object *l,
+                 int32_t k,
+                 boolean v,
+                 Object *r) {
+  setReturnType(Tree);
+  assertType(Tree, l);
+  assertType(Tree, r);
+
+  auto newTree = buildObject(Tree);
+  writeBoolean(getObjectField(newTree, 0), (boolean)c);
+  writePointer(getObjectField(newTree, 1), l);
+  writeInt32(getObjectField(newTree, 2), k);
+  writeBoolean(getObjectField(newTree, 3), v);
+  writePointer(getObjectField(newTree, 4), r);
+
+  return newTree;
 }
 
-int main(int argc, char ** argv) {
-    unsigned n = 4200000;
-    if (argc == 2) {
-      n = atoi(argv[1]);
+boolean isRed(Object *t) {
+  assertType(Tree, t);
+
+  if (t == nullptr) {
+    return false;
+  }
+
+  auto c = (Color)readBoolean(getObjectField(t, 0));
+
+  return c == Color::Red;
+}
+
+Object *balanceRight(int kv,
+                     boolean vv,
+                     Object *t,
+                     Object *n) {
+  setReturnType(Tree);
+  assertType(Tree, t);
+  assertType(Tree, n);
+
+  if (n == nullptr) {
+    return nullptr;
+  }
+
+  auto n_leftFld = getObjectField(n, 1);
+  auto n_left = readPointer(n_leftFld);
+  if (n_left != nullptr) {
+    auto l = n_left;
+
+    auto l_colorFld = getObjectField(l, 0);
+    auto l_color = (Color)readBoolean(l);
+    if (l_color == Color::Red) {
+      auto l_left = readPointer(getObjectField(l, 1));
+      auto l_key = readInt32(getObjectField(l, 2));
+      auto l_val = readBoolean(getObjectField(l, 3));
+      auto l_right = readPointer(getObjectField(l, 4));
+      auto newLeft = new_Tree(Color::Black,
+                              l_left,
+                              l_key,
+                              l_val,
+                              l_right);
+
+      auto n_right = readPointer(getObjectField(n, 4));
+      auto newRight =
+          new_Tree(Color::Black, n_right, kv, vv, t);
+
+      auto n_key = readInt32(getObjectField(n, 2));
+      auto n_val = readBoolean(getObjectField(n, 3));
+      return new_Tree(Color::Red,
+                      newLeft,
+                      n_key,
+                      n_val,
+                      newRight);
     }
-    map m = mk_map(n);
-    std::cout << fold(m) << "\n";
-    return 0;
+
+    // TODO
+  }
 }
