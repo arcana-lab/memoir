2001e83e088e3b16cbf5882071201f40bb5e926e
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-05-25 15:10:59 -0500

    Updated the run.sh script to be prettier

--- tests/bench/koka-bench/scripts/run.sh
+++ tests/bench/koka-bench/scripts/run.sh
@@ -6,10 +6,10 @@ OUT=runs.out ;
 
 echo "" > runs.out ;
 
-RUNS="1 2 3 4 5 6 7 8 9 10" ;
-for i in ${RUNS}
+NUM_RUNS=10 ;
+for ((i=1; i <= NUM_RUNS; i++))
 do
     echo "Run ${i}" ;
-    perf stat ${CMD} 26 ;
+    perf stat ${CMD} ;
     echo "" ;
 done
