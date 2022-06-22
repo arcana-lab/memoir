9b6e9710a8454631a8f50d05df36554d5e55e84f
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-06-22 16:44:59 -0500

    Updated Object Lowering pass to not clone internal methods

--- compiler/passes/object_lowering/src/ObjectLowering.hpp
+++ compiler/passes/object_lowering/src/ObjectLowering.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "noelle/core/Noelle.hpp"
-#include "Utils.hpp"
-#include "types.hpp"
 #include "Parser.hpp"
+#include "Utils.hpp"
+#include "noelle/core/Noelle.hpp"
+#include "types.hpp"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/IRBuilder.h"
+
+#include "common/support/Metadata.hpp"
 
 /*
  * Pass to perform lowering from object-ir to LLVM IR
@@ -20,24 +22,23 @@ class ObjectLowering {
 private:
   Module &M;
   Noelle *noelle;
-  ModulePass* mp;
+  ModulePass *mp;
   Parser *parser;
 
   // llvm Type*s
-  Type* object_star;
-  Type* type_star;
-  Type* type_star_star;
+  Type *object_star;
+  Type *type_star;
+  Type *type_star_star;
 
   // collect all GlobalVals which are Type*
   std::vector<GlobalValue *> typeDefs;
 
-  std::map<Function*, Function*> clonedFunctionMap;
-  std::map<Function*, map<Argument *, Argument *>> functionArgumentMaps;
-
+  std::map<Function *, Function *> clonedFunctionMap;
+  std::map<Function *, map<Argument *, Argument *>> functionArgumentMaps;
 
 public:
-  ObjectLowering(Module &M, Noelle *noelle, ModulePass* mp);
-  
+  ObjectLowering(Module &M, Noelle *noelle, ModulePass *mp);
+
   // ==================== ANALYSIS ====================
 
   void analyze();
@@ -45,8 +46,10 @@ public:
   void cacheTypes(); // analyze the global values for type*
 
   // handle object passing and returning
-  void inferArgTypes(llvm::Function* f, vector<Type*> *arg_vector); // build a new list of argument types
-  ObjectType* inferReturnType(llvm::Function* f);
+  void inferArgTypes(
+      llvm::Function *f,
+      vector<Type *> *arg_vector); // build a new list of argument types
+  ObjectType *inferReturnType(llvm::Function *f);
 
   // ======================== STACK VS HEAP =====================
 
@@ -56,17 +59,21 @@ public:
 
   void transform();
 
-  void FunctionTransform(Function* func);
-  
-  void BasicBlockTransformer(DominatorTree &DT, BasicBlock *bb,
-                             std::map<Value*, Value*> &replacementMapping,
-                             std::set<PHINode*>& phiNodesToPopulate,
-                             std::set<CallInst*>& allocaBuildObj);
+  void FunctionTransform(Function *func);
 
-  Value* CreateGEPFromFieldWrapper(FieldWrapper *wrapper, IRBuilder<> &builder, std::map<Value*, Value*> &replacementMapping);
+  void BasicBlockTransformer(DominatorTree &DT,
+                             BasicBlock *bb,
+                             std::map<Value *, Value *> &replacementMapping,
+                             std::set<PHINode *> &phiNodesToPopulate,
+                             std::set<CallInst *> &allocaBuildObj);
+
+  Value *CreateGEPFromFieldWrapper(
+      FieldWrapper *wrapper,
+      IRBuilder<> &builder,
+      std::map<Value *, Value *> &replacementMapping);
 
   // recursively add users of `i` to `toDelete`
-  void findInstsToDelete(Value* i, std::set<Value*> &toDelete);
-  };
+  void findInstsToDelete(Value *i, std::set<Value *> &toDelete);
+};
 
 } // namespace object_lowering
