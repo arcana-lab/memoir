1273a9a0018df7ca6f069019ca3649e58c83fc9e
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-06-23 17:13:51 -0500

    Runtime: started updating implementation for objects according to new definitions

--- runtime/src/objects.cpp
+++ runtime/src/objects.cpp
@@ -12,23 +12,17 @@ Object::Object(Type *ty) : type(ty) {
   // Resolve the type.
   TypeCode code = this->type->getCode();
   if (code == TypeCode::StubTy) {
-    auto stubType = (StubType *)(this->type);
-    auto resolvedType = stubType->resolve();
-    this->type = resolvedType;
+    auto stub_type = (StubType *)(this->type);
+    auto resolved_type = stub_type->resolve();
+    this->type = resolved_type;
   }
 
   // Create object.
   switch (this->type->getCode()) {
-    case TypeCode::ObjectTy: {
-      // Initialize the fields
-      auto objectType = (ObjectType *)(this->type);
-      for (auto fieldType : objectType->fields) {
-        auto field = Field::createField(fieldType);
-        this->fields.push_back(field);
-      }
+    case TypeCode::StructTy: {
       break;
     }
-    case TypeCode::ArrayTy:
+    case TypeCode::TensorTy:
       // Do nothing.
       break;
     default:
@@ -37,18 +31,30 @@ Object::Object(Type *ty) : type(ty) {
   }
 }
 
-Array::Array(Type *ty, uint64_t length)
-  : Object(ty),
-    length(length) {
-
-  if (this->type->getCode() != TypeCode::ArrayTy) {
-    std::cerr
-        << "Trying to create an array of non-array type\n";
+Struct::Struct(Type *type) : Object(type) {
+  if (this->type->getCode() != TypeCode::StructTy) {
+    std::cerr << "Trying to create a struct of non-struct type\n";
     exit(1);
   }
 
-  Type *elementTy =
-      ((ArrayType *)(this->type))->elementType;
+  // Initialize the fields
+  auto object_type = (StructType *)(this->type);
+  for (auto field_type : objectType->fields) {
+    auto field = Field::createField(field_type);
+    this->fields.push_back(field);
+  }
+}
+
+Tensor::Tensor(Type *ty, std::vector<uint64_t> &length_of_dimensions)
+  : Object(ty),
+    length_of_dimensions(length_of_dimensions) {
+
+  if (this->type->getCode() != TypeCode::TensorTy) {
+    std::cerr << "Trying to create an array of non-array type\n";
+    exit(1);
+  }
+
+  Type *elementTy = ((TensorType *)(this->type))->elementType;
   for (auto i = 0; i < length; i++) {
     auto field = Field::createField(elementTy);
     this->fields.push_back(field);
