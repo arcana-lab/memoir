d94ed8a4d503507454ab526f35dba29233bb5c47
Peter Zhong <zhongyong5@gmail.com>
2022-08-02 15:23:01 -0500

    restore changes

--- compiler/passes/object_lowering/src/ObjectLowering.cpp
+++ compiler/passes/object_lowering/src/ObjectLowering.cpp
@@ -48,20 +48,6 @@ namespace object_lowering {
 
     void ObjectLowering::analyze() {
 
-        for (auto &F : M) {
-            if(F.hasName() && F.getName()=="main")
-            {
-                for(auto &i : instructions(F))
-                {
-                    if(auto callins = dyn_cast<CallInst> (&i)) {
-                        auto &allocana = memoir::AllocationAnalysis::get(M);
-                        auto allocsum = allocana.getAllocationSummary(*callins);
-                        auto &type = allocsum->getType();
-                        errs() << type.toString() << "\n";
-                    }
-                }
-            }
-        }
         // simple testing stuff to make sure it's working
 
 //  auto &allocAna = memoir::AllocationAnalysis::get(M);
