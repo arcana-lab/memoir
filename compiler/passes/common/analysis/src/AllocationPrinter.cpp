0f495983a5339d8265a8b67b8d299fd43233b7cd
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-11-16 11:52:46 -0600

    Compiler: Added printers for associative array and sequence collection summaries

--- compiler/passes/common/analysis/src/AllocationPrinter.cpp
+++ compiler/passes/common/analysis/src/AllocationPrinter.cpp
@@ -51,4 +51,34 @@ std::string TensorAllocationSummary::toString(std::string indent) const {
   return str;
 }
 
+std::string AssocArrayAllocationSummary::toString(std::string indent) const {
+  std::string str, call_str;
+  llvm::raw_string_ostream call_ss(call_str);
+  call_ss << this->getCallInst();
+
+  str = "(associative array\n";
+  str += indent + "LLVM: " + call_ss.str() + "\n";
+  str += indent + "Key Type: \n";
+  str += indent + "  " + this->getKeyType().toString(indent + "  ") + "\n";
+  str += indent + "Value Type: \n";
+  str += indent + "  " + this->getValueType().toString(indent + "  ") + "\n";
+  str += indent + ")\n";
+
+  return str;
+}
+
+std::string SequenceAllocationSummary::toString(std::string indent) const {
+  std::string str, call_str;
+  llvm::raw_string_ostream call_ss(call_str);
+  call_ss << this->getCallInst();
+
+  str = "(sequence\n";
+  str += indent + "LLVM: " + call_ss.str() + "\n";
+  str += indent + "Element Type: \n";
+  str += indent + "  " + this->getElementType().toString(indent + "  ") + "\n";
+  str += indent + ")\n";
+
+  return str;
+}
+
 } // namespace llvm::memoir
