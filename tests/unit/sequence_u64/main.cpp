cdf6d04e2bd3a052d75d53b5e175d4170a8ab56a
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-08 16:28:58 -0600

    Tests: added sequence of u64 test

---
+++ tests/unit/sequence_u64/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "memoir.h"
+
+using namespace memoir;
+
+#define VAL0 10
+#define VAL1 20
+#define VAL2 30
+
+int main() {
+  std::cout << "\nInitializing sequence\n";
+
+  auto seq = memoir_allocate_sequence(memoir_u64_t, 3);
+
+  memoir_write_u64(VAL0, seq, 0);
+  memoir_write_u64(VAL1, seq, 1);
+  memoir_write_u64(VAL2, seq, 2);
+
+  std::cout << "\nReading sequence\n";
+
+  auto read0 = memoir_read_u64(seq, 0);
+  auto read1 = memoir_read_u64(seq, 1);
+  auto read2 = memoir_read_u64(seq, 2);
+
+  std::cout << " Result:\n";
+  std::cout << "  HEAD -> " << std::to_string(read0) << "\n";
+  std::cout << "       -> " << std::to_string(read1) << "\n";
+  std::cout << "       -> " << std::to_string(read2) << "\n\n";
+
+  std::cout << "Expected:\n";
+  std::cout << "  HEAD -> " << std::to_string(VAL0) << "\n";
+  std::cout << "       -> " << std::to_string(VAL1) << "\n";
+  std::cout << "       -> " << std::to_string(VAL2) << "\n\n";
+}
