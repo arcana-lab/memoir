de7a22b70fbac444e755546605953151a31f85e7
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-09 11:07:06 -0600

    Compiler: updated the optimization scripts to better place temporary files alongside the bitcode, updated test's Makefile.template to propagate these changes:

--- compiler/scripts/lower.sh
+++ compiler/scripts/lower.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ] ; then
     echo "This script will lower the bitcode from MemOIR to LLVM IR." ;
-    echo "  USAGE: `basename $0` <IR_FILE> [<OPTIONS, ...>]" ;
+    echo "  USAGE: `basename $0` <INPUT IR FILE> <OUTPUT IR FILE>  [<OPTIONS, ...>]" ;
 fi
 
 GIT_ROOT=`git rev-parse --show-toplevel` ;
@@ -10,11 +10,23 @@ LIB_DIR=${GIT_ROOT}/install/lib ;
 
 source ${GIT_ROOT}/enable ;
 
-IR_FILE="$1" ;
+IR_FILE_IN="$1" ;
+IR_FILE_OUT="$2" ;
 
-OUT_DIR=$(dirname $(realpath ${IR_FILE})) ;
+echo "" ;
+echo "Lowering pipeline (I: ${IR_FILE_IN}, O: ${IR_FILE_OUT})" ;
 
-IR_FILE_LOWERED=${OUT_DIR}/all_in_one_lowered.bc ;
+IR_FILE_BASENAME=$(basename -- ${IR_FILE_IN}) ;
+IR_FILE_FILENAME="${IR_FILE_BASENAME%.*}" ;
+
+OUT_DIR=$(dirname ${IR_FILE_IN}) ;
+
+IR_FILE=${OUT_DIR}/${IR_FILE_FILENAME}_temp.bc ;
+cp ${IR_FILE_IN} ${IR_FILE} ;
+
+IR_FILE_LOWERED=${OUT_DIR}/${IR_FILE_FILENAME}_lowered.bc ;
 echo "Lower Objects (I: ${IR_FILE}, O: ${IR_FILE_LOWERED})" ;
 noelle-load -load ${LIB_DIR}/ObjectLowering.so -ObjectLowering ${IR_FILE} -o ${IR_FILE} ;
 cp ${IR_FILE} ${IR_FILE_LOWERED} ;
+
+cp ${IR_FILE} ${IR_FILE_OUT} ;
