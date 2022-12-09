50b926d809b480c5f2583d78ea6e48ab7968faf2
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-09 11:07:06 -0600

    Compiler: updated the optimization scripts to better place temporary files alongside the bitcode, updated test's Makefile.template to propagate these changes:

--- compiler/scripts/normalize.sh
+++ compiler/scripts/normalize.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ] ; then
     echo "This script will normalize the bitcode for usage in MemOIR and NOELLE passes." ;
-    echo "  USAGE: `basename $0` <IR_FILE> [<OPTIONS, ...>]" ;
+    echo "  USAGE: `basename $0` <INPUT IR FILE> <OUTPUT IR FILE> [<OPTIONS, ...>]" ;
 fi
 
 GIT_ROOT=`git rev-parse --show-toplevel` ;
@@ -10,19 +10,34 @@ LIB_DIR=${GIT_ROOT}/install/lib ;
 
 source ${GIT_ROOT}/enable ;
 
-IR_FILE="$1" ;
+IR_FILE_IN="$1" ;
+IR_FILE_OUT="$2" ;
 
-OUT_DIR=$(dirname $(realpath ${IR_FILE})) ;
+echo "" ;
+echo "Normalization pipeline (I: ${IR_FILE_IN}, O: ${IR_FILE_OUT})" ;
 
-IR_FILE_NORM=${OUT_DIR}/all_in_one_norm.bc ;
-echo "Normalize Bitcode (I: ${IR_FILE}, O: ${IR_FILE_NORM})" ;
+IR_FILE_BASENAME=$(basename -- ${IR_FILE_IN}) ;
+IR_FILE_FILENAME="${IR_FILE_BASENAME%.bc}" ;
+
+OUT_DIR=$(dirname ${IR_FILE_IN}) ;
+
+IR_FILE=${OUT_DIR}/${IR_FILE_FILENAME}_temp.bc ;
+cp ${IR_FILE_IN} ${IR_FILE} ;
+
+IR_FILE_NOELLE_NORM=${OUT_DIR}/${IR_FILE_FILENAME}_noelle_norm.bc ;
+echo "Normalize Bitcode (I: ${IR_FILE}, O: ${IR_FILE_NOELLE_NORM})" ;
 noelle-norm ${IR_FILE} -o ${IR_FILE};
-cp ${IR_FILE} ${IR_FILE_NORM} ;
+cp ${IR_FILE} ${IR_FILE_NOELLE_NORM} ;
 
-PROF_FILE="toProfileBinary" ;
-IR_FILE_PROF=${OUT_DIR}/all_in_one_prof.bc ;
+PROF_FILE="${OUT_DIR}/toProfileBinary" ;
+IR_FILE_PROF=${OUT_DIR}/${IR_FILE_FILENAME}_prof.bc ;
 echo "Profile Bitcode (I: ${IR_FILE}, O: ${IR_FILE_PROF})" ;
 noelle-prof-coverage ${IR_FILE} ${PROF_FILE} -lm -lstdc++ ;
 ./${PROF_FILE} ;
-noelle-meta-prof-embed default.profraw ${IR_FILE} -o ${IR_FILE};
+mv default.profraw ${OUT_DIR}/default.profraw
+noelle-meta-prof-embed ${OUT_DIR}/default.profraw ${IR_FILE} -o ${IR_FILE};
 cp ${IR_FILE} ${IR_FILE_PROF} ;
+
+cp ${IR_FILE} ${IR_FILE_OUT} ;
+
+echo "" ;
--- compiler/scripts/optimize.sh
+++ compiler/scripts/optimize.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ] ; then
     echo "This script will run MemOIR optimizations and lower to LLVM IR." ;
-    echo "  USAGE: `basename $0` <IR_FILE> [<OPTIONS, ...>]" ;
+    echo "  USAGE: `basename $0` <INPUT IR FILE> <OUTPUT IR FILE> [<OPTIONS, ...>]" ;
 fi
 
 GIT_ROOT=`git rev-parse --show-toplevel` ;
@@ -10,12 +10,13 @@ LIB_DIR=${GIT_ROOT}/install/lib ;
 
 source ${GIT_ROOT}/enable ;
 
-IR_FILE="$1" ;
+INPUT_IR_FILE="$1" ;
+OUTPUT_IR_FILE="$2" ;
 
-echo "Running ObjectIR optimization pipeline (I: ${IR_FILE}, O: ${IR_FILE})" ;
+echo "Running MemOIR optimization pipeline (I: ${INPUT_IR_FILE}, O: ${OUTPUT_IR_FILE})" ;
 
 # Normalize the bitcode
-${GIT_ROOT}/compiler/scripts/normalize.sh ${IR_FILE} ;
+${GIT_ROOT}/compiler/scripts/normalize.sh ${INPUT_IR_FILE} ${OUTPUT_IR_FILE} ;
 
 # Lower the bitcode
-${GIT_ROOT}/compiler/scripts/lower.sh ${IR_FILE} ;
+${GIT_ROOT}/compiler/scripts/lower.sh ${INPUT_IR_FILE} ${OUTPUT_IR_FILE} ;
