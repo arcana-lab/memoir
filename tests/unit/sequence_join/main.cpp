58b4d452d010c7c2915b8a1e3bd0bd62731b0551
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-15 10:15:23 -0600

    Tests: updated sequence_join test to new API

--- tests/unit/sequence_join/main.cpp
+++ tests/unit/sequence_join/main.cpp
@@ -16,24 +16,24 @@ int main() {
 
   auto seq0 = memoir_allocate_sequence(memoir_u64_t, 3);
 
-  memoir_write_u64(VAL0, seq0, 0);
-  memoir_write_u64(VAL1, seq0, 1);
-  memoir_write_u64(VAL2, seq0, 2);
+  memoir_index_write(u64, VAL0, seq0, 0);
+  memoir_index_write(u64, VAL1, seq0, 1);
+  memoir_index_write(u64, VAL2, seq0, 2);
 
   auto seq1 = memoir_allocate_sequence(memoir_u64_t, 3);
 
-  memoir_write_u64(VAL3, seq1, 0);
-  memoir_write_u64(VAL4, seq1, 1);
-  memoir_write_u64(VAL5, seq1, 2);
+  memoir_index_write(u64, VAL3, seq1, 0);
+  memoir_index_write(u64, VAL4, seq1, 1);
+  memoir_index_write(u64, VAL5, seq1, 2);
 
   std::cout << "\nReading sequences\n";
 
-  auto read00 = memoir_read_u64(seq0, 0);
-  auto read01 = memoir_read_u64(seq0, 1);
-  auto read02 = memoir_read_u64(seq0, 2);
-  auto read10 = memoir_read_u64(seq1, 0);
-  auto read11 = memoir_read_u64(seq1, 1);
-  auto read12 = memoir_read_u64(seq1, 2);
+  auto read00 = memoir_index_read(u64, seq0, 0);
+  auto read01 = memoir_index_read(u64, seq0, 1);
+  auto read02 = memoir_index_read(u64, seq0, 2);
+  auto read10 = memoir_index_read(u64, seq1, 0);
+  auto read11 = memoir_index_read(u64, seq1, 1);
+  auto read12 = memoir_index_read(u64, seq1, 2);
 
   std::cout << "Result:\n";
   std::cout << " Sequence 1:\n";
@@ -61,12 +61,12 @@ int main() {
 
   std::cout << "\nReading sequence\n";
 
-  auto read0 = memoir_read_u64(join_seq, 0);
-  auto read1 = memoir_read_u64(join_seq, 1);
-  auto read2 = memoir_read_u64(join_seq, 2);
-  auto read3 = memoir_read_u64(join_seq, 3);
-  auto read4 = memoir_read_u64(join_seq, 4);
-  auto read5 = memoir_read_u64(join_seq, 5);
+  auto read0 = memoir_index_read(u64, join_seq, 0);
+  auto read1 = memoir_index_read(u64, join_seq, 1);
+  auto read2 = memoir_index_read(u64, join_seq, 2);
+  auto read3 = memoir_index_read(u64, join_seq, 3);
+  auto read4 = memoir_index_read(u64, join_seq, 4);
+  auto read5 = memoir_index_read(u64, join_seq, 5);
 
   std::cout << " Result:\n";
   std::cout << "  HEAD -> " << std::to_string(read0) << "\n";
