639e89243c61c838dd863ff5338349f925a73822
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-15 10:49:07 -0600

    Tests: updated struct_u64 test to new API

--- tests/unit/struct_u64/main.cpp
+++ tests/unit/struct_u64/main.cpp
@@ -4,34 +4,34 @@
 
 using namespace memoir;
 
-Type *objTy = MEMOIR_FUNC(define_struct_type)("Foo",
-                                              3,
-                                              MEMOIR_FUNC(u64_type)(),
-                                              MEMOIR_FUNC(u64_type)(),
-                                              MEMOIR_FUNC(u64_type)());
+auto objTy = memoir_define_struct_type("Foo",
+                                       3,
+                                       MEMOIR_FUNC(u64_type)(),
+                                       MEMOIR_FUNC(u64_type)(),
+                                       MEMOIR_FUNC(u64_type)());
 
 int main() {
-  Object *myObj = MEMOIR_FUNC(allocate_struct)(objTy);
+  auto myObj = memoir_allocate_struct(objTy);
 
-  MEMOIR_FUNC(write_u64)(123, myObj, 0);
-  MEMOIR_FUNC(write_u64)(456, myObj, 1);
-  MEMOIR_FUNC(write_u64)(789, myObj, 2);
+  memoir_struct_write(u64, 123, myObj, 0);
+  memoir_struct_write(u64, 456, myObj, 1);
+  memoir_struct_write(u64, 789, myObj, 2);
 
-  uint64_t read1 = MEMOIR_FUNC(read_u64)(myObj, 0);
-  uint64_t read2 = MEMOIR_FUNC(read_u64)(myObj, 1);
-  uint64_t read3 = MEMOIR_FUNC(read_u64)(myObj, 2);
+  auto read1 = memoir_struct_read(u64, myObj, 0);
+  auto read2 = memoir_struct_read(u64, myObj, 1);
+  auto read3 = memoir_struct_read(u64, myObj, 2);
 
   std::cerr << "1: " << read1 << "\n";
   std::cerr << "2: " << read2 << "\n";
   std::cerr << "3: " << read3 << "\n\n";
 
-  MEMOIR_FUNC(write_u64)(read1 + read2, myObj, 0);
-  MEMOIR_FUNC(write_u64)(read2 + read3, myObj, 1);
-  MEMOIR_FUNC(write_u64)(read3 + read1, myObj, 2);
+  memoir_struct_write(u64, read1 + read2, myObj, 0);
+  memoir_struct_write(u64, read2 + read3, myObj, 1);
+  memoir_struct_write(u64, read3 + read1, myObj, 2);
 
-  read1 = MEMOIR_FUNC(read_u64)(myObj, 0);
-  read2 = MEMOIR_FUNC(read_u64)(myObj, 1);
-  read3 = MEMOIR_FUNC(read_u64)(myObj, 2);
+  read1 = memoir_struct_read(u64, myObj, 0);
+  read2 = memoir_struct_read(u64, myObj, 1);
+  read3 = memoir_struct_read(u64, myObj, 2);
 
   std::cerr << "1: " << read1 << "\n";
   std::cerr << "2: " << read2 << "\n";
