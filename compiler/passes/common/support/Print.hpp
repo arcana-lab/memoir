3c2e7661aa756a44d00f627612a233a7b38a63be
Tommy McMichen <mcmichen@u.northwestern.edu>
2022-12-19 17:50:49 -0600

    Compiler: added a stub header file to include functions for debug prints

---
+++ compiler/passes/common/support/Print.hpp
@@ -0,0 +1,22 @@
+#ifndef COMMON_PRINT_H
+#define COMMON_PRINT_H
+#pragma once
+
+#include <ostream>
+
+/*
+ * This file provides utility functions for printing.
+ *
+ * Author(s): Tommy McMichen
+ * Created: December 19, 2022
+ */
+
+/*
+ * Default to debug mode.
+ */
+#ifndef MEMOIR_DEBUG
+#  define MEMOIR_DEBUG true
+#endif
+
+namespace llvm::memoir {} // namespace llvm::memoir
+#endif
