c08483ef91745ae0ec8e306417975dee94de0aad
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-01-03 07:54:09 -0600

    Compiler: DeleteInst: fleshed out impl and fixed compiler errors

--- compiler/passes/memoir/ir/src/DeleteInst.cpp
+++ compiler/passes/memoir/ir/src/DeleteInst.cpp
@@ -2,20 +2,43 @@
 
 namespace llvm::memoir {
 
-llvm::Value &DeleteInst::getObjectOperand() const {
-  return this->getObjectOperandAsUse().get();
+Struct &DeleteStructInst::getStructDeleted() const {
+  // TODO: run StructAnalysis.
+  return;
 }
 
-llvm::Use &DeleteInst::getObjectOperandAsUse() const {
+llvm::Value &DeleteStructInst::getStructOperand() const {
+  return *(this->getStructOperandAsUse().get());
+}
+
+llvm::Use &DeleteStructInst::getStructOperandAsUse() const {
   return this->getCallInst().getArgOperandUse(0);
 }
 
-std::string DeleteInst::toString(std::string indent = "") const {
+std::string DeleteStructInst::toString(std::string indent) const {
   std::string str, llvm_str;
   llvm::raw_string_ostream llvm_ss(llvm_str);
   llvm_ss << this->getCallInst();
 
-  str = "DeleteInst: " + llvm_str;
+  str = "DeleteStructInst: " + llvm_str;
+
+  return str;
+}
+
+llvm::Value &DeleteCollectionInst::getCollectionOperand() const {
+  return *(this->getCollectionOperandAsUse().get());
+}
+
+llvm::Use &DeleteCollectionInst::getCollectionOperandAsUse() const {
+  return this->getCallInst().getArgOperandUse(0);
+}
+
+std::string DeleteCollectionInst::toString(std::string indent) const {
+  std::string str, llvm_str;
+  llvm::raw_string_ostream llvm_ss(llvm_str);
+  llvm_ss << this->getCallInst();
+
+  str = "DeleteCollectionInst: " + llvm_str;
 
   return str;
 }
