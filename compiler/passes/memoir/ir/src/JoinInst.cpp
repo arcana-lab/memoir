df8b7e15cabe665b1b520e5b5f2ce3edb4dd09a5
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-01-03 07:51:50 -0600

    Compiler: updated implementation of JoinInst for the updated API

--- compiler/passes/memoir/ir/src/JoinInst.cpp
+++ compiler/passes/memoir/ir/src/JoinInst.cpp
@@ -4,8 +4,9 @@
 
 namespace llvm::memoir {
 
-llvm::Value &JoinInst::getJoinedObject() const {
-  return this->getCallInst();
+Collection &JoinInst::getCollection() const {
+  // TODO: add CollectionAnalysis.
+  return;
 }
 
 unsigned JoinInst::getNumberOfJoins() const {
@@ -20,25 +21,32 @@ unsigned JoinInst::getNumberOfJoins() const {
       (num_joins < 256),
       "Attempt to perform join operation with more than 255 collections.\n"
       "This is unsupported due to llvm::CallInst only accepting 255 arguments.");
+
+  return num_joins;
 }
 
 llvm::Value &JoinInst::getNumberOfJoinsOperand() const {
-  return this->getNumberOfJoinsOperandAsUse().get();
+  return *(this->getNumberOfJoinsOperandAsUse().get());
 }
 
 llvm::Use &JoinInst::getNumberOfJoinsOperandAsUse() const {
   return this->getCallInst().getArgOperandUse(0);
 }
 
-llvm::Value &JoinInst::getObjectJoined(unsigned join_index) const {
+Collection &JoinInst::getCollectionJoined(unsigned join_index) const {
+  // TODO: run CollectionAnalysis
+  return;
+}
+
+llvm::Value &JoinInst::getJoinedOperand(unsigned join_index) const {
   return this->getObjectJoinedAsUse().get();
 }
 
-llvm::Use &JoinInst::getObjectJoinedAsUse(unsigned join_index) const {
+llvm::Use &JoinInst::getJoinedOperandAsUse(unsigned join_index) const {
   return this->getCallInst().getArgOperandUse(1 + join_index);
 }
 
-std::string JoinInst::toString(std::string indent = "") const {
+std::string JoinInst::toString(std::string indent) const {
   std::string str, llvm_str;
   llvm::raw_string_ostream llvm_ss(llvm_str);
   llvm_ss << this->getCallInst();
