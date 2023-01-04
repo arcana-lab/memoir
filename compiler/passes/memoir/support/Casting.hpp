09610fea2b94164dbec2f28f75198345ae8c1368
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-01-04 09:59:25 -0600

    Compiler: Casting: added a common include for memoir casting utilities and llvm casting

---
+++ compiler/passes/memoir/support/Casting.hpp
@@ -0,0 +1,19 @@
+#ifndef MEMOIR_CASTING_H
+#define MEMOIR_CASTING_H
+#pragma once
+
+#include <type_traits>
+
+#include "llvm/Support/Casting.h"
+
+#include "memoir/support/Assert.hpp"
+
+namespace llvm::memoir {
+
+/* TODO
+ * Function to dyn_cast an llvm::Instruction to a MemOIRInst
+ */
+
+} // namespace llvm::memoir
+
+#endif
