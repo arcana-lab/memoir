3dbd3e05caf0a2bb810856ef4745a491bd67a668
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-01-05 13:07:44 -0600

    ReturnTypeInst: added call to TypeAnalysis and fixed compiler errors

--- compiler/passes/memoir/ir/src/ReturnTypeInst.cpp
+++ compiler/passes/memoir/ir/src/ReturnTypeInst.cpp
@@ -1,21 +1,24 @@
 #include "memoir/ir/Instructions.hpp"
 
+#include "memoir/analysis/TypeAnalysis.hpp"
+
 namespace llvm::memoir {
 
 Type &ReturnTypeInst::getType() const {
-  // TODO
-  return;
+  auto type = TypeAnalysis::analyze(this->getTypeOperand());
+  MEMOIR_NULL_CHECK(type, "Could not determine the return type");
+  return *type;
 }
 
 llvm::Value &ReturnTypeInst::getTypeOperand() const {
-  return this->getTypeOperandAsUse().get();
+  return *(this->getTypeOperandAsUse().get());
 }
 
 llvm::Use &ReturnTypeInst::getTypeOperandAsUse() const {
   return this->getCallInst().getArgOperandUse(0);
 }
 
-std::string ReturnTypeInst::toString(std::string indent = "") const {
+std::string ReturnTypeInst::toString(std::string indent) const {
   std::string str, llvm_str;
   llvm::raw_string_ostream llvm_ss(llvm_str);
   llvm_ss << this->getCallInst();
