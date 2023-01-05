6b85f98f7b6e5601fdd0be38824756e92a030614
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-01-05 13:06:14 -0600

    JoinInst: added CollectionAnalysis

--- compiler/passes/memoir/ir/src/JoinInst.cpp
+++ compiler/passes/memoir/ir/src/JoinInst.cpp
@@ -2,16 +2,20 @@
 
 #include "memoir/support/Assert.hpp"
 
+#include "memoir/analysis/CollectionAnalysis.hpp"
+
 namespace llvm::memoir {
 
 Collection &JoinInst::getCollection() const {
-  // TODO: add CollectionAnalysis.
-  return;
+  auto collection = CollectionAnalysis::analyze(this->getCallInst());
+  MEMOIR_NULL_CHECK(collection,
+                    "Could not determine the collection after join");
+  return *collection;
 }
 
 unsigned JoinInst::getNumberOfJoins() const {
-  auto num_joins_value = this->getNumberOfJoinsOperand();
-  auto num_joins_constant = dyn_cast<llvm::ConstantInst>(num_joins_value);
+  auto &num_joins_value = this->getNumberOfJoinsOperand();
+  auto num_joins_constant = dyn_cast<llvm::ConstantInt>(&num_joins_value);
   MEMOIR_NULL_CHECK(
       num_joins_constant,
       "Attempt to perform join operation with a non-static number of collections");
@@ -34,12 +38,15 @@ llvm::Use &JoinInst::getNumberOfJoinsOperandAsUse() const {
 }
 
 Collection &JoinInst::getJoinedCollection(unsigned join_index) const {
-  // TODO: run CollectionAnalysis
-  return;
+  auto collection =
+      CollectionAnalysis::analyze(this->getJoinedOperandAsUse(join_index));
+  MEMOIR_NULL_CHECK(collection,
+                    "Could not determine the collection being joined");
+  return *collection;
 }
 
 llvm::Value &JoinInst::getJoinedOperand(unsigned join_index) const {
-  return this->getObjectJoinedAsUse().get();
+  return *(this->getJoinedOperandAsUse(join_index).get());
 }
 
 llvm::Use &JoinInst::getJoinedOperandAsUse(unsigned join_index) const {
