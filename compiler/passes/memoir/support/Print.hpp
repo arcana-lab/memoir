1229bc54b146a4934fe0595fbf3a74c4ac159b50
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-03-08 09:56:53 -0600

    [Compiler][Support][Print] Added `print` and `println`

--- compiler/passes/memoir/support/Print.hpp
+++ compiler/passes/memoir/support/Print.hpp
@@ -1,8 +1,8 @@
-#ifndef COMMON_PRINT_H
-#define COMMON_PRINT_H
+#ifndef MEMOIR_PRINT_H
+#define MEMOIR_PRINT_H
 #pragma once
 
-#include <ostream>
+#include "llvm/Support/raw_ostream.h"
 
 /*
  * This file provides utility functions for printing.
@@ -10,13 +10,29 @@
  * Author(s): Tommy McMichen
  * Created: December 19, 2022
  */
+namespace llvm::memoir {
 
-/*
- * Default to debug mode.
- */
-#ifndef MEMOIR_DEBUG
-#  define MEMOIR_DEBUG true
-#endif
+inline void fprint(llvm::raw_ostream &out) {}
+template <class T, class... Ts>
+inline void fprint(llvm::raw_ostream &out, T const &first, Ts const &... rest) {
+  out << first;
+  fprint(out, rest...);
+}
 
-namespace llvm::memoir {} // namespace llvm::memoir
+template <class... Ts>
+inline void fprintln(llvm::raw_ostream &out, Ts const &... args) {
+  fprint(out, args..., '\n');
+}
+
+template <class... Ts>
+inline void println(Ts const &... args) {
+  fprintln(llvm::errs(), args...);
+}
+
+template <class... Ts>
+inline void print(Ts const &... args) {
+  fprint(llvm::errs(), args...);
+}
+
+} // namespace llvm::memoir
 #endif
