456e2fdb67f57e54b511cef0478bda2c4d1a98db
Peter Zhong <zhongyong5@gmail.com>
2023-04-27 00:02:55 -0500

    Update main.cpp

--- tests/unit/struct_u64-interprocedural/main.cpp
+++ tests/unit/struct_u64-interprocedural/main.cpp
@@ -7,15 +7,23 @@ using namespace memoir;
 auto objTy =
     memoir_define_struct_type("Foo", memoir_u64_t);
 
+
+void writeStruct(Struct* myObj)
+{
+  memoir_assert_struct_type(objTy, myObj);
+  memoir_struct_write(u64, 123, myObj, 0);
+}
 int64_t readstruct(Struct* myObj)
 {
   memoir_assert_struct_type(objTy, myObj);
   return memoir_struct_read(u64, myObj, 0);
 }
 
+
+
 int main() {
   auto myObj = memoir_allocate_struct(objTy);
-  memoir_struct_write(u64, 123, myObj, 0);
+  writeStruct(myObj);
   int64_t read1 = readstruct(myObj);
   std::cerr << "1: " << read1 << "\n";
   return 0;
