9d62a1fd4f23f0029a5bc5210de2f062cc5748ac
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-07-26 10:17:10 -0500

    [compiler][scripts] Updated branding in

--- compiler/scripts/normalize_runtime.sh
+++ compiler/scripts/normalize_runtime.sh
@@ -1,31 +1,30 @@
 #!/bin/bash -e
 
 if [ $# -lt 1 ] ; then
-    echo "This script will normalize the bitcode for usage in ObjectIR and NOELLE passes." ;
-    echo "  USAGE: `basename $0` <IR_FILE> [<OPTIONS, ...>]" ;
+    echo "This script will normalize the MemOIR bitcode to generate the declaration file."
+    echo "  USAGE: `basename $0` <INPUT_IR_FILE> <OUTPUT_IR_FILE> [<OPTIONS, ...>]"
 fi
 
-GIT_ROOT=`git rev-parse --show-toplevel` ;
-LIB_DIR=${GIT_ROOT}/install/lib ;
+GIT_ROOT=`git rev-parse --show-toplevel`
+LIB_DIR=${GIT_ROOT}/install/lib
 
-source ${GIT_ROOT}/enable ;
+source ${GIT_ROOT}/enable
 
-IN_IR_FILE="$1" ;
-TMP_IR_FILE="temp.bc" ;
-OUT_IR_FILE="$2" ;
+IN_IR_FILE="$1"
+OUT_IR_FILE="$2"
 
-cp ${IN_IR_FILE} ${TMP_IR_FILE} ;
+TMP_IR_FILE="norm.temp.bc"
 
-echo "Normalize Runtime (I: ${IN_IR_FILE}, O: ${OUT_IR_FILE})" ;
+cp ${IN_IR_FILE} ${TMP_IR_FILE}
 
-CMD="llvm-extract -glob llvm.used -rfunc memoir__* ${TMP_IR_FILE} -o ${TMP_IR_FILE}" ;
-echo "$CMD" ;
-eval $CMD ;
+echo "Normalize Runtime (I: ${IN_IR_FILE}, O: ${OUT_IR_FILE})"
 
-CMD="opt -strip -load ${LIB_DIR}/Normalization.so -NormalizationPass -only-runtime ${TMP_IR_FILE} -o ${OUT_IR_FILE}" ;
-echo "$CMD" ;
-eval $CMD ;
+CMD="llvm-extract -glob llvm.used -rfunc memoir__* ${TMP_IR_FILE} -o ${TMP_IR_FILE}"
+echo "$CMD"
+eval $CMD
 
+CMD="opt -strip -load ${LIB_DIR}/Normalization.so -memoir-norm -only-runtime ${TMP_IR_FILE} -o ${OUT_IR_FILE}"
+echo "$CMD"
+eval $CMD
 
-
-# rm ${TMP_IR_FILE} ;
+rm ${TMP_IR_FILE} ;
