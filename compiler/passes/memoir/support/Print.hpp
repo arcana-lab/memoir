1ea55867e1a3c40846bc6905f94077ef20eb79fe
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-08-11 10:56:08 -0500

    [compiler][support] Added warn and warnln alternatives to printing

--- compiler/passes/memoir/support/Print.hpp
+++ compiler/passes/memoir/support/Print.hpp
@@ -12,6 +12,7 @@
  */
 namespace llvm::memoir {
 
+// Printing.
 inline void fprint(llvm::raw_ostream &out) {}
 template <class T, class... Ts>
 inline void fprint(llvm::raw_ostream &out, T const &first, Ts const &... rest) {
@@ -32,6 +33,37 @@ inline void println(Ts const &... args) {
 template <class... Ts>
 inline void print(Ts const &... args) {
   fprint(llvm::errs(), args...);
+}
+
+// Warnings.
+template <class... Ts>
+inline void fwarn(llvm::raw_ostream &out, Ts const &... args) {
+  if (out.has_colors()) {
+    out.changeColor(llvm::raw_ostream::Colors::YELLOW, /*bold=*/true);
+  }
+
+  fprint(out, "WARNING: ");
+
+  if (out.has_colors()) {
+    out.resetColor();
+  }
+
+  fprint(out, args...);
+}
+
+template <class... Ts>
+inline void fwarnln(llvm::raw_ostream &out, Ts const &... args) {
+  fwarn(out, args..., '\n');
+}
+
+template <class... Ts>
+inline void warnln(Ts const &... args) {
+  fwarnln(llvm::errs(), args...);
+}
+
+template <class... Ts>
+inline void warn(Ts const &... args) {
+  fwarn(llvm::errs(), args...);
 }
 
 } // namespace llvm::memoir
