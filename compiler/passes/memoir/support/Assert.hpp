d4056232adf6ed3cbbc7bb4b8f1b4480e557d0ba
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-08-17 10:37:23 -0500

    [compiler][support] Added MEMOIR_SANITIZE, which does the same thing as sanitize, but uses the function and line number of where the macro is invoked

--- compiler/passes/memoir/support/Assert.hpp
+++ compiler/passes/memoir/support/Assert.hpp
@@ -7,28 +7,36 @@
 #include <string>
 #include <type_traits>
 
-#define MEMOIR_ASSERT(c, msg)                                                  \
+#define _MEMOIR_ASSERT(c, msg, pretty_func, pretty_line)                       \
   if (!(c)) {                                                                  \
     fprintf(stderr, "\n");                                                     \
     fprintf(stderr, "\x1b[31m====================================\n");         \
     fprintf(stderr, "\x1b[1;31mMemOIR Assert Failed!\x1b[0m\n");               \
     fprintf(stderr, "  \x1b[1;33m%s\x1b[0m\n", msg);                           \
-    fprintf(stderr, "  in \x1b[1;35m%s\x1b[0m\n", __PRETTY_FUNCTION__);        \
-    fprintf(stderr, "  at \x0b[1;35m%s\x1b[0m\n", __LINE__);                   \
+    fprintf(stderr, "  in \x1b[1;35m%s\x1b[0m\n", pretty_func);                \
+    fprintf(stderr, "  at \x0b[1;35m%d\x1b[0m\n", pretty_line);                \
     fprintf(stderr, "\x1b[31m====================================\n\n");       \
     assert(c);                                                                 \
   }
+
+#define MEMOIR_ASSERT(c, msg)                                                  \
+  _MEMOIR_ASSERT(c, msg, __PRETTY_FUNCTION__, __LINE__)
 
 #define MEMOIR_UNREACHABLE(msg) MEMOIR_ASSERT(false, msg)
 
 #define MEMOIR_NULL_CHECK(v, msg) MEMOIR_ASSERT((v != nullptr), msg)
 
+#define MEMOIR_SANITIZE(v, msg) sanitize(v, msg, __PRETTY_FUNCTION__, __LINE__)
+
 template <typename T>
 inline typename std::enable_if_t<
     std::is_pointer_v<T>,
     std::add_lvalue_reference_t<std::remove_pointer_t<T>>>
-sanitize(T t, const char *message) {
-  MEMOIR_NULL_CHECK(t, message);
+sanitize(T t,
+         const char *message,
+         const char *pretty_func = "",
+         int pretty_line = 0) {
+  _MEMOIR_ASSERT((t != nullptr), message, pretty_func, pretty_line);
   return *t;
 }
 
