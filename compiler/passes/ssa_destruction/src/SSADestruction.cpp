a9dcdd30dffc5b399a32058f6b9e20aefa153812
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-08-17 17:58:07 -0500

    [compiler][ssa-destruct] Added support for lowering to swap/remove on the same collection

--- compiler/passes/ssa_destruction/src/SSADestruction.cpp
+++ compiler/passes/ssa_destruction/src/SSADestruction.cpp
@@ -205,7 +205,9 @@ void SSADestructionVisitor::visitJoinInst(JoinInst &I) {
   // join with it.
   bool all_dead = true;
   for (auto join_idx = 0; join_idx < num_joined; join_idx++) {
-    auto &joined_collection = I.getJoinedOperand(join_idx);
+    auto &joined_use = I.getJoinedOperandAsUse(join_idx);
+    auto &joined_collection =
+        MEMOIR_SANITIZE(joined_use.get(), "Use by join is NULL!");
     if (this->LA.is_live(joined_collection, I)) {
       all_dead = false;
 
@@ -217,33 +219,285 @@ void SSADestructionVisitor::visitJoinInst(JoinInst &I) {
     }
   }
 
-  // If all join operands are dead, convert the join to a series of appends.
-  if (all_dead) {
-    auto &first_collection = I.getJoinedOperand(0);
-    MemOIRBuilder builder(I);
-    for (auto join_idx = 1; join_idx < num_joined; join_idx++) {
-      auto &joined_collection = I.getJoinedOperand(join_idx);
-      debugln("Creating append");
-      debugln("  ", first_collection);
-      debugln("  ", joined_collection);
-
-      builder.CreateSeqAppendInst(&first_collection, &joined_collection);
-    }
-
-    // Coalesce the first operand and the join.
-    this->coalesce(I, first_collection);
-
-    // The join is dead after coalescence.
-    this->markForCleanup(I);
-
+  if (!all_dead) {
+    infoln("Not all incoming values are dead after a join.");
+    infoln(" |-> ", I);
     return;
   }
+
+  // If all join operands are dead, convert the join to a series of appends.
+  // if (all_dead) {
+  //   auto &first_collection = I.getJoinedOperand(0);
+  //   MemOIRBuilder builder(I);
+  //   for (auto join_idx = 1; join_idx < num_joined; join_idx++) {
+  //     auto &joined_collection = I.getJoinedOperand(join_idx);
+  //     debugln("Creating append");
+  //     debugln("  ", first_collection);
+  //     debugln("  ", joined_collection);
+
+  //     builder.CreateSeqAppendInst(&first_collection, &joined_collection);
+  //   }
+
+  //   // Coalesce the first operand and the join.
+  //   this->coalesce(I, first_collection);
+
+  //   // The join is dead after coalescence.
+  //   this->markForCleanup(I);
+
+  //   return;
+  // }
 
   // If all operands of the join are views of the same collection:
   //  - If views are in order, coallesce resultant and the viewed collection.
   //  - Otherwise, determine if the size is the same after the join:
   //     - If the size is the same, convert to a swap.
   //     - Otherwise, convert to a remove.
+  vector<ViewInst *> views = {};
+  views.reserve(num_joined);
+  bool all_views = true;
+  llvm::Value *base_collection = nullptr;
+  for (auto join_idx = 0; join_idx < num_joined; join_idx++) {
+    auto &joined_use = I.getJoinedOperandAsUse(join_idx);
+    auto &joined_collection =
+        MEMOIR_SANITIZE(joined_use.get(), "Use by join is NULL!");
+
+    auto *joined_as_inst = dyn_cast<llvm::Instruction>(&joined_collection);
+    if (!joined_as_inst) {
+      base_collection = nullptr;
+      break;
+    }
+
+    auto *joined_collection_as_memoir = MemOIRInst::get(*joined_as_inst);
+    llvm::Value *used_collection;
+    if (auto *view_inst =
+            dyn_cast_or_null<ViewInst>(joined_collection_as_memoir)) {
+      auto &viewed_collection = view_inst->getCollectionOperand();
+      used_collection = &viewed_collection;
+      views.push_back(view_inst);
+    } else {
+      infoln("Join uses non-view");
+      infoln(" |-> ", I);
+      all_views = false;
+      continue;
+    }
+
+    // See if we are still using the same base collection.
+    if (base_collection == nullptr) {
+      base_collection = used_collection;
+      continue;
+    } else if (base_collection == used_collection) {
+      continue;
+    } else {
+      base_collection = nullptr;
+      break;
+    }
+  }
+
+  if (base_collection != nullptr && all_views) {
+    println("Join uses a single base collection");
+    println(" |-> ", I);
+
+    // Determine if the size is the same.
+    set<ViewInst *> visited = {};
+    list<size_t> view_order = {};
+    list<pair<llvm::Value *, llvm::Value *>> ranges_to_remove = {};
+    llvm::Value *lower_limit = nullptr;
+    llvm::Value *upper_limit = nullptr;
+    bool swappable = true;
+    while (visited.size() != views.size()) {
+      bool new_limit = false;
+      for (auto view_idx = 0; view_idx < views.size(); view_idx++) {
+        auto *view = views[view_idx];
+        auto &begin_index = view->getBeginIndex();
+        auto &end_index = view->getEndIndex();
+        if (lower_limit == nullptr) {
+          lower_limit = &begin_index;
+          upper_limit = &end_index;
+          visited.insert(view);
+          view_order.push_back(view_idx);
+          new_limit = true;
+          continue;
+        } else if (lower_limit == &end_index) {
+          lower_limit = &begin_index;
+          visited.insert(view);
+          view_order.push_front(view_idx);
+          new_limit = true;
+          continue;
+        } else if (upper_limit == &begin_index) {
+          upper_limit = &end_index;
+          visited.insert(view);
+          view_order.push_back(view_idx);
+          new_limit = true;
+          continue;
+        }
+      }
+
+      if (new_limit) {
+        continue;
+      }
+
+      // Determine the ranges that must be removed.
+      auto *lower_limit_expr = this->VN.get(*lower_limit);
+      auto *upper_limit_expr = this->VN.get(*upper_limit);
+      ValueExpression *new_lower_limit_expr = nullptr;
+      ViewInst *lower_limit_view = nullptr;
+      ValueExpression *new_upper_limit_expr = nullptr;
+      ViewInst *upper_limit_view = nullptr;
+      for (auto view_idx = 0; view_idx < views.size(); view_idx++) {
+        auto *view = views[view_idx];
+        if (visited.find(view) != visited.end()) {
+          continue;
+        }
+
+        // Find the nearest
+        auto &view_begin_expr =
+            MEMOIR_SANITIZE(this->VN.get(view->getBeginIndex()),
+                            "Error making value expression for begin index");
+        auto &view_end_expr =
+            MEMOIR_SANITIZE(this->VN.get(view->getEndIndex()),
+                            "Error making value expression for end index");
+
+        auto check_left = view_end_expr < *lower_limit_expr;
+        auto check_right = view_begin_expr > *upper_limit_expr;
+
+        if (check_left) {
+          // Check if this is the new lower bound.
+          if (new_lower_limit_expr == nullptr) {
+            new_lower_limit_expr = &view_end_expr;
+            lower_limit_view = view;
+            new_limit = true;
+          } else if (view_end_expr < *new_lower_limit_expr) {
+            new_lower_limit_expr = &view_end_expr;
+            lower_limit_view = view;
+            new_limit = true;
+          }
+          continue;
+        } else if (check_right) {
+          // Check if this is the new upper bound.
+          if (new_upper_limit_expr == nullptr) {
+            new_upper_limit_expr = &view_begin_expr;
+            upper_limit_view = view;
+            new_limit = true;
+          } else if (view_begin_expr > *new_upper_limit_expr) {
+            new_upper_limit_expr = &view_begin_expr;
+            upper_limit_view = view;
+            new_limit = true;
+          }
+          continue;
+        }
+      }
+
+      // If we found a new limit, update the lower and/or upper limit and mark
+      // the range for removal.
+      if (new_limit) {
+        if (lower_limit_view != nullptr) {
+          auto &new_lower_limit = lower_limit_view->getEndIndex();
+          ranges_to_remove.push_front(make_pair(&new_lower_limit, lower_limit));
+          lower_limit = &new_lower_limit;
+        }
+
+        if (upper_limit_view != nullptr) {
+          auto &new_upper_limit = upper_limit_view->getBeginIndex();
+          ranges_to_remove.push_back(make_pair(upper_limit, &new_upper_limit));
+          upper_limit = &new_upper_limit;
+        }
+
+        // Keep on chugging.
+        continue;
+      }
+
+      println("!!!!");
+      println("The join: ", I);
+      MEMOIR_UNREACHABLE(
+          "We shouldn't be here, couldnt' find out what to swap/remove forthe above join!");
+    }
+
+    if (swappable) {
+      infoln("Convert the join to a swap.");
+      infoln(" |-> ", I);
+      infoln(" |-> Original order:");
+      for (auto view_idx : view_order) {
+        infoln(" | |-> ", *views[view_idx]);
+      }
+      infoln(" |-> New order:");
+      for (auto view : views) {
+        infoln(" | |-> ", *view);
+      }
+
+      auto new_idx = 0;
+      auto i = 0;
+      for (auto it = view_order.begin(); it != view_order.end(); ++it, i++) {
+        auto orig_idx = *it;
+        if (orig_idx != new_idx) {
+          println(" |-> Swap ", orig_idx, " <=> ", new_idx);
+
+          // Check that these views are the same size.
+          auto &from_begin = views[orig_idx]->getBeginIndex();
+          auto *from_begin_expr = this->VN.get(from_begin);
+          auto &from_end = views[orig_idx]->getEndIndex();
+          auto *from_end_expr = this->VN.get(from_end);
+          auto &to_begin = views[new_idx]->getBeginIndex();
+          auto *to_begin_expr = this->VN.get(to_begin);
+          auto &to_end = views[new_idx]->getEndIndex();
+          auto *to_end_expr = this->VN.get(to_end);
+
+          // Create the expression for the size calculation.
+          auto *from_size_expr = new BasicExpression(
+              llvm::Instruction::Sub,
+              vector<ValueExpression *>({ from_end_expr, from_begin_expr }));
+          auto *to_size_expr = new BasicExpression(
+              llvm::Instruction::Sub,
+              vector<ValueExpression *>({ to_end_expr, to_begin_expr }));
+
+          // If they are the same size, swap them.
+          if (*from_size_expr == *to_size_expr) {
+            // Create the swap.
+            MemOIRBuilder builder(I);
+            builder.CreateSeqSwapInst(base_collection,
+                                      &from_begin,
+                                      &from_end,
+                                      &to_begin);
+
+            // Swap the operands.
+            auto &from_use = I.getJoinedOperandAsUse(orig_idx);
+            auto &to_use = I.getJoinedOperandAsUse(new_idx);
+            from_use.swap(to_use);
+
+            // NOTE: we don't need to update other users here, as these views
+            // were already checked to be dead following.
+
+            // Update the view order post-swap.
+            auto r = view_order.size() - 1;
+            for (auto rit = view_order.rbegin(); r > i; ++rit, r--) {
+              if (*rit == new_idx) {
+                std::swap(*(views.begin() + i), *(views.begin() + r));
+                std::swap(*it, *rit);
+                break;
+              }
+            }
+          } else {
+            MEMOIR_UNREACHABLE("Size of from and to for swap are not the same! "
+                               "Someone must have changed the program.");
+          }
+        }
+        new_idx++;
+      }
+
+      // Replace the join with the base collection that has been swapped.
+      this->coalesce(I, *base_collection);
+      this->markForCleanup(I);
+    }
+
+    // Now to remove any ranges that were marked.
+    for (auto range : ranges_to_remove) {
+      auto *begin = range.first;
+      auto *end = range.second;
+      MemOIRBuilder builder(I);
+      // TODO: check if this range is alive as a view, if it is we need to
+      // convert it to a slice.
+      builder.CreateSeqRemoveInst(base_collection, begin, end);
+    }
+  }
 
   // Otherwise, some operands are views from a different collection:
   //  - If the size is the same, convert to a swap.
