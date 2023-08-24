96dcc44744fb060decb1aae119bf3dc078e85811
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-08-23 21:05:55 -0500

    [runtime][api][c] Updated mstring.h to act more like a library

--- runtime/api/c/mstring.h
+++ runtime/api/c/mstring.h
@@ -1,4 +1,10 @@
+#ifndef MEMOIR_API_MSTRING_H
+#define MEMOIR_API_MSTRING_H
+#pragma once
+
 #include "cmemoir.h"
+
+using namespace memoir;
 
 #define memoir_string_t memoir_sequence_type(memoir_u8_t)
 
@@ -7,115 +13,51 @@
 //   memoir_strcmp(str1, begin1, str2, begin2)
 //   -- or --
 //   memoir_strcmp(str, begin1, begin2)
-#define memoir_strcmp(str, a, b, ...)                                          \
-  memoir_strcmp_(str, a, ##__VA_ARGS__, 4, 3, 2)
+#define memoir_strcmp(a, b, ...) memoir_strcmp_(a, b, ##__VA_ARGS__, 4, 3, 2)
 #define memoir_strcmp_(a, b, c, d, n, ...) memoir_strcmp##n(a, b, c, d)
 #define memoir_strcmp4(str, a, str2, b) memoir_strcmp__diff(str, a, str2, b)
 #define memoir_strcmp3(str, a, b, ...) memoir_strcmp__same(str, a, b)
 #define memoir_strcmp2(str, str2, ...) memoir_strcmp__start(str, str2)
 
-int memoir_strcmp__start(Collection *str, Collection *str2) {
-  memoir_assert_collection_type(memoir_string_t, str);
+#define memoir_strtok(str, delims, /*size_t*/ last, /*size_t*/ next)           \
+  do {                                                                         \
+    /* Scan leading delimiters */                                              \
+    last += memoir_strspn(str, delims, last);                                  \
+    if (memoir_index_read(u8, str, last) == '\0') {                            \
+      last = 0;                                                                \
+      break;                                                                   \
+    }                                                                          \
+                                                                               \
+    /* Find the end of the token */                                            \
+    next = last;                                                               \
+    last = memoir_strpbrk(str, delims, next);                                  \
+    if (last != 0) {                                                           \
+      /* Terminate the token */                                                \
+      memoir_index_write(u8, '\0', str, last++);                               \
+    }                                                                          \
+  } while (0)
 
-  auto a = 0;
-  auto b = 0;
+extern int memoir_strcmp__start(Collection *str, Collection *str2);
 
-  auto ac = memoir_index_read(u8, str, a);
-  auto bc = memoir_index_read(u8, str2, b);
-  while (ac != '\0' && (ac == bc)) {
-    ac = memoir_index_read(u8, str, a++);
-    bc = memoir_index_read(u8, str2, b++);
-  }
+extern int memoir_strcmp__same(Collection *str, size_t a, size_t b);
 
-  return (int)(a - b);
-}
+extern int memoir_strcmp__diff(Collection *str,
+                               size_t a,
+                               Collection *str2,
+                               size_t b);
 
-int memoir_strcmp__same(Collection *str, size_t a, size_t b) {
-  memoir_assert_collection_type(memoir_string_t, str);
+extern size_t memoir_strlen(Collection *str, size_t start = 0);
 
-  auto ac = memoir_index_read(u8, str, a);
-  auto bc = memoir_index_read(u8, str, b);
-  while (ac != '\0' && (ac == bc)) {
-    ac = memoir_index_read(u8, str, a++);
-    bc = memoir_index_read(u8, str, b++);
-  }
+/* extern size_t memoir_strtok(Collection *str, */
+/*                             const char *delimiters, */
+/*                             size_t last = 0); */
 
-  return (int)(a - b);
-}
+extern size_t memoir_strspn(Collection *str,
+                            const char *accept,
+                            size_t start = 0);
 
-int memoir_strcmp__diff(Collection *str, size_t a, Collection *str2, size_t b) {
-  memoir_assert_collection_type(memoir_string_t, str);
-  memoir_assert_collection_type(memoir_string_t, str2);
+extern size_t memoir_strpbrk(Collection *str,
+                             const char *accept,
+                             size_t start = 0);
 
-  auto ac = memoir_index_read(u8, str, a);
-  auto bc = memoir_index_read(u8, str2, b);
-  while (ac != '\0' && (ac == bc)) {
-    ac = memoir_index_read(u8, str, a++);
-    bc = memoir_index_read(u8, str2, b++);
-  }
-  return (int)(a - b);
-}
-
-size_t memoir_strtok(Collection *str, const char *delimiters, size_t last = 0) {
-
-  /* Instead of saving this value as a global variable, we will handle the wrap
-   * case  explicitly*/
-  if (last != 0) {
-    last++;
-  }
-
-  /* Scan leading delimiters */
-  last += strspn(str, delimiters);
-  if (memoir_index_read(u8, str, last) == '\0') {
-    return 0;
-  }
-
-  /* Find the end of the token */
-  size_t token = last;
-  last = memoir_strpbrk(str, delimiters, token);
-  if (last != 0) {
-    /* Terminate the token */
-    memoir_index_write(u8, '\0', str, last);
-  }
-  return token;
-}
-
-size_t memoir_strspn(Collection *str, const char *accept, size_t start = 0) {
-  size_t count = 0;
-
-  size_t p = start;
-  auto pc = memoir_index_read(u8, str, p);
-  for (; pc != '\0';) {
-    for (const char *a = accept; *a != '\0'; ++a) {
-      if (pc == *a) {
-        break;
-      }
-      if (*a == '\0') {
-        return count;
-      } else {
-        ++count;
-      }
-    }
-    pc = memoir_index_read(u8, str, ++p);
-  }
-
-  return count;
-}
-
-size_t memoir_strpbrk(Collection *str, const char *accept, size_t start = 0) {
-  size_t p = start;
-
-  auto pc = memoir_index_read(u8, str, p);
-  while (pc != '\0') {
-    const char *a = accept;
-    while (*a != '\0') {
-      if (*a++ == pc) {
-        return p;
-      }
-      ++p;
-    }
-    pc = memoir_index_read(u8, str, p);
-  }
-
-  return 0;
-}
+#endif
