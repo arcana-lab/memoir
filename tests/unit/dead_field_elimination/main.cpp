35d3949ae45eb2fa84882ed157a87e804c331d8e
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-08-25 11:54:55 -0500

    [tests][unit] added escape analysis testing to dfe test

--- tests/unit/dead_field_elimination/main.cpp
+++ tests/unit/dead_field_elimination/main.cpp
@@ -8,6 +8,12 @@ using namespace memoir;
 auto objTy =
     memoir_define_struct_type("Foo", memoir_u64_t, memoir_u64_t, memoir_u64_t);
 
+// type escapes, should not be freed.
+auto escaped_type =
+    memoir_define_struct_type("Esc", memoir_u64_t, memoir_u64_t, memoir_u64_t);
+
+extern void escape_hatch(Struct *);
+
 int main() {
   auto myObj = memoir_allocate_struct(objTy);
 
@@ -28,6 +34,11 @@ int main() {
 
   printf(".1= %lu\n", read1);
   printf(".3= %lu\n\n", read3);
+
+  auto escapee = memoir_allocate_struct(escaped_type);
+  memoir_struct_write(u64, 123, escapee, 1);
+  memoir_struct_write(u64, 123, escapee, 2);
+  escape_hatch(escapee);
 
   return 0;
 }
