6c309e0de664e2cbe371adef64870ffd7d73523e
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-11-06 09:03:41 -0600

    [runtime][backend] Added u64 target for stl_vector and llvm_smallvector

--- runtime/backend/llvm_smallvector/impl.cc
+++ runtime/backend/llvm_smallvector/impl.cc
@@ -1,3 +1,5 @@
 #include "llvm_smallvector.h"
 
+INSTANTIATE_llvm_smallvector(u64, uint64_t)
+
 INSTANTIATE_llvm_smallvector(ptr, void *)
--- runtime/backend/stl_vector/impl.cc
+++ runtime/backend/stl_vector/impl.cc
@@ -1,6 +1,7 @@
 #include "stl_vector.h"
 
 INSTANTIATE_stl_vector(ptr, void *)
+INSTANTIATE_stl_vector(u64, uint64_t)
 
 #pragma pack(1)
     typedef struct {
