a6250051f44a585eec64291d6ea4e3c2d9bb587f
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-11-06 14:09:13 -0600

    [compiler] Removed old/unimplemented passes

--- compiler/passes/object_abstraction/src/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils.hpp"
-
-using namespace object_abstraction;
-
-bool object_abstraction::isObjectIRCall(
-    std::string functionName) {
-  return FunctionNamesToObjectIR.count(functionName) != 0;
-}
--- compiler/passes/object_inlining/src/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils.hpp"
-
-using namespace object_inlining;
-
-bool object_inlining::isObjectIRCall(
-    std::string functionName) {
-  return false;
-}
