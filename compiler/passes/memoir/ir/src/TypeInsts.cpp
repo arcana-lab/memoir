671fd88e5526212b6debcdd5ec29ad624a50b8b7
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-11-09 12:50:19 -0600

    [compiler][ir] Updated implementation of TypeInsts to use InstructionUtils.hpp

--- compiler/passes/memoir/ir/src/TypeInsts.cpp
+++ compiler/passes/memoir/ir/src/TypeInsts.cpp
@@ -4,6 +4,8 @@
 
 #include "memoir/analysis/TypeAnalysis.hpp"
 
+#include "memoir/support/InstructionUtils.hpp"
+
 /*
  * Implementation of the MemOIR Type Instructions.
  *
@@ -23,96 +25,87 @@ namespace llvm::memoir {
     return *type;                                                              \
   }
 
-#define TO_STRING_IMPL(CLASS_NAME)                                             \
-  std::string CLASS_NAME::toString(std::string indent) const {                 \
-    std::string str, llvm_str;                                                 \
-    llvm::raw_string_ostream llvm_ss(llvm_str);                                \
-    llvm_ss << this->getCallInst();                                            \
-    str = #CLASS_NAME ": " + llvm_str;                                         \
-    return str;                                                                \
-  }
-
 /*
  * UInt64TypeInst implementation
  */
 GET_TYPE_IMPL(UInt64TypeInst)
-TO_STRING_IMPL(UInt64TypeInst)
+TO_STRING(UInt64TypeInst)
 
 /*
  * UInt32TypeInst implementation
  */
 GET_TYPE_IMPL(UInt32TypeInst)
-TO_STRING_IMPL(UInt32TypeInst)
+TO_STRING(UInt32TypeInst)
 
 /*
  * UInt16TypeInst implementation
  */
 GET_TYPE_IMPL(UInt16TypeInst)
-TO_STRING_IMPL(UInt16TypeInst)
+TO_STRING(UInt16TypeInst)
 
 /*
  * UInt8TypeInst implementation
  */
 GET_TYPE_IMPL(UInt8TypeInst)
-TO_STRING_IMPL(UInt8TypeInst)
+TO_STRING(UInt8TypeInst)
 
 // UInt8TypeInst implementation
 GET_TYPE_IMPL(UInt2TypeInst)
-TO_STRING_IMPL(UInt2TypeInst)
+TO_STRING(UInt2TypeInst)
 
 /*
  * Int64TypeInst implementation
  */
 GET_TYPE_IMPL(Int64TypeInst)
-TO_STRING_IMPL(Int64TypeInst)
+TO_STRING(Int64TypeInst)
 
 /*
  * Int32TypeInst implementation
  */
 GET_TYPE_IMPL(Int32TypeInst)
-TO_STRING_IMPL(Int32TypeInst)
+TO_STRING(Int32TypeInst)
 
 /*
  * Int16TypeInst implementation
  */
 GET_TYPE_IMPL(Int16TypeInst)
-TO_STRING_IMPL(Int16TypeInst)
+TO_STRING(Int16TypeInst)
 
 /*
  * Int8TypeInst implementation
  */
 GET_TYPE_IMPL(Int8TypeInst)
-TO_STRING_IMPL(Int8TypeInst)
+TO_STRING(Int8TypeInst)
 
 /*
  * Int2TypeInst implementation
  */
 GET_TYPE_IMPL(Int2TypeInst)
-TO_STRING_IMPL(Int2TypeInst)
+TO_STRING(Int2TypeInst)
 
 /*
  * BoolTypeInst implementation
  */
 GET_TYPE_IMPL(BoolTypeInst)
-TO_STRING_IMPL(BoolTypeInst)
+TO_STRING(BoolTypeInst)
 
 /*
  * FloatType implementation
  */
 GET_TYPE_IMPL(FloatTypeInst)
-TO_STRING_IMPL(FloatTypeInst)
+TO_STRING(FloatTypeInst)
 
 /*
  * DoubleType implementation
  */
 GET_TYPE_IMPL(DoubleTypeInst)
-TO_STRING_IMPL(DoubleTypeInst)
+TO_STRING(DoubleTypeInst)
 
 /*
  * PointerType implementation
  */
 GET_TYPE_IMPL(PointerTypeInst)
-TO_STRING_IMPL(PointerTypeInst)
+TO_STRING(PointerTypeInst)
 
 /*
  * ReferenceType implementation
@@ -127,15 +120,9 @@ Type &ReferenceTypeInst::getReferencedType() const {
   return *type;
 }
 
-llvm::Value &ReferenceTypeInst::getReferencedTypeOperand() const {
-  return *(this->getReferencedTypeAsUse().get());
-}
+OPERAND(ReferenceTypeInst, ReferencedTypeOperand, 0)
 
-llvm::Use &ReferenceTypeInst::getReferencedTypeAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
-
-TO_STRING_IMPL(ReferenceTypeInst)
+TO_STRING(ReferenceTypeInst)
 
 /*
  * DefineStructType implementation
@@ -168,13 +155,7 @@ std::string DefineStructTypeInst::getName() const {
   return name_constant->getAsCString();
 }
 
-llvm::Value &DefineStructTypeInst::getNameOperand() const {
-  return *(this->getNameOperandAsUse().get());
-}
-
-llvm::Use &DefineStructTypeInst::getNameOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
+OPERAND(DefineStructTypeInst, NameOperand, 0)
 
 unsigned DefineStructTypeInst::getNumberOfFields() const {
   auto &num_fields_value = this->getNumberOfFieldsOperand();
@@ -191,13 +172,7 @@ unsigned DefineStructTypeInst::getNumberOfFields() const {
   return num_fields;
 }
 
-llvm::Value &DefineStructTypeInst::getNumberOfFieldsOperand() const {
-  return *(this->getNumberOfFieldsOperandAsUse().get());
-}
-
-llvm::Use &DefineStructTypeInst::getNumberOfFieldsOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(1);
-}
+OPERAND(DefineStructTypeInst, NumberOfFieldsOperand, 1)
 
 Type &DefineStructTypeInst::getFieldType(unsigned field_index) const {
   auto type = TypeAnalysis::analyze(this->getFieldTypeOperand(field_index));
@@ -206,17 +181,9 @@ Type &DefineStructTypeInst::getFieldType(unsigned field_index) const {
   return *type;
 }
 
-llvm::Value &DefineStructTypeInst::getFieldTypeOperand(
-    unsigned field_index) const {
-  return *(this->getFieldTypeOperandAsUse(field_index).get());
-}
+VAR_OPERAND(DefineStructTypeInst, FieldTypeOperand, 2)
 
-llvm::Use &DefineStructTypeInst::getFieldTypeOperandAsUse(
-    unsigned field_index) const {
-  return this->getCallInst().getArgOperandUse(2 + field_index);
-}
-
-TO_STRING_IMPL(DefineStructTypeInst)
+TO_STRING(DefineStructTypeInst)
 
 /*
  * StructType implementation
@@ -245,15 +212,9 @@ std::string StructTypeInst::getName() const {
   return name_constant->getAsCString();
 }
 
-llvm::Value &StructTypeInst::getNameOperand() const {
-  return *(this->getNameOperandAsUse().get());
-}
+OPERAND(StructTypeInst, NameOperand, 0)
 
-llvm::Use &StructTypeInst::getNameOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
-
-TO_STRING_IMPL(StructTypeInst)
+TO_STRING(StructTypeInst)
 
 /*
  * StaticTensorType implementation
@@ -267,13 +228,7 @@ Type &StaticTensorTypeInst::getElementType() const {
   return *type;
 }
 
-llvm::Value &StaticTensorTypeInst::getElementTypeOperand() const {
-  return *(this->getElementTypeOperandAsUse().get());
-}
-
-llvm::Use &StaticTensorTypeInst::getElementTypeOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
+OPERAND(StaticTensorTypeInst, ElementTypeOperand, 0)
 
 unsigned StaticTensorTypeInst::getNumberOfDimensions() const {
   auto &dims_value = this->getNumberOfDimensionsOperand();
@@ -291,13 +246,7 @@ unsigned StaticTensorTypeInst::getNumberOfDimensions() const {
   return num_dims;
 }
 
-llvm::Value &StaticTensorTypeInst::getNumberOfDimensionsOperand() const {
-  return *(this->getNumberOfDimensionsOperandAsUse().get());
-}
-
-llvm::Use &StaticTensorTypeInst::getNumberOfDimensionsOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(1);
-}
+OPERAND(StaticTensorTypeInst, NumberOfDimensionsOperand, 1)
 
 size_t StaticTensorTypeInst::getLengthOfDimension(
     unsigned dimension_index) const {
@@ -316,17 +265,9 @@ size_t StaticTensorTypeInst::getLengthOfDimension(
   return length;
 }
 
-llvm::Value &StaticTensorTypeInst::getLengthOfDimensionOperand(
-    unsigned dimension_index) const {
-  return *(this->getLengthOfDimensionOperandAsUse(dimension_index).get());
-}
+VAR_OPERAND(StaticTensorTypeInst, LengthOfDimensionOperand, 2)
 
-llvm::Use &StaticTensorTypeInst::getLengthOfDimensionOperandAsUse(
-    unsigned dimension_index) const {
-  return this->getCallInst().getArgOperandUse(2 + dimension_index);
-}
-
-TO_STRING_IMPL(StaticTensorTypeInst)
+TO_STRING(StaticTensorTypeInst)
 
 /*
  * TensorType implementation
@@ -339,13 +280,7 @@ Type &TensorTypeInst::getElementType() const {
   return *type;
 }
 
-llvm::Value &TensorTypeInst::getElementOperand() const {
-  return *(this->getElementOperandAsUse().get());
-}
-
-llvm::Use &TensorTypeInst::getElementOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
+OPERAND(TensorTypeInst, ElementOperand, 0)
 
 unsigned TensorTypeInst::getNumberOfDimensions() const {
   auto &dims_value = this->getNumberOfDimensionsOperand();
@@ -362,15 +297,9 @@ unsigned TensorTypeInst::getNumberOfDimensions() const {
   return num_dims;
 }
 
-llvm::Value &TensorTypeInst::getNumberOfDimensionsOperand() const {
-  return *(this->getNumberOfDimensionsOperandAsUse().get());
-}
+OPERAND(TensorTypeInst, NumberOfDimensionsOperand, 1)
 
-llvm::Use &TensorTypeInst::getNumberOfDimensionsOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(1);
-}
-
-TO_STRING_IMPL(TensorTypeInst);
+TO_STRING(TensorTypeInst);
 
 /*
  * AssocArrayType implementation
@@ -383,13 +312,7 @@ Type &AssocArrayTypeInst::getKeyType() const {
   return *type;
 }
 
-llvm::Value &AssocArrayTypeInst::getKeyOperand() const {
-  return *(this->getKeyOperandAsUse().get());
-}
-
-llvm::Use &AssocArrayTypeInst::getKeyOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
+OPERAND(AssocArrayTypeInst, KeyOperand, 0)
 
 Type &AssocArrayTypeInst::getValueType() const {
   auto type = TypeAnalysis::analyze(this->getValueOperand());
@@ -398,15 +321,9 @@ Type &AssocArrayTypeInst::getValueType() const {
   return *type;
 }
 
-llvm::Value &AssocArrayTypeInst::getValueOperand() const {
-  return *(this->getValueOperandAsUse().get());
-}
+OPERAND(AssocArrayTypeInst, ValueOperand, 1)
 
-llvm::Use &AssocArrayTypeInst::getValueOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(1);
-}
-
-TO_STRING_IMPL(AssocArrayTypeInst)
+TO_STRING(AssocArrayTypeInst)
 
 /*
  * SequenceType implementation
@@ -419,14 +336,8 @@ Type &SequenceTypeInst::getElementType() const {
   return *type;
 }
 
-llvm::Value &SequenceTypeInst::getElementOperand() const {
-  return *(this->getElementOperandAsUse().get());
-}
+OPERAND(SequenceTypeInst, ElementOperand, 0)
 
-llvm::Use &SequenceTypeInst::getElementOperandAsUse() const {
-  return this->getCallInst().getArgOperandUse(0);
-}
-
-TO_STRING_IMPL(SequenceTypeInst)
+TO_STRING(SequenceTypeInst)
 
 } // namespace llvm::memoir
