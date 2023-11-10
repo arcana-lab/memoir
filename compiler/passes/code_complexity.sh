3fd1a3bbf6fcce0e2ae8c6d2af742cb8fac304a1
Tommy McMichen <mcmichen@u.northwestern.edu>
2023-11-10 15:24:46 -0600

    [compiler][passes] Updated code_complexity.sh to run from any dir

--- compiler/passes/code_complexity.sh
+++ compiler/passes/code_complexity.sh
@@ -1,21 +1,33 @@
 #!/bin/bash -e
 
+# Parse input.
 OUTPUT_FILE="${1}"
 shift
 
+# Get the directory of this script.
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "${SOURCE}" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "${SOURCE}")
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ ${SOURCE} != /* ]] && SOURCE=$DIR/$SOURCE 
+done
+SCRIPT_DIR=$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )
+
+# Setup output file.
 touch ${OUTPUT_FILE}
 echo -n "" > ${OUTPUT_FILE}
 
-SLOC="scc -c"
-
+# Declare passes to evaluate.
 declare -A PASSES=(["DEE"]="slice_*" \
                    ["DFE"]="dead_field_elimination" \
                    ["FE"]="field_elision" \
                    ["RIE"]="key_folding" \
-                   ["SSAConstruction"]="mut_to_immut" \
-                   ["SSADestruction"]="ssa_destruction" \
+                   ["SSA Construction"]="mut_to_immut" \
+                   ["SSA Destruction"]="ssa_destruction" \
                   )
 
+# Create table.
 printf "\e[3m%-20s\e[0m ┃ \e[3m%-10s\e[0m\n" "Pass" "SLOC" >> ${OUTPUT_FILE}
 for i in $(seq 1 21) ; do printf "━"  >> ${OUTPUT_FILE}; done
 printf "╋" >> ${OUTPUT_FILE}
@@ -24,7 +36,8 @@ printf "\n" >> ${OUTPUT_FILE}
 for KEY in "${!PASSES[@]}"
 do
     printf "%-20s ┃ " "${KEY}" >> ${OUTPUT_FILE}
-    scc -c ${PASSES[${KEY}]} | awk -f code_complexity.awk >> ${OUTPUT_FILE}
+    scc -c ${SCRIPT_DIR}/${PASSES[${KEY}]} | awk -f ${SCRIPT_DIR}/code_complexity.awk >> ${OUTPUT_FILE}
 done
 
+# Print output to display.
 cat ${OUTPUT_FILE}
