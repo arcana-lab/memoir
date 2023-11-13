98caf938dc6c577c50a19f9df33b5fd395fed0cd
Haocheng Gao <hcg3942@gmail.com>
2023-11-13 11:31:54 -0600

    [compiler][ssa_destruction] Fix missing include

--- compiler/passes/ssa_destruction/src/SSADestruction.hpp
+++ compiler/passes/ssa_destruction/src/SSADestruction.hpp
@@ -5,6 +5,8 @@
 #include "llvm/IR/Metadata.h"
 #include "llvm/IR/Module.h"
 
+#include "noelle/core/Noelle.hpp"
+
 #include "memoir/ir/Builder.hpp"
 #include "memoir/ir/InstVisitor.hpp"
 #include "memoir/ir/Instructions.hpp"
