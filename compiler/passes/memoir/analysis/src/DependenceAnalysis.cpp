b647c78f5119a4db5735c556499a57ceedcdcb68
Haocheng Gao <hcg3942@gmail.com>
2023-11-13 09:00:27 -0600

    [compiler][analysis] Dependency analysis for memoir-to-memoir case

--- compiler/passes/memoir/analysis/src/DependenceAnalysis.cpp
+++ compiler/passes/memoir/analysis/src/DependenceAnalysis.cpp
@@ -2,117 +2,118 @@
 
 namespace llvm::memoir {
 
+CallInst *isMemOIRCall(Instruction *i) {
+  CallInst *call = dyn_cast<CallInst>(i);
+  if (!call)
+    return nullptr;
+  if (!FunctionNames::is_memoir_call(*call))
+    return nullptr;
+  return call;
+}
+
+struct DependenceSummary {
+  MemoryDataDependenceStrength RAW;
+  MemoryDataDependenceStrength WAR;
+  MemoryDataDependenceStrength WAW;
+
+  bool exists() {
+    return !(RAW == CANNOT_EXIST && WAR == CANNOT_EXIST && WAW == CANNOT_EXIST);
+  }
+
+  MemoryDataDependenceStrength query(DataDependenceType t) {
+    switch (t) {
+      case DG_DATA_RAW:
+        return RAW;
+        break;
+      case DG_DATA_WAR:
+        return WAR;
+        break;
+      case DG_DATA_WAW:
+        return WAW;
+        break;
+      case DG_DATA_NONE:
+        return MAY_EXIST;
+        break;
+    }
+  }
+};
+
+static DependenceSummary checkMemOIRDataDependence(Instruction *fromInst,
+                                                   Instruction *toInst) {
+  CallInst *fromMemOIRCall = isMemOIRCall(fromInst);
+  CallInst *toMemOIRCall = isMemOIRCall(toInst);
+
+  // MemOIR Inst --> MemOIR Inst
+  if (fromMemOIRCall && toMemOIRCall) {
+    for (Value *arg : toMemOIRCall->args()) {
+      if (fromMemOIRCall == arg) {
+        return DependenceSummary{
+          .RAW = MUST_EXIST,
+          .WAR = CANNOT_EXIST,
+          .WAW = CANNOT_EXIST,
+        };
+      }
+    }
+    return DependenceSummary{
+      .RAW = CANNOT_EXIST,
+      .WAR = CANNOT_EXIST,
+      .WAW = CANNOT_EXIST,
+    };
+  }
+
+  // MemOIR Inst --> General Inst
+  else if (fromMemOIRCall && !toMemOIRCall) {
+    return DependenceSummary{
+      .RAW = MAY_EXIST,
+      .WAR = MAY_EXIST,
+      .WAW = MAY_EXIST,
+    };
+  }
+
+  // General Inst --> MemOIR Inst
+  else if (!fromMemOIRCall && toMemOIRCall) {
+    return DependenceSummary{
+      .RAW = MAY_EXIST,
+      .WAR = MAY_EXIST,
+      .WAW = MAY_EXIST,
+    };
+  }
+
+  // General Inst --> General Inst
+  else {
+    return DependenceSummary{
+      .RAW = CANNOT_EXIST,
+      .WAR = CANNOT_EXIST,
+      .WAW = CANNOT_EXIST,
+    };
+  }
+}
+
 DependenceAnalysis::DependenceAnalysis(const std::string &name)
   : noelle::DependenceAnalysis(name) {}
 
 bool DependenceAnalysis::canThereBeAMemoryDataDependence(Instruction *fromInst,
                                                          Instruction *toInst) {
-  return true;
+  return checkMemOIRDataDependence(fromInst, toInst).exists();
 }
 
 bool DependenceAnalysis::canThereBeAMemoryDataDependence(Instruction *fromInst,
                                                          Instruction *toInst,
                                                          Function &function) {
-  CallInst *fromCall = dyn_cast<CallInst>(fromInst);
-  CallInst *toCall = dyn_cast<CallInst>(toInst);
-  if (fromCall == nullptr || toCall == nullptr)
-    return true;
-
-  MemOIR_Func fromFunc = FunctionNames::get_memoir_enum(*fromCall);
-  MemOIR_Func toFunc = FunctionNames::get_memoir_enum(*toCall);
-  if (fromFunc == MemOIR_Func::NONE || toFunc == MemOIR_Func::NONE)
-    return true; // TODO
-
-  // TODO: let `equal` to account for phi and select
-  // TODO: handle instructions other than ACCESS
-  //
-  // TYPE
-  //   ...
-  //
-  // ALLOC
-  //   ...
-  //
-  // ACCESS
-  //   READ
-  //     STRUCT_READ
-  //     INDEX_READ
-  //     ASSOC_READ
-  //   WRITE
-  //     STRUCT_WRITE
-  //     INDEX_WRITE
-  //     ASSOC_WRITE
-  //   GET
-  //     STRUCT_GET
-  //     INDEX_GET
-  //     ASSOC_GET
-  //
-  // SEQ_INSERT
-  // SEQ_REMOVE
-  // SEQ_APPEND
-  // SEQ_SWAP
-  // SEQ_SPLIT
-
-  if ((FunctionNames::is_read(fromFunc) || FunctionNames::is_write(fromFunc))
-      && (FunctionNames::is_read(toFunc) || FunctionNames::is_write(toFunc))) {
-    // TODO: def phi will soon be gone
-    return false;
-  }
-
-  if (FunctionNames::is_get(fromFunc) && FunctionNames::is_get(toFunc))
-    return false;
-
-  if (toFunc == MemOIR_Func::DEF_PHI) {
-    if (FunctionNames::is_write(fromFunc)) {
-      Value *written = fromCall->getArgOperand(1);
-      Value *def = toCall->getArgOperand(0);
-      if (written == def)
-        return true; // TODO this is a must
-    }
-    return false;
-  }
-
-  if (toFunc == MemOIR_Func::USE_PHI) {
-    if (FunctionNames::is_read(fromFunc)) {
-      Value *read = fromCall->getArgOperand(0);
-      Value *use = toCall->getArgOperand(0);
-      if (read == use)
-        return true; // TODO this is a must
-    }
-    return false;
-  }
-
-  if (fromFunc == MemOIR_Func::DEF_PHI || fromFunc == MemOIR_Func::USE_PHI) {
-    Value *access = FunctionNames::is_read(toFunc)
-                        ? toCall->getArgOperand(0)
-                        : FunctionNames::is_write(toFunc)
-                              ? toCall->getArgOperand(1)
-                              : FunctionNames::is_get(toFunc)
-                                    ? toCall->getArgOperand(0)
-                                    : nullptr;
-    if (access) {
-      if (access == fromCall) {
-        return true; // TODO this is a must
-      } else {
-        return false;
-      }
-    }
-  }
-
-  return true;
+  return checkMemOIRDataDependence(fromInst, toInst).exists();
 }
 
 bool DependenceAnalysis::canThereBeAMemoryDataDependence(Instruction *fromInst,
                                                          Instruction *toInst,
                                                          LoopStructure &loop) {
-  println("(^o^) LOOP (^o^)");
-  return true;
+  return checkMemOIRDataDependence(fromInst, toInst).exists();
 }
 
 MemoryDataDependenceStrength DependenceAnalysis::
     isThereThisMemoryDataDependenceType(DataDependenceType t,
                                         Instruction *fromInst,
                                         Instruction *toInst) {
-  return MAY_EXIST;
+  return checkMemOIRDataDependence(fromInst, toInst).query(t);
 }
 
 MemoryDataDependenceStrength DependenceAnalysis::
@@ -120,7 +121,7 @@ MemoryDataDependenceStrength DependenceAnalysis::
                                         Instruction *fromInst,
                                         Instruction *toInst,
                                         Function &function) {
-  return MAY_EXIST;
+  return checkMemOIRDataDependence(fromInst, toInst).query(t);
 }
 
 MemoryDataDependenceStrength DependenceAnalysis::
@@ -128,7 +129,7 @@ MemoryDataDependenceStrength DependenceAnalysis::
                                         Instruction *fromInst,
                                         Instruction *toInst,
                                         LoopStructure &loop) {
-  return MAY_EXIST;
+  return checkMemOIRDataDependence(fromInst, toInst).query(t);
 }
 
 bool DependenceAnalysis::canThisDependenceBeLoopCarried(
