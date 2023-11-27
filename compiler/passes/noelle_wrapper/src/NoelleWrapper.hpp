2871fa481564c9d4b879345164e087d8f09cc976
Haocheng Gao <hcg3942@gmail.com>
2023-11-27 17:20:35 -0600

    [compiler][analysis] Add back noelle_wrapper (it was removed due to bad merge)

---
+++ compiler/passes/noelle_wrapper/src/NoelleWrapper.hpp
@@ -0,0 +1,35 @@
+#ifndef MEMOIR_NOELLEWRAPPER_H
+#define MEMOIR_NOELLEWRAPPER_H
+#pragma once
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+#include "memoir/analysis/DependenceAnalysis.hpp"
+#include "noelle/core/Noelle.hpp"
+
+namespace llvm::memoir {
+
+class NoelleWrapper : public ModulePass {
+  Noelle *noelle;
+  memoir::DependenceAnalysis dependenceAnalysis;
+
+public:
+  static char ID;
+
+  NoelleWrapper();
+
+  Noelle &getNoelle(void);
+
+  bool doInitialization(Module &M) override;
+
+  bool runOnModule(Module &M) override;
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override;
+};
+
+} // namespace llvm::memoir
+
+#endif
