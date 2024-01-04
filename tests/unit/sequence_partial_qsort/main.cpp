3b4f6f059b0e6be32bcd585fba78f90e5534f1c6
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-01-04 16:27:10 -0600

    [tests][unit][sequence_partial_qsort] Updated test to new API

--- tests/unit/sequence_partial_qsort/main.cpp
+++ tests/unit/sequence_partial_qsort/main.cpp
@@ -4,68 +4,65 @@
 
 using namespace memoir;
 
-#define K 5
+#define B 1
 
-memoir::Collection *qsort(memoir::Collection *seq_to_sort) {
-  // Type information
-  memoir_assert_collection_type(memoir_sequence_type(memoir_u64_t),
-                                seq_to_sort);
-
-  // Size information
-  auto n = memoir_size(seq_to_sort);
-
+void qsort(memoir::Collection *seq, size_t start, size_t end) {
+  size_t n = end - start;
   // Perform quicksort
-  if (n < 2) {
-    return seq_to_sort;
+  if (end <= start || n <= 1) {
+    return;
   }
 
   // Perform insertion sort for n < 3
-  if (n < 3) {
-    auto l_slice = memoir_sequence_slice(seq_to_sort, 0, 1);
-    auto r_slice = memoir_sequence_slice(seq_to_sort, 1, 2);
-    if (memoir_index_read(u64, l_slice, 0)
-        > memoir_index_read(u64, r_slice, 0)) {
-      return memoir_join(r_slice, l_slice);
-    } else {
-      return memoir_join(l_slice, r_slice);
+  if (n == 2) {
+    if (memoir_index_read(u64, seq, start)
+        > memoir_index_read(u64, seq, end - 1)) {
+      memoir_seq_swap_within(seq, start, end - 1);
     }
+    return;
   }
 
   // Select pivot
-  auto p = n / 2;
-  auto p_slice = memoir_sequence_slice(seq_to_sort, p, p + 1);
-  auto l_slice = memoir_sequence_slice(seq_to_sort, 0, p);
-  auto r_slice = memoir_sequence_slice(seq_to_sort, p + 1, -1);
-  auto rest = memoir_join(l_slice, r_slice);
-  auto remaining = n - 1;
+  auto p = n / 2 + start;
 
-  // Create the initial partitions
-  auto l_part = memoir_allocate_sequence(memoir_u64_t, 0);
-  auto l_size = 0;
-  auto r_part = memoir_allocate_sequence(memoir_u64_t, 0);
-  auto r_size = 0;
+  // Move pivot.
+  memoir_seq_swap_within(seq, start, p);
 
-  do {
-    auto first = memoir_sequence_slice(rest, 0, 1);
-    if (remaining > 1) {
-      rest = memoir_sequence_slice(rest, 1, -1);
+  // Get the pivot value.
+  auto pv = memoir_index_read(u64, seq, start);
+
+  // Construct partitions.
+  auto l = start;
+  auto r = end;
+  while (true) {
+    while (true) {
+      r--;
+      if (memoir_index_read(u64, seq, r) < pv || r <= l) {
+        break;
+      }
     }
-    remaining -= 1;
-
-    if (memoir_index_read(u64, first, 0) < memoir_index_read(u64, p_slice, 0)) {
-      l_part = memoir_join(l_part, first);
-      l_size += 1;
+    while (true) {
+      l++;
+      if (memoir_index_read(u64, seq, l) > pv || l >= r) {
+        break;
+      }
+    }
+    if (l < r) {
+      memoir_seq_swap_within(seq, l, r);
     } else {
-      r_part = memoir_join(r_part, first);
-      r_size += 1;
+      break;
     }
+  }
 
-  } while (remaining > 0);
+  // Move the pivot back into place.
+  p = r;
+  memoir_seq_swap_within(seq, start, p);
 
-  auto l_sorted = qsort(l_part);
-  auto r_sorted = qsort(r_part);
+  // Recurse.
+  qsort(seq, start, r);
+  qsort(seq, r + 1, end);
 
-  return memoir_join(l_sorted, p_slice, r_sorted);
+  return;
 }
 
 int main(int argc, char *argv[]) {
@@ -75,28 +72,26 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  auto length = (uint64_t)argc - 1;
+  auto size = argc - 1;
+  auto seq = memoir_allocate_sequence(memoir_u64_t, size);
 
-  auto seq = memoir_allocate_sequence(memoir_u64_t, length);
-
-  for (auto i = 0; i < length; i++) {
-    auto input_element = atoi(argv[1 + i]);
-    memoir_index_write(u64, input_element, seq, i);
+  for (auto i = 1; i < argc; i++) {
+    auto input_element = atoi(argv[i]);
+    memoir_index_write(u64, input_element, seq, i - 1);
   }
 
   printf("\nSorting sequence\n");
 
-  auto sorted_seq = qsort(seq);
+  qsort(seq, 0, size);
 
-  auto print_length = (length < K) ? length : K;
-  printf("\nResult (first %d elements): \n", print_length);
+  auto print_length = size > B ? B : size;
+
+  printf("\nResult: \n");
   for (auto i = 0; i < print_length; i++) {
-    auto read = memoir_index_read(u64, sorted_seq, i);
-    if (i == 0) {
-      printf("%lu", read);
-    } else {
-      printf(", %lu", read);
-    }
+    auto read = memoir_index_read(u64, seq, i);
+    printf("%lu, ", read);
   }
   printf("\n");
+
+  return 0;
 }
