7c58410deb0c7fc8bf195683ac16cc3492498c4b
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-02-20 09:46:32 -0600

    [tests][unit] Rename and updated assoc_structref_to_u64

--- tests/unit/assoc_structref_to_u64/main.cpp
+++ tests/unit/assoc_structref_to_u64/main.cpp
@@ -11,7 +11,7 @@ using namespace memoir;
 auto objTy = memoir_define_struct_type("Foo", memoir_u64_t);
 
 int main() {
-  std::cout << "Initializing keys\n";
+  printf("Initializing keys\n");
 
   auto obj0 = memoir_allocate_struct(objTy);
   auto obj1 = memoir_allocate_struct(objTy);
@@ -21,27 +21,30 @@ int main() {
   memoir_struct_write(u64, 2, obj1, 0);
   memoir_struct_write(u64, 3, obj2, 0);
 
-  std::cout << "Initializing map\n";
+  printf("Initializing map\n");
 
-  auto map = memoir_allocate_assoc_array(objTy, memoir_u64_t);
+  auto map = memoir_allocate_assoc_array(memoir_ref_t(objTy), memoir_u64_t);
 
+  memoir_assoc_insert(map, obj0);
   memoir_assoc_write(u64, VAL0, map, obj0);
+  memoir_assoc_insert(map, obj1);
   memoir_assoc_write(u64, VAL1, map, obj1);
+  memoir_assoc_insert(map, obj2);
   memoir_assoc_write(u64, VAL2, map, obj2);
 
-  std::cout << "Reading map\n";
+  printf("Reading map\n");
 
   auto read0 = memoir_assoc_read(u64, map, obj0);
   auto read1 = memoir_assoc_read(u64, map, obj1);
   auto read2 = memoir_assoc_read(u64, map, obj2);
 
-  std::cout << " Result:\n";
-  std::cout << "  obj0 -> " << std::to_string(read0) << "\n";
-  std::cout << "  obj1 -> " << std::to_string(read1) << "\n";
-  std::cout << "  obj2 -> " << std::to_string(read2) << "\n";
+  printf(" Result:\n");
+  printf("  obj0 -> %d\n", read0);
+  printf("  obj1 -> %d\n", read1);
+  printf("  obj2 -> %d\n", read2);
 
-  std::cout << "Expected:\n";
-  std::cout << "  obj0 -> " << std::to_string(VAL0) << "\n";
-  std::cout << "  obj1 -> " << std::to_string(VAL1) << "\n";
-  std::cout << "  obj2 -> " << std::to_string(VAL2) << "\n";
+  printf("Expected:\n");
+  printf("  obj0 -> %d\n", VAL0);
+  printf("  obj1 -> %d\n", VAL1);
+  printf("  obj2 -> %d\n", VAL2);
 }
