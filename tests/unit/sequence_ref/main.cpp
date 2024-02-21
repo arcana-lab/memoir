37e9eeb40d7b4d26244aded56a463ff8e2f8b588
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-02-20 18:22:18 -0600

    [tests][unit] Updated sequence_ref test

--- tests/unit/sequence_ref/main.cpp
+++ tests/unit/sequence_ref/main.cpp
@@ -11,65 +11,59 @@ using namespace memoir;
 #define VAL2_0 3
 #define VAL2_1 30
 
-auto type = memoir_define_struct_type("Foo", memoir_u64_t, memoir_u64_t);
+auto type = memoir_define_struct_type("Foo", memoir_u32_t, memoir_u32_t);
 
 int main() {
 
-  std::cout << "\nIntializing elements\n";
-
   auto obj0 = memoir_allocate_struct(type);
-  memoir_struct_write(u64, (uint64_t)rand(), obj0, 0);
-  memoir_struct_write(u64, VAL0_1, obj0, 1);
+  memoir_struct_write(u32, rand(), obj0, 0);
+  memoir_struct_write(u32, VAL0_1, obj0, 1);
 
   auto obj1 = memoir_allocate_struct(type);
-  memoir_struct_write(u64, (uint64_t)rand(), obj1, 0);
-  memoir_struct_write(u64, VAL1_1, obj1, 1);
+  memoir_struct_write(u32, rand(), obj1, 0);
+  memoir_struct_write(u32, VAL1_1, obj1, 1);
 
   auto obj2 = memoir_allocate_struct(type);
-  memoir_struct_write(u64, (uint64_t)rand(), obj2, 0);
-  memoir_struct_write(u64, VAL2_1, obj2, 1);
-
-  std::cout << "\nInitializing sequence\n";
+  memoir_struct_write(u32, rand(), obj2, 0);
+  memoir_struct_write(u32, VAL2_1, obj2, 1);
 
   auto seq = memoir_allocate_sequence(memoir_ref_t(type), 3);
   memoir_index_write(struct_ref, obj0, seq, 0);
   memoir_index_write(struct_ref, obj1, seq, 1);
   memoir_index_write(struct_ref, obj2, seq, 2);
 
-  std::cout << "\nUpdating sequence\n";
+  obj0 = memoir_index_read(struct_ref, seq, 0);
+  memoir_struct_write(u32, VAL0_0, obj0, 0);
+  obj1 = memoir_index_read(struct_ref, seq, 1);
+  memoir_struct_write(u32, VAL1_0, obj1, 0);
+  obj2 = memoir_index_read(struct_ref, seq, 2);
+  memoir_struct_write(u32, VAL2_0, obj2, 0);
 
   obj0 = memoir_index_read(struct_ref, seq, 0);
-  memoir_struct_write(u64, VAL0_0, obj0, 0);
+  auto read0_0 = memoir_struct_read(u32, obj0, 0);
+  auto read0_1 = memoir_struct_read(u32, obj0, 1);
   obj1 = memoir_index_read(struct_ref, seq, 1);
-  memoir_struct_write(u64, VAL1_0, obj1, 0);
+  auto read1_0 = memoir_struct_read(u32, obj1, 0);
+  auto read1_1 = memoir_struct_read(u32, obj1, 1);
   obj2 = memoir_index_read(struct_ref, seq, 2);
-  memoir_struct_write(u64, VAL2_0, obj2, 0);
+  auto read2_0 = memoir_struct_read(u32, obj2, 0);
+  auto read2_1 = memoir_struct_read(u32, obj2, 1);
 
-  std::cout << "\nReading sequence\n";
+  printf(" Result:\n");
+  printf("  [ (%d, %d), (%d, %d), (%d, %d) ]\n",
+         read0_0,
+         read0_1,
+         read1_0,
+         read1_1,
+         read2_0,
+         read2_1);
 
-  obj0 = memoir_index_read(struct_ref, seq, 0);
-  auto read0_0 = memoir_struct_read(u64, obj0, 0);
-  auto read0_1 = memoir_struct_read(u64, obj0, 1);
-  obj1 = memoir_index_read(struct_ref, seq, 1);
-  auto read1_0 = memoir_struct_read(u64, obj1, 0);
-  auto read1_1 = memoir_struct_read(u64, obj1, 1);
-  obj2 = memoir_index_read(struct_ref, seq, 2);
-  auto read2_0 = memoir_struct_read(u64, obj2, 0);
-  auto read2_1 = memoir_struct_read(u64, obj2, 1);
-
-  std::cout << " Result:\n";
-  std::cout << "  HEAD -> " << std::to_string(read0_0) << ", "
-            << std::to_string(read0_1) << "\n";
-  std::cout << "       -> " << std::to_string(read1_0) << ", "
-            << std::to_string(read1_1) << "\n";
-  std::cout << "       -> " << std::to_string(read2_0) << ", "
-            << std::to_string(read2_1) << "\n\n";
-
-  std::cout << "Expected:\n";
-  std::cout << "  HEAD -> " << std::to_string(VAL0_0) << ", "
-            << std::to_string(VAL0_1) << "\n";
-  std::cout << "       -> " << std::to_string(VAL1_0) << ", "
-            << std::to_string(VAL1_1) << "\n";
-  std::cout << "       -> " << std::to_string(VAL2_0) << ", "
-            << std::to_string(VAL2_1) << "\n\n";
+  printf("Expected:\n");
+  printf("  [ (%d, %d), (%d, %d), (%d, %d) ]\n",
+         VAL0_0,
+         VAL0_1,
+         VAL1_0,
+         VAL1_1,
+         VAL2_0,
+         VAL2_1);
 }
