a516eddf4eb3ff5086b3052dea3592821fcf53e1
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-03-02 02:21:03 -0600

    [compiler][support] Changed into to cast from/to llvm::Value

--- compiler/memoir/support/Casting.hpp
+++ compiler/memoir/support/Casting.hpp
@@ -12,12 +12,12 @@
 namespace llvm::memoir {
 
 // Functions to dyn_cast an llvm::Instruction to a MemOIRInst
-template <
-    class To,
-    class From,
-    std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
-    std::enable_if_t<std::is_base_of_v<llvm::Instruction, From>, bool> = true>
-To *into(From *I) {
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<llvm::Value, From>, bool> = true>
+To *into(From *V) {
+  auto *I = dyn_cast_or_null<llvm::Instruction>(V);
   if (I == nullptr) {
     return nullptr;
   }
@@ -25,30 +25,31 @@ To *into(From *I) {
   return dyn_cast_or_null<To>(memoir_inst);
 }
 
-template <
-    class To,
-    class From,
-    std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
-    std::enable_if_t<std::is_base_of_v<llvm::Instruction, From>, bool> = true>
-To *into(From &I) {
-  auto *memoir_inst = MemOIRInst::get(I);
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<llvm::Value, From>, bool> = true>
+To *into(From &V) {
+  auto *I = dyn_cast_or_null<llvm::Instruction>(&V);
+  if (I == nullptr) {
+    return nullptr;
+  }
+  auto *memoir_inst = MemOIRInst::get(*I);
   return dyn_cast_or_null<To>(memoir_inst);
 }
 
-template <
-    class To,
-    class From,
-    std::enable_if_t<std::is_base_of_v<llvm::Instruction, To>, bool> = true,
-    std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<llvm::Value, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
 To *into(From *I) {
   return dyn_cast_or_null<To>(&I->getCallInst());
 }
 
-template <
-    class To,
-    class From,
-    std::enable_if_t<std::is_base_of_v<llvm::Instruction, To>, bool> = true,
-    std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<llvm::Value, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
 To *into(From &I) {
   return dyn_cast_or_null<To>(&I.getCallInst());
 }
