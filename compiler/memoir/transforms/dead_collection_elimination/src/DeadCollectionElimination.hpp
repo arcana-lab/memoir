81cd76452b2e99b18082fa6662bee46daaab9fbf
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-06-27 14:40:20 -0500

    [compiler][transforms] Updated DeadCollectionElimination

--- compiler/memoir/transforms/dead_collection_elimination/src/DeadCollectionElimination.hpp
+++ compiler/memoir/transforms/dead_collection_elimination/src/DeadCollectionElimination.hpp
@@ -3,21 +3,9 @@
 #pragma once
 
 // LLVM
-#include "llvm/IR/Function.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/Metadata.h"
-#include "llvm/IR/Module.h"
-#include "llvm/Pass.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-
 #include "llvm/IR/Dominators.h"
 
 #include "llvm/Analysis/CFG.h"
-
-// NOELLE
-#include "noelle/core/Noelle.hpp"
 
 // MemOIR
 #include "memoir/ir/Builder.hpp"
--- compiler/memoir/transforms/dead_collection_elimination/src/Pass.cpp
+++ compiler/memoir/transforms/dead_collection_elimination/src/Pass.cpp
@@ -3,15 +3,12 @@
 
 // LLVM
 #include "llvm/IR/Function.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Metadata.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Pass.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 // MemOIR
+#include "memoir/passes/Passes.hpp"
+
 #include "memoir/ir/InstVisitor.hpp"
 #include "memoir/ir/Instructions.hpp"
 
@@ -25,40 +22,22 @@
 namespace llvm::memoir {
 
 /*
- * This pass canonicalizes the conservative iteration space of sequences in
- * loops by inserting slice and join operations before and after the loop.
+ * This pass eliminates dead collections.
  *
  * Author(s): Tommy McMichen
  * Created: February 22, 2023
  */
 
 struct DeadCollectionEliminationPass : public ModulePass {
-  static char ID;
-
-  DeadCollectionEliminationPass() : ModulePass(ID) {}
-
-  bool doInitialization(llvm::Module &M) override {
-    return false;
-  }
-
-  bool runOnModule(llvm::Module &M) override {
+  llvm::PreservedAnalyses runOnModule(llvm::Module &M,
+                                      llvm::ModuleAnalysisManager &MAM) {
     debugln("Running dead collection elimination pass");
     debugln();
 
     auto DCE = DeadCollectionElimination(M);
 
-    return true;
-  }
-
-  void getAnalysisUsage(llvm::AnalysisUsage &AU) const override {
-    return;
+    return llvm::PreservedAnalyses::none();
   }
 };
-
-// Next there is code to register your pass to "opt"
-char DeadCollectionEliminationPass::ID = 0;
-static llvm::RegisterPass<DeadCollectionEliminationPass> X(
-    "memoir-dce",
-    "Eliminates dead collection allocations.");
 
 } // namespace llvm::memoir
