520a8ed15d8cbbcfcdff1f9e27ac55d02e94f09f
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-06-28 09:07:10 -0500

    [compiler][analysis] Updated LivenessAnalysis to new structure. Relies on NOELLE, and also needs to be updated based on prior NOELLE changes

--- compiler/memoir/analysis/LivenessAnalysis.hpp
+++ compiler/memoir/analysis/LivenessAnalysis.hpp
@@ -10,22 +10,97 @@
 #include "memoir/ir/Instructions.hpp"
 
 namespace llvm::memoir {
-class LivenessAnalysis {
-public:
-  // Construction and analysis invocation.
-  LivenessAnalysis(llvm::Function &F, arcana::noelle::DataFlowEngine DFE);
 
-  // Queries.
+struct LivenessResult {
+public:
+  /**
+   * Query if a value is alive at a given MEMOIR instruction.
+   *
+   * @param V the value to query liveness for
+   * @param I the MEMOIR instruction to query at
+   * @param after query liveness immediately before (false) or after (true) the
+   * instruction (default = true)
+   *
+   * @returns true if the value is alive, false otherwise.
+   */
   bool is_live(llvm::Value &V, MemOIRInst &I, bool after = true);
+
+  /**
+   * Query if a value is alive at a given LLVM instruction.
+   *
+   * @param V the value to query liveness for
+   * @param I the LLVM instruction to query at
+   * @param after query liveness immediately before (false) or after (true) the
+   * instruction (default = true)
+   *
+   * @returns true if the value is alive, false otherwise.
+   */
   bool is_live(llvm::Value &V, llvm::Instruction &I, bool after = true);
+
+  /**
+   * Get the set of live values at a given MEMOIR instruction.
+   *
+   * @param I the LLVM instruction to query at
+   * @param after query liveness immediately before (false) or after (true) the
+   * instruction (default = true)
+   *
+   * @returns a reference to the set of live values
+   */
   std::set<llvm::Value *> &get_live_values(MemOIRInst &I, bool after = true);
+
+  /**
+   * Get the set of live values at a given LLVM instruction.
+   *
+   * @param I the LLVM instruction to query at
+   * @param after query liveness immediately before (false) or after (true) the
+   * instruction (default = true)
+   *
+   * @returns a reference to the set of live values
+   */
   std::set<llvm::Value *> &get_live_values(llvm::Instruction &I,
                                            bool after = true);
 
+  /**
+   * Get the underlying NOELLE data flow result.
+   *
+   * @returns a reference to the data flow result
+   */
+  arcana::noelle::DataFlowResult &get_dataflow_result();
+
 protected:
-  llvm::Function &F;
-  arcana::noelle::DataFlowEngine DFE;
   arcana::noelle::DataFlowResult *DFR;
 };
+
+class LivenessDriver {
+public:
+  LivenessDriver(llvm::Function &F, arcana::noelle::DataFlowEngine DFE);
+
+protected:
+  LivenessResult &result;
+
+  llvm::Function &F;
+  arcana::noelle::DataFlowEngine DFE;
+};
+
+LivenessResult LiveAnalysis::run(llvm::Function &F,
+                                 llvm::FunctionAnalysisManager &FAM) {
+  // Construct a new result.
+  LivenessResult result;
+
+  // Get the ModuleAnalysisManager proxy.
+  auto &MAM = GET_MODULE_ANALYSIS_MANAGER(FAM, F);
+
+  // Get NOELLE from the analysis manager.
+  auto &NOELLE = MAM.getResult<arcana::noelle::Noelle>(M);
+
+  // Get the DataFlowEngine from NOELLE.
+  auto &DFE = NOELLE.getDataFlowEngine();
+
+  // Run the analysis.
+  LivenessDriver driver(F, DFE);
+
+  // Return the analysis.
+  return result;
+}
 
 } // namespace llvm::memoir
