42e9b4a087ce9586fb406699fcaa39c44e86c5f7
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-02 14:41:17 -0500

    [tests][unit] Updated key_folding tests

--- tests/unit/key_folding/main.cpp
+++ tests/unit/key_folding/main.cpp
@@ -1,38 +1,70 @@
 #include <iostream>
 
 #include "cmemoir/cmemoir.h"
+#include "cmemoir/test.hpp"
 
 using namespace memoir;
 
-auto type = memoir_define_struct_type("Foo", memoir_u64_t, memoir_u64_t);
+auto type = memoir_define_struct_type("Foo", memoir_u32_t, memoir_u32_t);
 
 int main() {
-  auto *seq = memoir_allocate_sequence(type, 100);
+  TEST(fold_assoc_onto_seq) {
+    auto *seq = memoir_allocate_sequence(type, 100);
 
-  for (auto i = 0; i < 100; i++) {
-    auto *elem = memoir_index_get(struct, seq, i);
-    memoir_struct_write(u64, i, elem, 0);
-    memoir_struct_write(u64, 100 + i, elem, 1);
+    for (auto i = 0; i < 100; i++) {
+      auto *elem = memoir_index_get(struct, seq, i);
+      memoir_struct_write(u32, i, elem, 0);
+      memoir_struct_write(u32, 100 + i, elem, 1);
+    }
+
+    auto *assoc = memoir_allocate_assoc_array(memoir_ref_t(type), memoir_u32_t);
+    for (auto i = 99; i >= 0; --i) {
+      auto *elem = memoir_index_get(struct, seq, i);
+      memoir_assoc_write(u32, i, assoc, elem);
+    }
+
+    int sum = 0;
+    for (auto i = 0; i < 100; i++) {
+      auto *elem = memoir_index_get(struct, seq, i);
+      auto read1 = memoir_struct_read(u32, elem, 0);
+      auto read2 = memoir_struct_read(u32, elem, 1);
+      auto read3 = memoir_assoc_read(u32, assoc, elem);
+      sum += read2;
+      sum -= read1;
+      sum += read3;
+    }
+
+    EXPECT(sum == 14950, "sum differs!");
   }
 
-  auto *assoc = memoir_allocate_assoc_array(memoir_ref_t(type), memoir_u64_t);
-  for (auto i = 99; i >= 0; --i) {
-    auto *elem = memoir_index_get(struct, seq, i);
-    memoir_assoc_write(u64, i, assoc, elem);
-  }
+  TEST(fold_assoc_onto_assoc) {
+    auto *base = memoir_allocate_assoc_array(memoir_u32_t, type);
 
-  int sum = 0;
-  for (auto i = 0; i < 100; i++) {
-    auto *elem = memoir_index_get(struct, seq, i);
-    auto read1 = memoir_struct_read(u64, elem, 0);
-    auto read2 = memoir_struct_read(u64, elem, 1);
-    auto read3 = memoir_assoc_read(u64, assoc, elem);
-    sum += read2;
-    sum -= read1;
-    sum += read3;
-  }
+    for (auto i = 0; i < 100; i += 2) {
+      auto *elem = memoir_assoc_get(struct, base, i);
+      memoir_struct_write(u32, i, elem, 0);
+      memoir_struct_write(u32, 100 + i, elem, 1);
+    }
 
-  printf("sum = %lu\n", sum);
+    auto *assoc = memoir_allocate_assoc_array(memoir_ref_t(type), memoir_u32_t);
+    for (auto i = 98; i >= 0; i -= 2) {
+      auto *elem = memoir_assoc_get(struct, base, i);
+      memoir_assoc_write(u32, i, assoc, elem);
+    }
+
+    int sum = 0;
+    for (auto i = 0; i < 100; i += 2) {
+      auto *elem = memoir_assoc_get(struct, base, i);
+      auto read1 = memoir_struct_read(u32, elem, 0);
+      auto read2 = memoir_struct_read(u32, elem, 1);
+      auto read3 = memoir_assoc_read(u32, assoc, elem);
+      sum += read2;
+      sum -= read1;
+      sum += read3;
+    }
+
+    EXPECT(sum == 7450, "sum differs!");
+  }
 
   return 0;
 }
