87c40c904f2b143b6f1270baa9f71e7df76c6f8c
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-02 09:48:37 -0500

    [tests][unit] Migrated the sequence_ref test into sequence

--- tests/unit/sequence/main.cpp
+++ tests/unit/sequence/main.cpp
@@ -13,6 +13,15 @@ using namespace memoir;
 #define VAL4 uint64_t(50)
 #define VAL5 uint64_t(60)
 
+#define VAL0_0 1
+#define VAL0_1 10
+#define VAL1_0 2
+#define VAL1_1 20
+#define VAL2_0 3
+#define VAL2_1 30
+
+auto type = memoir_define_struct_type("Foo", memoir_u32_t, memoir_u32_t);
+
 int main(int argc, char *argv[]) {
   TEST(read_and_write) {
     auto seq = memoir_allocate_sequence(memoir_u64_t, 3);
@@ -148,5 +157,41 @@ int main(int argc, char *argv[]) {
     EXPECT(memoir_index_read(u64, seq, 1) == VAL1, "seq[1] differs!");
     EXPECT(memoir_index_read(u64, seq, 2) == VAL2, "seq[2] differs!");
     EXPECT(memoir_index_read(u64, seq, 3) == VAL0, "seq[3] differs!");
+  }
+
+  TEST(sequence_ref) {
+    auto *obj0 = memoir_allocate_struct(type);
+    memoir_struct_write(u32, rand(), obj0, 0);
+    memoir_struct_write(u32, VAL0_1, obj0, 1);
+
+    auto *obj1 = memoir_allocate_struct(type);
+    memoir_struct_write(u32, rand(), obj1, 0);
+    memoir_struct_write(u32, VAL1_1, obj1, 1);
+
+    auto *obj2 = memoir_allocate_struct(type);
+    memoir_struct_write(u32, rand(), obj2, 0);
+    memoir_struct_write(u32, VAL2_1, obj2, 1);
+
+    auto seq = memoir_allocate_sequence(memoir_ref_t(type), 3);
+    memoir_index_write(struct_ref, obj0, seq, 0);
+    memoir_index_write(struct_ref, obj1, seq, 1);
+    memoir_index_write(struct_ref, obj2, seq, 2);
+
+    obj0 = memoir_index_read(struct_ref, seq, 0);
+    memoir_struct_write(u32, VAL0_0, obj0, 0);
+    obj1 = memoir_index_read(struct_ref, seq, 1);
+    memoir_struct_write(u32, VAL1_0, obj1, 0);
+    obj2 = memoir_index_read(struct_ref, seq, 2);
+    memoir_struct_write(u32, VAL2_0, obj2, 0);
+
+    obj0 = memoir_index_read(struct_ref, seq, 0);
+    EXPECT(memoir_struct_read(u32, obj0, 0) == VAL0_0, "[0].0 differs");
+    EXPECT(memoir_struct_read(u32, obj0, 1) == VAL0_1, "[0].1 differs");
+    obj1 = memoir_index_read(struct_ref, seq, 1);
+    EXPECT(memoir_struct_read(u32, obj1, 0) == VAL1_0, "[1].0 differs");
+    EXPECT(memoir_struct_read(u32, obj1, 1) == VAL1_1, "[1].1 differs");
+    obj2 = memoir_index_read(struct_ref, seq, 2);
+    EXPECT(memoir_struct_read(u32, obj2, 0) == VAL2_0, "[2].0 differs");
+    EXPECT(memoir_struct_read(u32, obj2, 1) == VAL2_1, "[2].1 differs");
   }
 }
