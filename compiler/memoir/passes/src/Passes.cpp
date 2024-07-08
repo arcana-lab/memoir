beda7057c7dee666809b9ffd5aedc3d7b4d94e4f
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-08 17:51:26 -0500

    [compiler][passes] Updated to include Analysis registration

--- compiler/memoir/passes/src/Passes.cpp
+++ compiler/memoir/passes/src/Passes.cpp
@@ -1,6 +1,10 @@
 // MEMOIR
 #include "memoir/passes/Passes.hpp"
 
+#include "memoir/analysis/LiveRangeAnalysis.hpp"
+#include "memoir/analysis/Liveness.hpp"
+#include "memoir/analysis/RangeAnalysis.hpp"
+
 using namespace llvm::memoir;
 
 // Helper function to adapt function pass to a module pass.
@@ -23,19 +27,41 @@ llvmGetPassPluginInfo() {
            "memoir",
            LLVM_VERSION_STRING,
            [](llvm::PassBuilder &PB) {
+             // Register transforation passes.
              PB.registerPipelineParsingCallback(
                  [](llvm::StringRef name,
                     llvm::ModulePassManager &MPM,
                     llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
 
-#define PASS(SCOPE, CLASS, NAME, ARGS...)                                      \
+#define PASS(SCOPE, CLASS, NAME)                                               \
   if (name == NAME) {                                                          \
-    MPM.addPass(CLASS(ARGS));                                                  \
+    MPM.addPass(CLASS());                                                      \
     return true;                                                               \
   }
 #include "memoir/passes/Passes.def"
-#undef PASS
                    return false;
+                 });
+
+             // Register module analyses.
+             PB.registerAnalysisRegistrationCallback(
+                 [](llvm::ModuleAnalysisManager &MAM) {
+#define MODULE_ANALYSIS(CLASS, RESULT)                                         \
+  MAM.registerPass([&] { return CLASS(); });
+#include "memoir/passes/Passes.def"
+                   // MAM.registerPass([&] { return
+                   // llvm::memoir::RangeAnalysis(); }); MAM.registerPass([&] {
+                   // return llvm::memoir::LiveRangeAnalysis(); });
+                 });
+
+             // Register function analyses.
+             PB.registerAnalysisRegistrationCallback(
+                 [](llvm::FunctionAnalysisManager &FAM) {
+#define FUNCTION_ANALYSIS(CLASS, RESULT)                                       \
+  FAM.registerPass([&] { return CLASS(); });
+#include "memoir/passes/Passes.def"
+                   // FAM.registerPass([&] { return
+                   // llvm::memoir::LivenessAnalysis();
+                   // });
                  });
            } };
 }
