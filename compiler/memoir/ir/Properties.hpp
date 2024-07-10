f3321530091b2d8743a01bda4ce5f51d1c4b5a31
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-10 14:08:46 -0500

    [compiler][ir] Added implementation of PropertyInst and Properties

---
+++ compiler/memoir/ir/Properties.hpp
@@ -0,0 +1,15 @@
+#ifndef MEMOIR_IR_PROPERTIES_H
+#define MEMOIR_IR_PROPERTIES_H
+
+#include "memoir/ir/Property.hpp"
+
+namespace llvm::memoir {
+
+// Implementations of user-defined properties go here.
+
+// Properties _must_ implement the following:
+//   static bool class_of(const Property &);
+
+} // namespace llvm::memoir
+
+#endif // MEMOIR_IR_PROPERTIES_H
