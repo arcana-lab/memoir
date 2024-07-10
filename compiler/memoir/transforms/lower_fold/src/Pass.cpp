88f0d20ab1b121a19212d3c3bc0392116aca793b
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-10 09:22:40 -0500

    [compiler][transforms] Added the LowerFoldPass, which lowers fold instructions to loops so that folks don't _need_ to implement support for FoldInst.

---
+++ compiler/memoir/transforms/lower_fold/src/Pass.cpp
@@ -0,0 +1,21 @@
+// LLVM
+#include "llvm/IR/Module.h"
+
+// MEMOIR
+#include "memoir/passes/Passes.hpp"
+
+#include "memoir/support/Print.hpp"
+
+#include "LowerFold.hpp"
+
+namespace llvm::memoir {
+
+llvm::PreservedAnalyses LowerFoldPass::run(llvm::Module &M,
+                                           llvm::ModuleAnalysisManager &MAM) {
+
+  LowerFold LF(M);
+
+  return llvm::PreservedAnalyses::none();
+}
+
+} // namespace llvm::memoir
