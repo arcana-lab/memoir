3350a1d1dd21f36559c1dc1d0d78dfd2f4f06893
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-10 14:39:20 -0500

    [compiler][support] Added casting support for properties

--- compiler/memoir/support/Casting.hpp
+++ compiler/memoir/support/Casting.hpp
@@ -7,9 +7,20 @@
 #include "llvm/Support/Casting.h"
 
 #include "memoir/ir/Instructions.hpp"
+#include "memoir/ir/Properties.hpp"
+
 #include "memoir/support/Assert.hpp"
 
 namespace llvm::memoir {
+
+using llvm::isa;
+using llvm::isa_and_nonnull;
+
+using llvm::cast;
+using llvm::cast_or_null;
+
+using llvm::dyn_cast;
+using llvm::dyn_cast_or_null;
 
 // Functions to dyn_cast an llvm::Instruction to a MemOIRInst
 template <class To,
@@ -52,6 +63,35 @@ template <class To,
           std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
 To *into(From &I) {
   return dyn_cast_or_null<To>(&I.getCallInst());
+}
+
+// Functions to check type of Property
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<Property, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<Property, From>, bool> = true>
+bool isa(const From &p) {
+  return To::class_of(p);
+}
+
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<Property, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<Property, From>, bool> = true>
+To cast(const From &p) {
+  return To(p.getPropertyInst());
+}
+
+template <class To,
+          class From,
+          std::enable_if_t<std::is_base_of_v<Property, To>, bool> = true,
+          std::enable_if_t<std::is_base_of_v<Property, From>, bool> = true>
+std::optional<To> try_cast(const From &p) {
+  if (isa<To>(p)) {
+    return To(p.getPropertyInst());
+  } else {
+    return std::nullopt;
+  }
 }
 
 } // namespace llvm::memoir
