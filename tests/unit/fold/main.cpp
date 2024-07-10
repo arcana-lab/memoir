ce83a385266d198a2b2bbef8a6b478db9e8d5fa9
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-10 10:20:18 -0500

    [tests][unit] Added the classic histogram of sequence test

--- tests/unit/fold/main.cpp
+++ tests/unit/fold/main.cpp
@@ -31,6 +31,22 @@ uint32_t sum_assoc(uint32_t accum, uint32_t k, uint32_t v) {
   return accum + k + v;
 }
 
+collection_ref accum_hist_seq(collection_ref accum, size_t i, uint32_t v) {
+  auto assoc_t = memoir_assoc_type(memoir_u32_t, memoir_u32_t);
+  memoir_assert_collection_type(assoc_t, accum);
+  memoir_return_type(assoc_t);
+
+  if (not memoir_assoc_has(accum, v)) {
+    memoir_assoc_insert(accum, v);
+    memoir_assoc_write(u32, 1, accum, v);
+  } else {
+    auto old = memoir_assoc_read(u32, accum, v);
+    memoir_assoc_write(u32, 1 + old, accum, v);
+  }
+
+  return accum;
+}
+
 int main() {
 
   TEST(fold_seq) {
@@ -77,6 +93,24 @@ int main() {
     for (size_t i = 0; i < 100; ++i) {
       EXPECT(memoir_index_read(u32, accum, i) == 1, "differs!");
     }
+  }
+
+  TEST(accum_assoc) {
+    auto seq = memoir_allocate_sequence(memoir_u32_t, 100);
+
+    for (size_t i = 0; i < 100; ++i) {
+      memoir_index_write(u32, 1, seq, i);
+    }
+
+    // Compute the histogram of the sequence.
+    auto hist = memoir_fold(collection_ref,
+                            memoir_allocate_assoc(memoir_u32_t, memoir_u32_t),
+                            seq,
+                            accum_hist_seq);
+
+    // The result should be { 1 : 100 }
+    EXPECT(memoir_size(hist) == 1, "Too many elements!");
+    EXPECT(memoir_assoc_read(u32, hist, 1) == 100, "Incorrect count for key=1");
   }
 
   TEST(close_immut_scalar) {
