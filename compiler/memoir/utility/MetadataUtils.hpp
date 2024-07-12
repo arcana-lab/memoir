5f367d91e0a9d49874eab608d2f578c62fe77b56
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-12 15:04:28 -0500

    [compiler][utility] Fixed a compiler warning in MetadataUtils

--- compiler/memoir/utility/MetadataUtils.hpp
+++ compiler/memoir/utility/MetadataUtils.hpp
@@ -11,11 +11,9 @@
 #define VALUE_OPERAND(CLASS, NAME, OP_NUM)                                     \
   llvm::Value &CLASS::get##NAME() const {                                      \
     auto &metadata = this->getArgumentMD();                                    \
-    auto &value_as_metadata =                                                  \
-        MEMOIR_SANITIZE( \ 
-      dyn_cast<llvm::ValueAsMetadata>(&argument_metadata),                     \
-                         #NAME " operand of " #CLASS                           \
-                               " is not a ValueAsMetadata");                   \
+    auto &value_as_metadata = MEMOIR_SANITIZE(                                 \
+        dyn_cast<llvm::ValueAsMetadata>(&argument_metadata),                   \
+        #NAME " operand of " #CLASS " is not a ValueAsMetadata");              \
     auto &value =                                                              \
         MEMOIR_SANITIZE(argument_value_as_metadata.getValue(),                 \
                         #NAME " operand of " #CLASS " is a NULL value!");      \
