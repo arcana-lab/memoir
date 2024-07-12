5a7b44c4f5f95c8f6f2bcfb70d57029a2bcce869
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-07-12 09:54:36 -0500

    [compiler][ir] Removed some deprecated methods

--- compiler/memoir/ir/src/Instructions/SSAInsts.cpp
+++ compiler/memoir/ir/src/Instructions/SSAInsts.cpp
@@ -7,57 +7,15 @@
 
 namespace llvm::memoir {
 
-/*
- * UsePHIInst implementation
- */
+// UsePHIInst implementation
 RESULTANT(UsePHIInst, ResultCollection)
 OPERAND(UsePHIInst, UsedCollection, 0)
-
-llvm::Instruction &UsePHIInst::getUseInst() const {
-  // Look for metadata attached to this use.
-  auto *value = MetadataManager::getMetadata(this->getCallInst(),
-                                             MetadataType::MD_USE_PHI);
-  auto *inst = dyn_cast_or_null<llvm::Instruction>(value);
-  MEMOIR_NULL_CHECK(inst, "Couldn't get the UseInst");
-  return *inst;
-}
-
-void UsePHIInst::setUseInst(llvm::Instruction &I) const {
-  // Update this information in the MetadataManager
-  MetadataManager::setMetadata(this->getCallInst(),
-                               MetadataType::MD_USE_PHI,
-                               &I);
-}
-
-void UsePHIInst::setUseInst(MemOIRInst &I) const {
-  this->setUseInst(I.getCallInst());
-}
 
 TO_STRING(UsePHIInst)
 
 // DefPHIInst implementation
 RESULTANT(DefPHIInst, ResultCollection)
 OPERAND(DefPHIInst, DefinedCollection, 0)
-
-llvm::Instruction &DefPHIInst::getDefInst() const {
-  // Look for metadata attached to this definition.
-  auto *value = MetadataManager::getMetadata(this->getCallInst(),
-                                             MetadataType::MD_DEF_PHI);
-  auto *inst = dyn_cast_or_null<llvm::Instruction>(value);
-  MEMOIR_NULL_CHECK(inst, "Couldn't get the DefInst");
-  return *inst;
-}
-
-void DefPHIInst::setDefInst(llvm::Instruction &I) const {
-  // Update this information in the MetadataManager
-  MetadataManager::setMetadata(this->getCallInst(),
-                               MetadataType::MD_DEF_PHI,
-                               &I);
-}
-
-void DefPHIInst::setDefInst(MemOIRInst &I) const {
-  this->setDefInst(I.getCallInst());
-}
 
 TO_STRING(DefPHIInst)
 
