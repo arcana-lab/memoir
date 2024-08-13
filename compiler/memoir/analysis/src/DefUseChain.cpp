1d2c2bbe32b852696b1ed66e00b5d3d3e562896d
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-13 15:44:27 -0500

    [compiler][analysis] Fleshed out implementation of DefUseChain

--- compiler/memoir/analysis/src/DefUseChain.cpp
+++ compiler/memoir/analysis/src/DefUseChain.cpp
@@ -1,5 +1,7 @@
 #include "memoir/analysis/DefUseChain.hpp"
 
+#include "memoir/support/Print.hpp"
+
 namespace llvm::memoir {
 
 // DefUseChainResult
@@ -38,6 +40,149 @@ set<llvm::Value *> DefUseChainResult::chain(llvm::Value &V) {
 }
 
 // DefUseChainAnalysis
+static void collect_uses(UnionFind<llvm::Value *> &chains,
+                         set<llvm::Value *> &visited,
+                         llvm::Value &V) {
+  // Check that we have not already visited this value.
+  if (visited.count(&V) > 0) {
+    return;
+  } else {
+    visited.insert(&V);
+  }
+
+  // For each user:
+  for (auto &use : V.uses()) {
+    // Unpack the use.
+    auto *user = use.getUser();
+
+    // If the user is not an instruction, skip it.
+    auto *user_as_inst = dyn_cast<llvm::Instruction>(user);
+    if (not user_as_inst) {
+      continue;
+    }
+
+    // If the user is a PHI node, add it to the Def-Use chain and merge all .
+    if (auto *phi = dyn_cast<llvm::PHINode>(user_as_inst)) {
+      chains.find(user);
+      chains.merge(&V, user);
+    }
+
+    // If the user is a MEMOIR instruction, add it to the Def-Use chain.
+    else if (auto *memoir_inst = into<MemOIRInst>(user_as_inst)) {
+      // Skip type instructions.
+      if (isa<AssertTypeInst>(memoir_inst)) {
+        continue;
+      }
+
+      // Handle basic operations.
+      if (auto *write = dyn_cast<WriteInst>(memoir_inst)) {
+        auto &resultant = write->getCallInst();
+        chains.merge(&V, &resultant);
+        collect_uses(chains, visited, resultant);
+      } else if (auto *insert = dyn_cast<InsertInst>(memoir_inst)) {
+
+        // If this is a SeqInsertSeqInst, check that the use is the base
+        // collection.
+        if (auto *seq_insert_seq = dyn_cast<SeqInsertSeqInst>(insert)) {
+          if (use.getOperandNo()
+              != seq_insert_seq->getBaseCollectionAsUse().getOperandNo()) {
+            continue;
+          }
+        }
+
+        auto &resultant = insert->getCallInst();
+        chains.merge(&V, &resultant);
+        collect_uses(chains, visited, resultant);
+      } else if (auto *remove = dyn_cast<RemoveInst>(memoir_inst)) {
+        auto &resultant = remove->getCallInst();
+        chains.merge(&V, &resultant);
+        collect_uses(chains, visited, resultant);
+      } else if (auto *swap = dyn_cast<SeqSwapInst>(memoir_inst)) {
+        warnln("SeqSwapInst is currently unhandled!");
+      } else if (auto *swap = dyn_cast<SwapInst>(memoir_inst)) {
+        auto &resultant = swap->getCallInst();
+        chains.merge(&V, &resultant);
+        collect_uses(chains, visited, resultant);
+      }
+
+      // If the instruction is a fold, and the use is the initial value, link
+      // the interprocedural def-use chain.
+      if (auto *fold = dyn_cast<FoldInst>(memoir_inst)) {
+        // If the value is the initial value operand, add the resultant to the
+        // def-use chain.
+        if (&fold->getInitial() == &V) {
+
+          auto &fold_result = fold->getResult();
+
+          chains.find(&fold_result);
+          chains.merge(&V, &fold_result);
+
+          collect_uses(chains, visited, fold_result);
+
+          continue;
+        }
+
+        // If the value is the collection being folded over, do nothing.
+        if (&fold->getCollection() == &V) {
+          continue;
+        }
+
+        // Otherwise, link with the corresponding argument.
+        auto &arg = fold->getClosedArgument(use);
+
+        chains.merge(&V, &arg);
+
+        collect_uses(chains, visited, arg);
+      }
+    }
+
+    // If the instruction is a call, link the interprocedural def-use chain.
+    else if (auto *call = dyn_cast<llvm::CallBase>(user_as_inst)) {
+
+      // Gather all arguments to unify.
+      set<llvm::Argument *> arguments = {};
+
+      // Get the called function.
+      auto *called_function = call->getCalledFunction();
+
+      // If this is an indirect call, gather all possibly called functions.
+      // TODO: this could be improved by using NOELLE's complete call graph or
+      // LLVM points-to analysis.
+      if (not called_function) {
+        auto *function_type = call->getFunctionType();
+
+        // For each function that shares the function type.
+        auto *module = user_as_inst->getModule();
+        for (auto &F : *module) {
+          // Check the function type.
+          if (F.getFunctionType() == function_type) {
+
+            // Get the corresponding argument.
+            auto *arg = F.getArg(use.getOperandNo());
+
+            arguments.insert(arg);
+          }
+        }
+      } else {
+        // Otherwise, this is a direct call, unify the corresponding argument.
+        auto *arg = called_function->getArg(use.getOperandNo());
+
+        arguments.insert(arg);
+      }
+
+      // For each argument, unify it with the value being inspected.
+      for (auto *arg : arguments) {
+        chains.merge(&V, arg);
+
+        // Recurse on the argument.
+        collect_uses(chains, visited, *arg);
+      }
+    }
+  }
+
+  return;
+}
+
 DefUseChainResult DefUseChainAnalysis::run(llvm::Module &M,
                                            llvm::ModuleAnalysisManager &MAM) {
   DefUseChainResult result;
@@ -56,15 +201,13 @@ DefUseChainResult DefUseChainAnalysis::run(llvm::Module &M,
   }
 
   // Collect the def-use chains with forward analysis.
+  set<llvm::Value *> visited = {};
   for (auto *decl : result.decls) {
     // Insert the declaration into its own chain.
     result.chains.insert(decl);
 
-    // Add each user to the chain.
-    for (auto *user : decl->users()) {
-      result.chains.insert(user);
-      result.chains.merge(decl, user);
-    }
+    // Collect all uses of the value.
+    collect_uses(result.chains, visited, *decl);
   }
 
   // Reify the def-use chains.
