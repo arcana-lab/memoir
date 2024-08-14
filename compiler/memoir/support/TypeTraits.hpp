674e4d9d17f673e101b81087f97c79ca44f621ab
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-14 12:17:04 -0500

    [memoir][support] Added missing include guards

--- compiler/memoir/support/TypeTraits.hpp
+++ compiler/memoir/support/TypeTraits.hpp
@@ -1,3 +1,6 @@
+#ifndef MEMOIR_SUPPORT_TYPETRAITS_H
+#define MEMOIR_SUPPORT_TYPETRAITS_H
+
 #include <type_traits>
 
 template <class T, template <class...> class Template>
@@ -6,3 +9,5 @@ inline constexpr bool is_instance_of_v = std::false_type{};
 template <template <class...> class Template, class... Args>
 inline constexpr bool is_instance_of_v<Template<Args...>, Template> =
     std::true_type{};
+
+#endif // MEMOIR_SUPPORT_TYPETRAITS_H
