70ebe0ecc739188f80c374171df47eaefccd661d
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-14 16:14:38 -0500

    [folio][analysis] Added first draft implementation of constraint inference

---
+++ compiler/folio/analysis/ConstraintInference.hpp
@@ -0,0 +1,85 @@
+#ifndef FOLIO_CONSTRAINTINFERENCE_H
+#define FOLIO_CONSTRAINTINFERENCE_H
+
+// C++
+#include <string>
+#include <type_traits>
+
+// LLVM
+#include "llvm/Pass.h"
+
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Value.h"
+
+// MEMOIR
+#include "memoir/ir/Instructions.hpp"
+
+#include "memoir/support/InternalDatatypes.hpp"
+#include "memoir/support/TypeTraits.hpp"
+
+// Folio
+#include "folio/analysis/Constraints.hpp"
+
+namespace folio {
+
+// Analysis result.
+using ConstraintSet = typename llvm::memoir::ordered_set<Constraint>;
+
+struct Constraints {
+public:
+  const ConstraintSet &get(llvm::Value &V) const;
+
+protected:
+  llvm::memoir::map<llvm::Value *, ConstraintSet> value_to_constraints;
+
+  void add(llvm::Value &V, Constraint C) {
+    this->value_to_constraints[&V].insert(C);
+  }
+
+  friend class ConstraintInference;
+  friend class ConstraintInferenceDriver;
+};
+
+// Driver
+class ConstraintInferenceDriver {
+public:
+  ConstraintInferenceDriver(Constraints &constraints, llvm::Module &M);
+
+protected:
+  /**
+   * Propagate the constraint to the given value.
+   *
+   * @param constraints the state
+   * @param V the value being propagated to
+   * @param C the constraint being propagated
+   * @returns true if V's constraints were changed, false otherwise
+   */
+  bool propagate(llvm::Value &V, ConstraintSet &C);
+
+  void init();
+
+  void infer();
+
+  Constraints &constraints;
+  llvm::Module &M;
+};
+
+// Analysis.
+class ConstraintInference
+  : public llvm::AnalysisInfoMixin<ConstraintInference> {
+
+  friend struct llvm::AnalysisInfoMixin<ConstraintInference>;
+
+  static llvm::AnalysisKey Key;
+
+public:
+  using Result = Constraints;
+
+  Result run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM);
+};
+
+} // namespace folio
+
+#endif // FOLIO_CONSTRAINTINFERENCE_H
---
+++ compiler/folio/analysis/src/ConstraintInference.cpp
@@ -0,0 +1,334 @@
+#include "memoir/support/Casting.hpp"
+
+#include "folio/analysis/ConstraintInference.hpp"
+
+using namespace llvm::memoir;
+
+namespace folio {
+
+void ConstraintInferenceDriver::init() {
+  for (auto &F : this->M) {
+    if (F.empty()) {
+      continue;
+    }
+
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+
+        // TODO: check if the instruction should be considered hot, if so apply
+        // the FastOperationConstraint as well.
+        // Set the is_hot variable to true if that is the case.
+        auto is_hot = false;
+
+        if (auto *read = into<ReadInst>(&I)) {
+          auto &collection = read->getObjectOperand();
+          auto *type = type_of(collection);
+
+          if (isa_and_nonnull<SequenceType>(type)
+              or isa_and_nonnull<AssocArrayType>(type)) {
+
+            this->constraints.add(collection, OperationConstraint<ReadInst>());
+            if (is_hot) {
+              this->constraints.add(collection,
+                                    FastOperationConstraint<ReadInst>());
+            }
+          }
+
+        } else if (auto *write = into<WriteInst>(&I)) {
+          auto *type = type_of(*write);
+
+          if (isa_and_nonnull<SequenceType>(type)
+              or isa_and_nonnull<AssocArrayType>(type)) {
+
+            this->constraints.add(I, OperationConstraint<ReadInst>());
+            if (is_hot) {
+              this->constraints.add(I, FastOperationConstraint<ReadInst>());
+            }
+          }
+        } else if (into<InsertInst>(&I)) {
+          this->constraints.add(I, OperationConstraint<InsertInst>());
+          if (is_hot) {
+            this->constraints.add(I, FastOperationConstraint<InsertInst>());
+          }
+        } else if (into<RemoveInst>(&I)) {
+          this->constraints.add(I, OperationConstraint<RemoveInst>());
+          if (is_hot) {
+            this->constraints.add(I, FastOperationConstraint<RemoveInst>());
+          }
+        } else if (into<SwapInst>(&I)) {
+          this->constraints.add(I, OperationConstraint<SwapInst>());
+          if (is_hot) {
+            this->constraints.add(I, FastOperationConstraint<SwapInst>());
+          }
+        } else if (into<AssocHasInst>(&I)) {
+          this->constraints.add(I, OperationConstraint<AssocHasInst>());
+          if (is_hot) {
+            this->constraints.add(I, FastOperationConstraint<AssocHasInst>());
+          }
+        } else if (auto *fold = into<FoldInst>(&I)) {
+          auto &collection = fold->getCollection();
+
+          this->constraints.add(collection, OperationConstraint<FoldInst>());
+          if (is_hot) {
+            this->constraints.add(collection,
+                                  FastOperationConstraint<FoldInst>());
+          }
+        } else if (auto *rfold = into<ReverseFoldInst>(&I)) {
+          auto &collection = rfold->getCollection();
+
+          this->constraints.add(collection,
+                                OperationConstraint<ReverseFoldInst>());
+          if (is_hot) {
+            this->constraints.add(collection,
+                                  FastOperationConstraint<ReverseFoldInst>());
+          }
+        }
+
+        // TODO: check if we have references live across operations that impact
+        // the index space.
+      }
+    }
+  }
+
+  return;
+}
+
+bool ConstraintInferenceDriver::propagate(llvm::Value &V, ConstraintSet &C) {
+  // Get the constraints for V.
+  auto &V_C = this->constraints.value_to_constraints[&V];
+
+  // Save the original size so that we can check for modification later.
+  auto original_size = V_C.size();
+
+  // Insert the incoming constraints into V_C
+  V_C.insert(C.begin(), C.end());
+
+  // Return true if the size changed.
+  return original_size != V_C.size();
+}
+
+void ConstraintInferenceDriver::infer() {
+
+  // Unpack the underlying constraint mapping.
+  map<llvm::Value *, ConstraintSet> &mapping =
+      this->constraints.value_to_constraints;
+
+  // Construct a mapping from functions to their possible callees.
+  map<llvm::Function *, ordered_set<llvm::CallBase *>> possible_callers = {};
+  map<llvm::CallBase *, ordered_set<llvm::Function *>> possible_callees = {};
+
+  for (auto &F : M) {
+    if (F.empty()) {
+      continue;
+    }
+
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto *call = dyn_cast<llvm::CallBase>(&I)) {
+          if (auto *fold = into<FoldInst>(&I)) {
+
+            auto &function = fold->getFunction();
+            possible_callers[&function].insert(call);
+
+            continue;
+          }
+
+          // If it is a direct call, record it.
+          if (auto *called_function = call->getCalledFunction()) {
+            possible_callers[called_function].insert(call);
+            possible_callees[call].insert(called_function);
+
+            continue;
+          }
+
+          // Otherwise, find all possible calls.
+          auto *function_type = call->getFunctionType();
+
+          // A call may occur if the function and call share a function type.
+          // NOTE: this can be made less naive with NOELLE's complete call
+          // graph, or points-to analysis
+          for (auto &OtherF : M) {
+            if (OtherF.getFunctionType() == function_type) {
+              possible_callers[&OtherF].insert(call);
+              possible_callees[call].insert(&OtherF);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // Gather the initial worklist.
+  vector<llvm::Value *> worklist;
+  worklist.reserve(this->constraints.value_to_constraints.size());
+
+  for (auto [val, c] : this->constraints.value_to_constraints) {
+    worklist.push_back(val);
+  }
+
+  // Iterate until a fixed-point is reached.
+  while (not worklist.empty()) {
+    // Pop off of the worklist.
+    auto *current = worklist.back();
+    worklist.pop_back();
+
+    // Fetch the constraint set for the current value.
+    auto &current_constraints = mapping[current];
+
+    // Propagate the constraint set to the used collections.
+    if (auto *arg = dyn_cast<llvm::Argument>(current)) {
+      // Unpack the argument's information.
+      auto arg_no = arg->getArgNo();
+      auto *parent_function = arg->getParent();
+
+      // If the current value is an argument, propagate the information to all
+      // possible callers.
+      for (auto *call : possible_callers[parent_function]) {
+        // If the caller is a FoldInst, handle it specially.
+        if (auto *fold = into<FoldInst>(call)) {
+          // TODO
+        }
+
+        // Otherwise, propagate the information to the used value.
+        auto &use = call->getOperandUse(arg_no);
+        auto &used = MEMOIR_SANITIZE(use.get(), "Used value is NULL");
+
+        // Propagate the constraints to the use.
+        auto changed = propagate(used, current_constraints);
+
+        // If the set of constraints changed, add the used variable to the
+        // worklist.
+        if (changed) {
+          worklist.push_back(&used);
+        }
+      }
+    } else if (auto *phi = dyn_cast<llvm::PHINode>(current)) {
+      // Propagate the PHI's constraints along all incoming edges.
+      for (auto &incoming_use : phi->incoming_values()) {
+        auto *incoming = incoming_use.get();
+
+        auto changed = propagate(*incoming, current_constraints);
+        if (changed) {
+          worklist.push_back(incoming);
+        }
+      }
+    } else if (auto *extract = dyn_cast<llvm::ExtractValueInst>(current)) {
+      // NOTE: this is a naive handling of tuples.
+      auto changed = propagate(*extract, current_constraints);
+      if (changed) {
+        worklist.push_back(extract);
+      }
+    } else if (auto *memoir_inst = into<MemOIRInst>(current)) {
+      if (auto *write = dyn_cast<WriteInst>(memoir_inst)) {
+        auto &collection = write->getObjectOperand();
+
+        auto changed = propagate(collection, current_constraints);
+        if (changed) {
+          worklist.push_back(&collection);
+        }
+      } else if (auto *insert = dyn_cast<InsertInst>(memoir_inst)) {
+        auto &collection = insert->getBaseCollection();
+
+        auto changed = propagate(collection, current_constraints);
+        if (changed) {
+          worklist.push_back(&collection);
+        }
+      } else if (auto *remove = dyn_cast<RemoveInst>(memoir_inst)) {
+        auto &collection = remove->getBaseCollection();
+
+        auto changed = propagate(collection, current_constraints);
+        if (changed) {
+          worklist.push_back(&collection);
+        }
+      } else if (auto *swap = dyn_cast<SwapInst>(memoir_inst)) {
+        auto &from_collection = swap->getFromCollection();
+        auto &to_collection = swap->getToCollection();
+
+        if (propagate(from_collection, current_constraints)) {
+          worklist.push_back(&from_collection);
+        }
+
+        if (propagate(to_collection, current_constraints)) {
+          worklist.push_back(&to_collection);
+        }
+      } else if (auto *fold = dyn_cast<FoldInst>(memoir_inst)) {
+        auto &collection = fold->getCollection();
+
+        if (propagate(collection, current_constraints)) {
+          worklist.push_back(&collection);
+        }
+      } else if (auto *copy = dyn_cast<CopyInst>(memoir_inst)) {
+        // NOTE: for simplicity we will force all copies to be the same
+        // selection as their input. If this assumption changes, this code needs
+        // to change with it!
+        auto &copied = copy->getCopiedCollection();
+
+        if (propagate(copied, current_constraints)) {
+          worklist.push_back(&copied);
+        }
+      } else if (auto *get = dyn_cast<GetInst>(memoir_inst)) {
+        // NOTE: we do not currently support nested collections, if that
+        // assumption changes additional handling needs to be added here.
+      } else if (auto *ret_phi = dyn_cast<RetPHIInst>(memoir_inst)) {
+        auto &collection = ret_phi->getInputCollection();
+
+        if (propagate(collection, current_constraints)) {
+          worklist.push_back(&collection);
+        }
+      }
+    } else if (auto *call = dyn_cast<llvm::CallBase>(current)) {
+
+      // For each possible callee, inspect it.
+      for (auto *callee : possible_callees[call]) {
+
+        // Find all return instructions in the function and propagate
+        // constraints to the returned value.
+        for (auto &BB : *callee) {
+          auto *terminator = BB.getTerminator();
+
+          if (auto *ret = dyn_cast<llvm::ReturnInst>(terminator)) {
+            // Get the returned value.
+            auto *returned = ret->getReturnValue();
+
+            // Propagate to the returned value.
+            if (propagate(*returned, current_constraints)) {
+              worklist.push_back(returned);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return;
+}
+
+ConstraintInferenceDriver::ConstraintInferenceDriver(Constraints &constraints,
+                                                     llvm::Module &M)
+  : constraints(constraints),
+    M(M) {
+  // Collect initial constraints.
+  this->init();
+
+  // Infer constraints.
+  this->infer();
+}
+
+// =======================
+// Pass
+Constraints ConstraintInference::run(llvm::Module &M,
+                                     llvm::ModuleAnalysisManager &MAM) {
+
+  // Initialize the result.
+  Constraints result;
+
+  // Run the constraint inference driver.
+  ConstraintInferenceDriver driver(result, M);
+
+  // Return the result.
+  return result;
+}
+
+llvm::AnalysisKey ConstraintInference::Key;
+
+} // namespace folio
