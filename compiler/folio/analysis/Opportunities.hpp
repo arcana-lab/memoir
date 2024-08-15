3c9e537ff3a20a5010cd13df2dd241e9a7f171b8
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-15 17:20:07 -0500

    [folio][analysis] Added stubs for the Opportunities and OpportunityDiscovery results

---
+++ compiler/folio/analysis/Opportunities.hpp
@@ -0,0 +1,10 @@
+#ifndef FOLIO_OPPORTUNITIES_H
+#define FOLIO_OPPORTUNITIES_H
+
+namespace folio {
+
+struct Opportunity {};
+
+} // namespace folio
+
+#endif // FOLIO_OPPORTUNITIES_H
---
+++ compiler/folio/analysis/OpportunityDiscovery.hpp
@@ -0,0 +1,14 @@
+#ifndef FOLIO_OPPORTUNITYDISCOVERY_H
+#define FOLIO_OPPORTUNITYDISCOVERY_H
+
+#include "memoir/support/InternalDatatypes.hpp"
+
+#include "folio/analysis/Opportunities.hpp"
+
+namespace folio {
+
+using Opportunities = typename llvm::memoir::list<Opportunity>;
+
+}
+
+#endif // FOLIO_OPPORTUNITYDISCOVERY_H
