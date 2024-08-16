2d9f791bb5e638e4b0bf0c6ed3c2739491ad4cef
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-16 14:00:07 -0500

    [folio][passes] Refactored the FolioPass and added in all basic functionality needed to perform collection selection

--- compiler/folio/passes/src/Pass.cpp
+++ compiler/folio/passes/src/Pass.cpp
@@ -1,9 +1,13 @@
+#include "memoir/utility/Metadata.hpp"
+
 #include "memoir/support/Casting.hpp"
 #include "memoir/support/InternalDatatypes.hpp"
 #include "memoir/support/Print.hpp"
 
 #include "folio/analysis/ConstraintInference.hpp"
 #include "folio/analysis/OpportunityDiscovery.hpp"
+
+#include "folio/transforms/SelectionMonomorphization.hpp"
 
 #include "folio/solver/Implementation.hpp"
 #include "folio/solver/Solver.hpp"
@@ -13,6 +17,39 @@
 using namespace llvm::memoir;
 
 namespace folio {
+
+namespace detail {
+
+void transform(llvm::Module &M, const Candidate &candidate) {
+
+  // First, annotate the selections while the LLVM Values are valid.
+  for (const auto &[value, impl] : candidate.selections()) {
+    println(*value, " --> ", impl->name());
+
+    // Get the value as an instruction.
+    auto *inst = dyn_cast<llvm::Instruction>(value);
+    if (not inst) {
+      warnln("Cannot annotate an argument with a selection.");
+      continue;
+    }
+
+    // Attach the SelectionMetadata to the instruction.
+    auto metadata = Metadata::get_or_add<SelectionMetadata>(*inst);
+
+    // Set the implementation to that selected.
+    metadata.setImplementation(impl->name());
+  }
+
+  // Then, transform the program to exploit all opportunities.
+  // TODO
+
+  // Finally, perform selection monomorphization.
+  SelectionMonomorphization monomorph(M);
+
+  return;
+}
+
+} // namespace detail
 
 llvm::PreservedAnalyses FolioPass::run(llvm::Module &M,
                                        llvm::ModuleAnalysisManager &MAM) {
@@ -50,10 +87,29 @@ llvm::PreservedAnalyses FolioPass::run(llvm::Module &M,
   Opportunities opportunities;
 
   // Instantiate all available implementations.
-  Implementations implementations;
+  Implementations implementations = {
+    { "stl_vector",
+      SeqImplementation("stl_vector", { PointerStableConstraint() }) },
+    // { "stl_list", SeqImplementation("stl_list", {}) },
+    { "stl_unordered_map",
+      AssocImplementation("stl_unordered_map", { PointerStableConstraint() }) },
+    { "stl_map", AssocImplementation("stl_map", {}) },
+  };
 
   // Pass the analysis results to the solver.
-  Solver(selectable, constraints, opportunities, implementations);
+  Solver solver(selectable, constraints, opportunities, implementations);
+
+  // If there are no candidates, we're all done.
+  if (solver.candidates().empty()) {
+    println("Empty candidate");
+    return llvm::PreservedAnalyses::all();
+  }
+
+  // Select a candidate.
+  auto &selected_candidate = solver.candidates().front();
+
+  // Transform the program to utilize the selected candidate.
+  detail::transform(M, selected_candidate);
 
   // All done.
   return llvm::PreservedAnalyses::none();
