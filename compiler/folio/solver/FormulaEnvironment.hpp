4d09be982958ff94280ba7b1f284220cef2a2aed
Tommy McMichen <mcmichen@u.northwestern.edu>
2024-08-19 17:51:47 -0500

    [folio][solver] Added FormulaEnvironment definition

---
+++ compiler/folio/solver/FormulaEnvironment.hpp
@@ -0,0 +1,35 @@
+#include "llvm/IR/Value.h"
+
+#include "memoir/support/InternalDatatypes.hpp"
+
+namespace folio {
+
+/**
+ * The FormulaEnvironment stores the mappings from ASP tokens
+ * to LLVM objects.
+ */
+struct FormulaEnvironment {
+public:
+  /**
+   * Lookup the LLVM value for the given LLVM value.
+   *
+   * @param id the identifier
+   * @returns the corresponding LLVM Value
+   */
+  llvm::Value &lookup(uint32_t id) const;
+
+  /**
+   * Get the identifier for the given LLVM Value, if it does not already exist,
+   * create a unique identifier for it.
+   *
+   * @param V the LLVM Value
+   * @returns the corresponding identifier
+   */
+  uint32_t get_id(llvm::Value &V);
+
+protected:
+  llvm::memoir::map<llvm::Value *, uint32_t> _value_ids = {};
+  llvm::memoir::map<uint32_t, llvm::Value *> _id_values = {};
+  uint32_t _current_id = 0;
+};
+} // namespace folio
