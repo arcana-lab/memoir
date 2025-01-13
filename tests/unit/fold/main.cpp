5d5d18477f36b0f45307967f080eae1731df4b8a
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-01-13 17:14:03 -0600

    [tests][unit][fold] Updated tests

--- tests/unit/fold/main.cpp
+++ tests/unit/fold/main.cpp
@@ -34,10 +34,10 @@ uint32_t sum_seq_mut_set(uint32_t accum,
   memoir_assert_collection_type(memoir_assoc_type(memoir_u32_t, memoir_void_t),
                                 seen);
 
-  if (memoir_assoc_has(seen, v)) {
+  if (memoir_has(seen, v)) {
     return accum;
   } else {
-    memoir_assoc_insert(seen, v);
+    memoir_insert(seen, v);
   }
 
   return accum + v;
@@ -56,12 +56,12 @@ collection_ref accum_hist_seq(collection_ref accum, size_t i, uint32_t v) {
   memoir_assert_collection_type(assoc_t, accum);
   memoir_return_type(assoc_t);
 
-  if (not memoir_assoc_has(accum, v)) {
-    memoir_assoc_insert(accum, v);
-    memoir_assoc_write(u32, 1, accum, v);
+  if (not memoir_has(accum, v)) {
+    memoir_insert(accum, v);
+    memoir_write(u32, 1, accum, v);
   } else {
-    auto old = memoir_assoc_read(u32, accum, v);
-    memoir_assoc_write(u32, 1 + old, accum, v);
+    auto old = memoir_read(u32, accum, v);
+    memoir_write(u32, 1 + old, accum, v);
   }
 
   return accum;
@@ -72,7 +72,7 @@ collection_ref push_each(collection_ref accum, size_t i, uint32_t v) {
   memoir_assert_collection_type(seq_t, accum);
   memoir_return_type(seq_t);
 
-  memoir_seq_insert(u32, v, accum, memoir_end());
+  memoir_insert_value(v, accum, memoir_end());
 
   return accum;
 }
@@ -87,34 +87,34 @@ int main() {
       memoir_index_write(u32, 1, seq, i);
     }
 
-    auto sum = memoir_fold(u32, 0, seq, sum_seq);
+    auto sum = memoir_fold(u32, sum_seq, 0, seq);
 
     EXPECT(sum == 100, "Sum incorrect!");
   }
 
   TEST(fold_assoc) {
-    auto assoc = memoir_allocate_assoc_array(memoir_u32_t, memoir_u32_t);
+    auto assoc = memoir_allocate_assoc(memoir_u32_t, memoir_u32_t);
 
-    memoir_assoc_insert(assoc, 10);
-    memoir_assoc_write(u32, 1, assoc, 10);
-    memoir_assoc_insert(assoc, 20);
-    memoir_assoc_write(u32, 2, assoc, 20);
-    memoir_assoc_insert(assoc, 30);
-    memoir_assoc_write(u32, 3, assoc, 30);
+    memoir_insert(assoc, 10);
+    memoir_write(u32, 1, assoc, 10);
+    memoir_insert(assoc, 20);
+    memoir_write(u32, 2, assoc, 20);
+    memoir_insert(assoc, 30);
+    memoir_write(u32, 3, assoc, 30);
 
-    auto sum = memoir_fold(u32, 0, assoc, sum_assoc);
+    auto sum = memoir_fold(u32, sum_assoc, 0, assoc);
 
     EXPECT(sum == (10 + 1 + 2 + 20 + 3 + 30), "Sum incorrect!");
   }
 
   TEST(fold_set) {
-    auto set = memoir_allocate_assoc_array(memoir_u32_t, memoir_void_t);
+    auto set = memoir_allocate_assoc(memoir_u32_t, memoir_void_t);
 
-    memoir_assoc_insert(set, 10);
-    memoir_assoc_insert(set, 20);
-    memoir_assoc_insert(set, 30);
+    memoir_insert(set, 10);
+    memoir_insert(set, 20);
+    memoir_insert(set, 30);
 
-    auto sum = memoir_fold(u32, 0, set, sum_set);
+    auto sum = memoir_fold(u32, sum_set, 0, set);
 
     EXPECT(sum == (10 + 20 + 30), "Sum incorrect!");
   }
@@ -127,13 +127,14 @@ int main() {
       memoir_index_write(u32, 1, seq, i);
     }
 
-    auto accum = memoir_fold(collection_ref,
+    auto accum = memoir_fold(ref,
+                             accum_seq,
                              memoir_allocate_sequence(memoir_u32_t, 100),
-                             seq,
-                             accum_seq);
+                             seq);
 
     for (size_t i = 0; i < 100; ++i) {
-      EXPECT(memoir_index_read(u32, accum, i) == 1, "differs!");
+      EXPECT(memoir_index_read(u32, accum, i) == 1,
+             "accumulated sequence differs!");
     }
   }
 
@@ -145,14 +146,14 @@ int main() {
     }
 
     // Compute the histogram of the sequence.
-    auto hist = memoir_fold(collection_ref,
+    auto hist = memoir_fold(ref,
+                            accum_hist_seq,
                             memoir_allocate_assoc(memoir_u32_t, memoir_u32_t),
-                            seq,
-                            accum_hist_seq);
+                            seq);
 
     // The result should be { 1 : 100 }
     EXPECT(memoir_size(hist) == 1, "Too many elements!");
-    EXPECT(memoir_assoc_read(u32, hist, 1) == 100, "Incorrect count for key=1");
+    EXPECT(memoir_read(u32, hist, 1) == 100, "Incorrect count for key=1");
   }
 
   TEST(close_immut_scalar) {
@@ -164,7 +165,7 @@ int main() {
 
     const uint32_t x = 10;
 
-    auto sum = memoir_fold(u32, 0, seq, sum_seq_times, x);
+    auto sum = memoir_fold(u32, sum_seq_times, 0, seq, memoir_closed(x));
 
     EXPECT(sum == 1000, "Sum incorrect!");
   }
@@ -173,13 +174,13 @@ int main() {
     auto seq = memoir_allocate_sequence(memoir_u32_t, 10);
 
     for (size_t i = 0; i < 10; ++i) {
-      memoir_index_write(u32, 1, seq, i);
+      memoir_write(u32, 1, seq, i);
     }
 
     uint32_t x;
     x = 1;
 
-    auto sum = memoir_fold(u32, 0, seq, sum_seq_times_mut, &x);
+    auto sum = memoir_fold(u32, sum_seq_times_mut, 0, seq, memoir_closed(&x));
 
     EXPECT(sum == (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10), "Sum incorrect!");
     EXPECT(x == 11, "x incorrect!");
@@ -195,7 +196,7 @@ int main() {
 
     auto set = memoir_allocate_assoc(memoir_u32_t, memoir_void_t);
 
-    auto sum = memoir_fold(u32, 0, seq, sum_seq_mut_set, set);
+    auto sum = memoir_fold(u32, sum_seq_mut_set, 0, seq, memoir_closed(set));
 
     EXPECT(sum == (0 + 1 + 2 + 3 + 4 + 5), "Sum incorrect!");
     EXPECT(memoir_size(set) == 6, "Size incorrect!");
@@ -206,24 +207,24 @@ int main() {
     auto seq = memoir_allocate_sequence(memoir_u32_t, 10);
 
     for (size_t i = 0; i < 10; ++i) {
-      memoir_index_write(u32, 9 - i, seq, i);
+      memoir_write(u32, 9 - i, seq, i);
     }
 
-    auto rev = memoir_rfold(collection_ref,
+    auto rev = memoir_rfold(ref,
+                            push_each,
                             memoir_allocate_sequence(memoir_u32_t, 0),
-                            seq,
-                            push_each);
+                            seq);
 
     EXPECT(memoir_size(rev) == 10, "wrong size!");
     for (size_t i = 0; i < 10; ++i) {
-      EXPECT(memoir_index_read(u32, rev, i) == i, "differs!");
+      EXPECT(memoir_read(u32, rev, i) == i, "differs!");
     }
   }
 
   TEST(empty) {
     auto seq = memoir_allocate_sequence(memoir_u32_t, 0);
 
-    auto sum = memoir_fold(u32, 0, seq, sum_seq);
+    auto sum = memoir_fold(u32, sum_seq, 0, seq);
 
     EXPECT(sum == 0, "differs!");
   }
