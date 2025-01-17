66dd9e66bb32ae6182a2e792eda7bb9a20a46092
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-01-16 18:35:42 -0600

    [compiler][ir] Added a file I forgot to commit forever ago

---
+++ compiler/memoir/ir/src/Instructions/EndInst.cpp
@@ -0,0 +1,9 @@
+#include "memoir/ir/Instructions.hpp"
+#include "memoir/utility/InstructionUtils.hpp"
+
+namespace llvm::memoir {
+
+RESULTANT(EndInst, Value)
+TO_STRING(EndInst)
+
+} // namespace llvm::memoir
