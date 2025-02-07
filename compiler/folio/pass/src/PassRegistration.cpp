63bf62c9721016f5228c8721d7c9bd131a0c0a3c
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-02-07 08:32:25 -0600

    [compiler][folio] Updated pass registration

--- compiler/folio/pass/src/PassRegistration.cpp
+++ compiler/folio/pass/src/PassRegistration.cpp
@@ -3,13 +3,16 @@
 #include "llvm/Transforms/Utils/LCSSA.h"
 #include "llvm/Transforms/Utils/LoopSimplify.h"
 
+// MEMOIR
+#include "memoir/passes/Passes.hpp"
+
 // Folio
 #include "folio/pass/Pass.hpp"
 
-#include "folio/analysis/ConstraintInference.hpp"
-#include "folio/analysis/ContentAnalysis.hpp"
+// #include "folio/analysis/ConstraintInference.hpp"
+// #include "folio/analysis/ContentAnalysis.hpp"
 
-#include "folio/opportunities/Analysis.hpp"
+// #include "folio/opportunities/Analysis.hpp"
 
 using namespace folio;
 
@@ -26,43 +29,57 @@ static auto adapt(T &&fp) {
     return true;                                                               \
   }
 
+void folio_selection(llvm::ModulePassManager &MPM) {
+  MPM.addPass(folio::FolioPass());
+  MPM.addPass(llvm::memoir::TempArgReificationPass());
+
+  return;
+}
+
 // Register the passes and pipelines with the new pass manager
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION,
-    "folio",
-    LLVM_VERSION_STRING,
-    [](llvm::PassBuilder &PB) {
-      // Register transforation passes.
-      PB.registerPipelineParsingCallback(
-          [](llvm::StringRef name,
-             llvm::ModulePassManager &MPM,
-             llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
-            if (name == "folio") {
-              MPM.addPass(folio::FolioPass());
-              return true;
-            }
+  return { LLVM_PLUGIN_API_VERSION,
+           "folio",
+           LLVM_VERSION_STRING,
+           [](llvm::PassBuilder &PB) {
+             // Register transforation passes.
+             PB.registerPipelineParsingCallback(
+                 [](llvm::StringRef name,
+                    llvm::ModulePassManager &MPM,
+                    llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                   if (name == "folio-selection") {
+                     folio_selection(MPM);
+                     return true;
+                   }
 
-            return false;
-          });
+                   if (name == "folio") {
+                     llvm::memoir::raise_memoir(MPM);
+                     folio_selection(MPM);
+                     llvm::memoir::lower_memoir(MPM);
+                     return true;
+                   }
 
-      // Register module analyses.
-      PB.registerAnalysisRegistrationCallback(
-          [](llvm::ModuleAnalysisManager &MAM) {
-            MAM.registerPass([&] { return folio::ConstraintInference(); });
-            MAM.registerPass([&] { return folio::ContentAnalysis(); });
-            MAM.registerPass([&] { return folio::OpportunityAnalysis(); });
-#define OPPORTUNITY(CLASS)                                                     \
-  MAM.registerPass([&] { return folio::CLASS##Analysis(); });
-#include "folio/opportunities/Opportunities.def"
-          });
+                   return false;
+                 });
 
-      // Register function analyses.
-      PB.registerAnalysisRegistrationCallback(
-          [](llvm::FunctionAnalysisManager &FAM) {
-            // None.
-          });
-    }
-  };
+             // Register module analyses.
+             PB.registerAnalysisRegistrationCallback(
+                 [](llvm::ModuleAnalysisManager &MAM) {
+                   // MAM.registerPass([&] { return
+                   // folio::ConstraintInference(); }); MAM.registerPass([&] {
+                   // return folio::ContentAnalysis(); }); MAM.registerPass([&]
+                   // { return folio::OpportunityAnalysis(); });
+                   // #define OPPORTUNITY(CLASS) \
+//   MAM.registerPass([&] { return folio::CLASS##Analysis();
+                   //   });
+                   // #include "folio/opportunities/Opportunities.def"
+                 });
+
+             // Register function analyses.
+             PB.registerAnalysisRegistrationCallback(
+                 [](llvm::FunctionAnalysisManager &FAM) {
+                   // None.
+                 });
+           } };
 }
