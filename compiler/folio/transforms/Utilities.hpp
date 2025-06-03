8fa518bfc46ab65b7db39a81161019d388624fc5
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-06-03 07:49:49 -0500

    [compiler][folio][transforms] Refactored, moved forward_analysis and is_last_index to utils

--- compiler/folio/transforms/Utilities.hpp
+++ compiler/folio/transforms/Utilities.hpp
@@ -4,14 +4,23 @@
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/User.h"
+#include "llvm/IR/Value.h"
 
-static llvm::Function *parent_function(llvm::Value &V) {
-  if (auto *arg = dyn_cast<llvm::Argument>(&V)) {
-    return arg->getParent();
-  } else if (auto *inst = dyn_cast<llvm::Instruction>(&V)) {
-    return inst->getFunction();
-  }
-  return nullptr;
-}
+#include "memoir/ir/Instructions.hpp"
+#include "memoir/support/DataTypes.hpp"
+
+namespace folio {
+
+llvm::Function *parent_function(llvm::Value &V);
+
+uint32_t forward_analysis(
+    llvm::memoir::Map<llvm::Function *, llvm::memoir::Set<llvm::Value *>>
+        &encoded);
+
+bool is_last_index(llvm::Use *use,
+                   llvm::memoir::AccessInst::index_op_iterator index_end);
+
+} // namespace folio
 
 #endif // FOLIO_TRANSFORMS_UTILITIES_h
