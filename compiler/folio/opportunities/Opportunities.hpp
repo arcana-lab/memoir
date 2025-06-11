1153970e6038759e79fd75f3dcd5f15750b2b963
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-06-11 17:20:01 -0500

    [compiler][folio] Deleted old files

--- compiler/folio/opportunities/Opportunities.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef FOLIO_OPPORTUNITIES_H
-#define FOLIO_OPPORTUNITIES_H
-
-#include "folio/opportunities/Opportunity.hpp"
-
-#include "folio/opportunities/ProxyOpportunity.hpp"
-
-#endif // FOLIO_OPPORTUNITIES_H
