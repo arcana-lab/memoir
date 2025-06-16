0c7e5c626c824971269f1cd63ef1b0c6b6e96931
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-06-16 10:26:47 -0500

    [compiler][folio][transforms] Added a helper type to the candidate to track mapping info

--- compiler/folio/transforms/Candidate.hpp
+++ compiler/folio/transforms/Candidate.hpp
@@ -15,12 +15,58 @@ namespace folio {
 struct Candidate : public llvm::memoir::Vector<ObjectInfo *> {
   using Base = typename llvm::memoir::Vector<ObjectInfo *>;
 
-  Candidate() : Base{}, decoded{}, encoded{}, added{}, encoded_values{} {}
+  Candidate()
+    : Base{},
+      decoded{},
+      encoded{},
+      added{},
+      encoded_values{},
+      encoder(),
+      decoder() {}
 
   // The uses prepared for transformation.
   llvm::memoir::Vector<CoalescedUses> decoded, encoded, added;
   llvm::memoir::Map<llvm::Function *, llvm::memoir::Set<llvm::Value *>>
       encoded_values;
+
+  // Information about the en/decoder mappings.
+  struct Mapping {
+    Mapping() : _alloc(NULL), _global(NULL), _locals{} {}
+
+    llvm::Value &alloc() const {
+      return *this->_alloc;
+    }
+
+    void alloc(llvm::Value &V) {
+      this->_alloc = &V;
+    }
+
+    llvm::GlobalVariable &global() const {
+      return *this->_global;
+    }
+
+    void global(llvm::GlobalVariable &GV) {
+      this->_global = &GV;
+    }
+
+    llvm::AllocaInst *local(llvm::Function &F) const {
+      auto found = this->_locals.find(&F);
+      if (found == this->_locals.end()) {
+        return NULL;
+      }
+
+      return found->second;
+    }
+
+    void local(llvm::Function &func, llvm::AllocaInst &alloca) {
+      this->_locals[&func] = &alloca;
+    }
+
+    llvm::Value *_alloc;
+    llvm::GlobalVariable *_global;
+    llvm::memoir::Map<llvm::Function *, llvm::AllocaInst *> _locals;
+  };
+  Mapping encoder, decoder;
 
   llvm::Function &function() const;
 
