ffe8ca78dd9ffb0abf2c0c1bc19f4127395ee96c
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-08-10 11:23:00 -0500

    [compiler][ir] Added an Object wrapper, which represents a nested object within an SSA value

---
+++ compiler/memoir/ir/Object.hpp
@@ -0,0 +1,58 @@
+#ifndef MEMOIR_IR_OBJECT_H
+#define MEMOIR_IR_OBJECT_H
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/IR/Value.h"
+
+#include "memoir/ir/Types.hpp"
+#include "memoir/support/DataTypes.hpp"
+
+namespace llvm::memoir {
+
+using Offset = unsigned;
+using Offsets = SmallVector<Offset>;
+using OffsetsRef = typename llvm::ArrayRef<Offset>;
+
+struct Object {
+  /** The SSA value of this object. */
+  llvm::Value &value() const;
+
+  /** The offset of the object within the SSA value. */
+  llvm::ArrayRef<Offset> offsets() const;
+
+  /** Type of the object */
+  Type &type() const;
+
+  /** Parent basic block of the object, if it exists */
+  llvm::BasicBlock *block() const;
+
+  /** Parent function of the object, if it exists */
+  llvm::Function *function() const;
+
+  /** Parent module of the object. */
+  llvm::Module &module() const;
+
+  // Constructors.
+  Object(llvm::Value &value, OffsetsRef offsets = {})
+    : _value(&value),
+      _offsets(offsets) {}
+
+  friend bool operator<(const Object &lhs, const Object &rhs);
+  friend bool operator==(const Object &lhs, const Object &rhs);
+
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os,
+                                       const Object &obj);
+
+protected:
+  llvm::Value *_value;
+  Offsets _offsets;
+};
+
+} // namespace llvm::memoir
+
+template <>
+struct std::hash<llvm::memoir::Object> {
+  std::size_t operator()(const llvm::memoir::Object &obj) const noexcept;
+};
+
+#endif // MEMOIR_IR_OBJECT_H
---
+++ compiler/memoir/ir/src/Object.cpp
@@ -0,0 +1,110 @@
+#include "memoir/ir/Object.hpp"
+#include "memoir/ir/ControlFlow.hpp"
+#include "memoir/ir/TypeCheck.hpp"
+
+namespace llvm::memoir {
+
+llvm::Value &Object::value() const {
+  return *this->_value;
+}
+
+OffsetsRef Object::offsets() const {
+  return this->_offsets;
+}
+
+Type &Object::type() const {
+  auto *type = type_of(this->value());
+
+  for (auto offset : this->offsets()) {
+    if (auto *tuple = dyn_cast<TupleType>(type)) {
+      type = &tuple->getFieldType(offset);
+    } else if (auto *collection = dyn_cast<CollectionType>(type)) {
+      type = &collection->getElementType();
+    }
+    MEMOIR_UNREACHABLE("Malformed object offset, or incorrect type");
+  }
+
+  return *type;
+}
+
+llvm::BasicBlock *Object::block() const {
+  return parent<llvm::BasicBlock>(this->value());
+}
+
+llvm::Function *Object::function() const {
+  return parent<llvm::Function>(this->value());
+}
+
+llvm::Module &Object::module() const {
+  return MEMOIR_SANITIZE(parent<llvm::Module>(this->value()),
+                         "Object has no parent module!");
+}
+
+bool operator<(const Object &lhs, const Object &rhs) {
+  auto *lvalue = &lhs.value(), *rvalue = &rhs.value();
+  if (lvalue != rvalue) {
+    return lvalue < rvalue;
+  }
+
+  auto lsize = lhs.offsets().size(), rsize = rhs.offsets().size();
+  if (lsize != rsize) {
+    return lsize < rsize;
+  }
+
+  for (size_t i = 0; i < lsize; ++i) {
+    auto loffset = lhs.offsets()[i], roffset = rhs.offsets()[i];
+    if (loffset != roffset) {
+      return loffset < roffset;
+    }
+  }
+
+  return false;
+}
+
+bool operator==(const Object &lhs, const Object &rhs) {
+  auto *lvalue = &lhs.value(), *rvalue = &rhs.value();
+  if (lvalue != rvalue) {
+    return false;
+  }
+
+  auto lsize = lhs.offsets().size(), rsize = rhs.offsets().size();
+  if (lsize != rsize) {
+    return false;
+  }
+
+  for (size_t i = 0; i < lsize; ++i) {
+    auto loffset = lhs.offsets()[i], roffset = rhs.offsets()[i];
+    if (loffset != roffset) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const Object &obj) {
+  os << "(" << obj.value() << ")";
+  for (auto offset : obj.offsets()) {
+    if (offset == unsigned(-1)) {
+      os << "[*]";
+    } else {
+      os << "." << std::to_string(offset);
+    }
+  }
+
+  return os;
+}
+
+} // namespace llvm::memoir
+
+using namespace llvm::memoir;
+
+std::size_t std::hash<Object>::operator()(const Object &obj) const noexcept {
+  std::size_t h1 = std::hash<llvm::Value *>{}(&obj.value());
+  std::size_t i = 0;
+  for (auto offset : obj.offsets()) {
+    std::size_t h2 = std::hash<Offset>{}(offset);
+    h1 ^= (h2 << ++i);
+  }
+  return h1;
+}
