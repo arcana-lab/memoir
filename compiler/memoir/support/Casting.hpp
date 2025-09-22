9966728a019ab9c811210c66fcc21bea3950995e
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-09-22 17:44:17 -0500

    [memoir][support] Fixed compiler warning

--- compiler/memoir/support/Casting.hpp
+++ compiler/memoir/support/Casting.hpp
@@ -22,40 +22,23 @@ using llvm::dyn_cast;
 using llvm::dyn_cast_or_null;
 
 // Functions to dyn_cast an llvm::Instruction to a MemOIRInst
-template <class To,
-          class From,
-          std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
-          std::enable_if_t<std::is_base_of_v<llvm::Value, From>, bool> = true>
+template <class To, class From>
 To *into(From *V);
 
-template <class To,
-          class From,
-          std::enable_if_t<std::is_base_of_v<MemOIRInst, To>, bool> = true,
-          std::enable_if_t<std::is_base_of_v<llvm::Value, From>, bool> = true>
+template <class To, class From>
 To *into(From &V);
 
-template <class To,
-          class From,
-          std::enable_if_t<std::is_base_of_v<llvm::Value, To>, bool> = true,
-          std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
+template <class To, class From>
 To *into(From *I);
 
-template <class To,
-          class From,
-          std::enable_if_t<std::is_base_of_v<llvm::Value, To>, bool> = true,
-          std::enable_if_t<std::is_base_of_v<MemOIRInst, From>, bool> = true>
+template <class To, class From>
 To *into(From &I);
 
 // Functions to check type of Keyword
-template <class To,
-          std::enable_if_t<std::is_base_of_v<Keyword, To>, bool> = true,
-          std::enable_if_t<not std::is_same_v<Keyword, To>, bool> = true>
+template <class To>
 std::optional<To> into(llvm::Use &U);
 
-template <class To,
-          class From,
-          std::enable_if_t<std::is_base_of_v<Keyword, To>, bool> = true,
-          std::enable_if_t<std::is_base_of_v<Keyword, From>, bool> = true>
+template <class To, class From>
 std::optional<To> try_cast(const From &kw);
 
 } // namespace memoir
