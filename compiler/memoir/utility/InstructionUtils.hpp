263265ef0947b871f61180eab78be6936b7ea41f
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-09-22 16:24:43 -0500

    [memoir][utility] Updated the default printing to expand the debuginfo attached to the instruction

--- compiler/memoir/utility/InstructionUtils.hpp
+++ compiler/memoir/utility/InstructionUtils.hpp
@@ -1,4 +1,5 @@
 #include "memoir/ir/Instructions.hpp"
+#include "llvm/IR/DebugInfoMetadata.h"
 
 #define RESULTANT(CLASS, NAME)                                                 \
   llvm::Value &CLASS::get##NAME() const {                                      \
@@ -21,11 +22,27 @@
     return this->getCallInst().getOperandUse(OP_OFFSET + index);               \
   }
 
-#define TO_STRING(CLASS)                                                       \
-  std::string CLASS::toString(std::string indent) const {                      \
+#define TO_STRING(CLASS, OP)                                                   \
+  std::string CLASS::toString() const {                                        \
     std::string str, llvm_str;                                                 \
     llvm::raw_string_ostream llvm_ss(llvm_str);                                \
-    llvm_ss << this->getCallInst();                                            \
-    str = #CLASS + llvm_str;                                                   \
-    return str;                                                                \
+    this->asValue().printAsOperand(llvm_ss, /*type?*/ false);                  \
+    llvm_ss << " = memoir." OP "(";                                            \
+    bool first = true;                                                         \
+    for (auto &arg : this->getCallInst().args()) {                             \
+      if (first)                                                               \
+        first = false;                                                         \
+      else                                                                     \
+        llvm_ss << ", ";                                                       \
+      arg.get()->printAsOperand(llvm_ss);                                      \
+    }                                                                          \
+    llvm_ss << ")";                                                            \
+    if (const auto &loc = this->getCallInst().getDebugLoc()) {                 \
+      auto *scope = cast<llvm::DIScope>(loc.getScope());                       \
+      llvm_ss << " @" << scope->getFilename();                                 \
+      llvm_ss << ":" << std::to_string(loc.getLine());                         \
+      if (loc.getCol() != 0)                                                   \
+        llvm_ss << ":" << std::to_string(loc.getCol());                        \
+    }                                                                          \
+    return llvm_str;                                                           \
   }
