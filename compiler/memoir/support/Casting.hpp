7079462e814afd6cc99fcc173ef92d6076ada73f
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-09-23 10:12:38 -0500

    [memoir][support] Fixed missing symbols in casting impl

--- compiler/memoir/support/Casting.hpp
+++ compiler/memoir/support/Casting.hpp
@@ -3,9 +3,11 @@
 
 #include <type_traits>
 
-#include "llvm/Support/Casting.h"
-
 #include "memoir/support/Assert.hpp"
+#include "memoir/support/Concepts.hpp"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Support/Casting.h"
 
 namespace memoir {
 
@@ -22,24 +24,42 @@ using llvm::dyn_cast;
 using llvm::dyn_cast_or_null;
 
 // Functions to dyn_cast an llvm::Instruction to a MemOIRInst
-template <class To, class From>
-To *into(From *V);
+namespace detail {
+MemOIRInst *get_memoir(llvm::Instruction &inst);
+}
 
-template <class To, class From>
-To *into(From &V);
+template <Derived<MemOIRInst> To, Derived<llvm::Value> From>
+To *into(From *V) {
+  auto *I = dyn_cast_or_null<llvm::Instruction>(V);
+  if (I == nullptr) {
+    return nullptr;
+  }
+  auto *memoir_inst = detail::get_memoir(*I);
+  return dyn_cast_or_null<To>(memoir_inst);
+}
 
-template <class To, class From>
-To *into(From *I);
+template <Derived<MemOIRInst> To, Derived<llvm::Value> From>
+To *into(From &V) {
+  return into<To>(&V);
+}
 
-template <class To, class From>
-To *into(From &I);
+template <Derived<llvm::Value> To, Derived<MemOIRInst> From>
+To *into(From *I) {
+  return dyn_cast_or_null<To>(&I->getCallInst());
+}
+
+template <Derived<llvm::Value> To, Derived<MemOIRInst> From>
+To *into(From &I) {
+  return into<To>(&I);
+}
 
 // Functions to check type of Keyword
-template <class To>
-std::optional<To> into(llvm::Use &U);
-
-template <class To, class From>
-std::optional<To> try_cast(const From &kw);
+template <Derived<Keyword> To, Derived<Keyword> From>
+std::optional<To> try_cast(const From &kw) {
+  if (To::classof(kw))
+    return To(kw.getAsUse());
+  return {};
+}
 
 } // namespace memoir
 
