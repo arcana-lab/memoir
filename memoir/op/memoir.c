f008bda0262d682e379d93d44ce274b190551d4d
Tommy McMichen <mcmichen@u.northwestern.edu>
2025-09-29 15:11:41 -0500

    [memoir] Reorganized and cleaned up the directory structure and build system

---
+++ memoir/op/memoir.c
@@ -0,0 +1,34 @@
+#ifndef MEMOIR_OP_MEMOIR_H
+#define MEMOIR_OP_MEMOIR_H
+
+#include "stdarg.h"
+#include "stdbool.h"
+#include "stddef.h"
+#include "stdint.h"
+
+#ifdef __cplusplus
+namespace memoir {
+#endif
+
+// Specify extern "C" for API functions.
+#ifdef __cplusplus
+#  define __C_ATTR extern "C"
+#else
+#  define __C_ATTR
+#endif
+
+// Emit empty function bodies.
+#define MEMOIR_BODY                                                            \
+  {}
+
+// Suppress warnings for empty function bodies.
+#pragma clang diagnostic ignored "-Wreturn-type"
+
+// Include the generated memoir API functions.
+#include "memoir/op/memoir.def"
+
+#ifdef __cplusplus
+} // namespace memoir
+#endif
+
+#endif
